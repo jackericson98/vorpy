b24ce7fbfbd63545df94b40045b0a50ec909ad3e
jacke <jackericson98@gmail.com>
2022-06-14 20:21:38 -0400

    created cube_cell.py to visualize a simple voronoi plot.

---
+++ presentation/cube_cell.py
@@ -0,0 +1,5 @@
+from visualize import *
+
+atoms = [Atom([0, 0, 0], 1), Atom([5, 0, 0], 1), Atom([-5, 0, 0], 1), Atom([0, 5, 0], 1), Atom([0, -5, 0], 1), Atom([0, 0, 5], 1), Atom([0, 0, -5], 1)]
+
+plot_atoms(atoms, colors=['r', 'b'])
