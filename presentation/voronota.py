5bb59f61a5174ce531bbca5cf3f6468833a5a6aa
jacke <jackericson98@gmail.com>
2022-06-15 06:54:29 -0400

    initial commit

---
+++ presentation/voronota.py
@@ -0,0 +1,12 @@
+from objects import Vertex
+
+
+# Read verts function. Used to interperate voronota data
+def read_verts(file):
+    file = open(file).readlines()
+    verts = []
+
+    for i in range(len(file)):
+        data = file[i].split(" ")
+        verts.append(Vertex(data[3:6], data[6]))
+        return verts
