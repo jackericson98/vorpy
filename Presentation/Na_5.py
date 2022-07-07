9e7cfbd50a92422f2de27581d4b9bfb450ab0806
jacke <jackericson98@gmail.com>
2022-07-07 02:25:14 -0400

    Got the grab down

--- Presentation/Na_5.py
+++ Presentation/Na_5.py
@@ -1,4 +1,16 @@
+import os
 from objects import System
-from pathlib import Path
+from build_network import build_network
+os.chdir("..")
 
-sys = System("./Na_W_cluster5.pdb")
+# Get the system
+sys = System("./Data/test_data/Na_W_cluster5.pdb")
+build_network(sys)
+# print(len(sys.net.verts))
+for vert in sys.net.verts:
+    ndxs = []
+    for atom in vert.atoms:
+        # print(atom.loc)
+        ndxs.append(sys.atoms.index(atom))
+    ndxs.sort()
+    # print(ndxs, vert.loc)
