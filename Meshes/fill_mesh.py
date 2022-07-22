5033018f726ff2b68c7263764f9e38ef1b0a6eb7
jacke <jackericson98@gmail.com>
2022-07-21 21:09:13 -0400

    set up better to add the points together

--- Meshes/fill_mesh.py
+++ Meshes/fill_mesh.py
@@ -2,29 +2,37 @@ from Meshes.edge_trace import *
 
 
 # Make mesh function. Goes in shrinking concentric circles inside the edges of the surface toward the com of the edges
-def make_mesh(surf, min_dist, circ_mesh=False, radius=None, edges_made=False):
+def make_mesh(surf, min_dist, radius=None, vta=False):
     # Reset the all surface points to empty lists
     surf.points, surf.vert_points, surf.edge_points, surf.surf_points = [], [], [], []
-    # Add the vertex points to the set of points in the mesh
-    if surf.verts:
+    # If the surface has vertices, add those points to the vert_points attribute of the surface
+    if surf.verts and not vta:
+        # Go through each vertex on the surface
         for vert in surf.verts:
+            # Add the points to the surface's list of vertex points
             surf.vert_points.append(vert.loc)
-    # Add the vert points to the surface's points
-    surf.points.append(surf.vert_points)
-    # Check to see if a circular mesh has been requested
-    if circ_mesh:
+        # Add the vert points to the surface's points
+        surf.points = surf.vert_points
+        # Use the edge tracing function to get edges' points
+        surf.edge_points = edge_trace1(surf, min_dist)
+    # If no edges exist create a circular edge
+    elif not surf.edges:
+        # If no radius is specified, create one 5x larger than the size of the center atom
         if radius is None:
             radius = surf.atoms[1].rad * 5
-        surf.edge_points = make_bless_mesh(surf, radius, min_dist)
-    # This is for the voronota plot, since I didn't want to edge trace the edges
-    elif edges_made:
+        # Add the circular edge points to the surfaces list of edge points
+        surf.edge_points = circ_edge_trace(surf, radius, min_dist)
+    # This is for the voronota plot. If the first edge in the list of edges has points add all edges' points so the list
+    elif vta:
+        # Go through the
         for edge in surf.edges:
-            surf.edge_points.append(edge.points)
-    # In the absence of special requests use the edge tracing function to get our edges
+            surf.edge_points += edge.points
+        surf.points += surf.edge_points
+        return
     else:
-        surf.edge_points = edge_trace1(surf, min_dist)
+        return
     # Add the edge points to the surface's points
-    surf.points.append(surf.edge_points)
+    surf.points += surf.edge_points
     # For each edge point set up a path list.
     paths = [[surf.edge_points[i]] for i in range(len(surf.edge_points))]
     # Grab the smallest of the 2 surface atoms' location
@@ -48,6 +56,7 @@ def make_mesh(surf, min_dist, circ_mesh=False, radius=None, edges_made=False):
     # Set the pn_1 point to infinity
     pn_1 = [np.inf, np.inf, np.inf]
     num_paths = len(paths)
+    surf_points = []
     # Go through ring by ring
     for j in range(num_rings):
         # Go through each of the remaining paths
@@ -58,7 +67,7 @@ def make_mesh(surf, min_dist, circ_mesh=False, radius=None, edges_made=False):
             # Check to see of the new point is too close to the previous point and the path has to end
             if calc_dist(pn, pn_1) < min_dist:
                 # Add the path to the surfaces points and remove it from the paths list
-                surf.surf_points += paths.pop(i)[1:]
+                surf_points += paths.pop(i)[1:]
                 ends.pop(i)
                 dthetas.pop(i)
                 num_paths -= 1
@@ -70,50 +79,7 @@ def make_mesh(surf, min_dist, circ_mesh=False, radius=None, edges_made=False):
                 i += 1
     # Add the remaining paths to the surface excluding the first point in the path (i.e. the edge point)
     for path in paths:
-        surf.surf_points += path[1:]
-
-
-# Make boundless mesh.
-def make_bless_mesh(surf, radius, min_dist):
-    # Grab the surfaces atoms and make sure the smaller one is a0
-    a0, a1 = surf.atoms[0], surf.atoms[1]
-    if a0.rad > a1.rad:
-        a0, a1, = a1, a0
-    r01 = np.array(a1.loc) - np.array(a0.loc)
-    # Get the normalized vector for the direction toward the center of the surface
-    r01_hat = r01/np.linalg.norm(r01)
-    # Get the point on the surface of a0 closest to a1
-    dist = calc_dist(a0.loc, a1.loc) - (a0.rad + a1.rad)
-    # Get the point on the surface corresponding to vc
-    center = a0.loc + r01_hat * (dist + a0.rad)
-    # Find a vector perpendicular to r01_hat
-    if abs(r01_hat[0]) > abs(r01_hat[1]):
-        p = np.array([r01_hat[1], -r01_hat[0], 0])
-    elif r01_hat[0] == 0 and r01_hat[1] == 0:
-        p = np.array([1, 0, 0])
-    else:
-        p = np.array([-r01_hat[1], r01_hat[0], 0])
-    # Normalize it
-    p_hat = p/np.linalg.norm(p)
-    c_points = [p_hat*radius + center]
-    # Get the circumference of the circle and divide by the minimum distance
-    num_points = int(2*np.pi*radius/min_dist)
-    # Get the incremental angle change around the circle
-    dtheta = 2*np.pi/num_points
-    # Find the amount we project on the circle
-    proj_dist = radius * np.tan(dtheta)
-    for i in range(num_points):
-        # Find the binormal vector to r01_hat and the previous circle point by taking their cross products
-        bi = np.cross(r01_hat, c_points[-1] - center)
-        # Normalize it
-        bi_hat = bi/np.linalg.norm(bi)
-        # Get the surface point
-        samp = c_points[-1] + proj_dist*bi_hat
-        rn = samp - center
-        rn_hat = rn/np.linalg.norm(rn)
-        c_points.append(center + rn_hat*radius)
-    edge_points = []
-    for i in range(len(c_points)):
-        edge_points.append(calc_surf_point(surf, c_points[i]))
-    return edge_points
-
+        surf_points += path[1:]
+    surf.surf_points = np.array(surf_points).tolist()
+    # Add the surface points to the general list of points
+    surf.points += surf.surf_points
