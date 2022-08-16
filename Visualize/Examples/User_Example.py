35f48257dd53afba944c3b498b62be925b568c56
jacke <jackericson98@gmail.com>
2022-08-16 19:15:02 -0400

    Added export and analyze

--- Visualize/Examples/User_Example.py
+++ Visualize/Examples/User_Example.py
@@ -6,14 +6,14 @@ os.chdir("../..")
 
 
 # Files
-m_file = "./Data/test_data/Na_W_cluster5.pdb"
+m_file = "./Data/test_data/Na_W_cluster7.pdb"
 b_file = "./Data/test_data/Na_W_cluster5_balls.txt"
 v_file = "./Data/test_data/Na_W_cluster5_vertices.txt"
 
 # Get the System
 sys = System(m_file)
 
-sys.build_network(1, surfs=True)
+sys.build_network(.1, surfs=True)
 
 # Plot the System
 fig = plt.figure()
@@ -21,4 +21,7 @@ ax = fig.add_subplot(projection='3d')
 plot_atoms(sys.atoms[:1], fig=fig, ax=ax, alpha=.1, colors=['w' for i in range(len(sys.atoms))])
 plot_verts(sys.atoms[0].verts, fig=fig, ax=ax, colors=['r' for i in range(len(sys.net.verts))])
 plot_edges(sys.atoms[0].edges, fig=fig, ax=ax)
-plot_surfs(sys.atoms[0].surfs, fig=fig, ax=ax, alpha=1, simps=True, Show=True)
+plot_surfs(sys.atoms[0].surfs, fig=fig, ax=ax, alpha=1, simps=True)
+
+sys.analyze()
+sys.export()
