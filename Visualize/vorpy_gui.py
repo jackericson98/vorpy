7a7bc91a209ef6d82bada31b33a835ca8f42aae3
jacke <jackericson98@gmail.com>
2022-09-06 21:26:57 -0400

    Entirely revamped

--- Visualize/vorpy_gui.py
+++ Visualize/vorpy_gui.py
@@ -1,136 +1,201 @@
 # Outside Imports
+import os
 import tkinter as tk
-from tkinter import filedialog, CENTER
+from tkinter import filedialog
 # Internal Imports
 from System.system import System
-from System.Network import build_network
-from Presentation.visualize import *
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
-import glob
 
 
 class Vorpy:
     """Vorpy GUI class. When instantiated the Gui will launch"""
-    def __init__(self, width=1000, height=800):
+    def __init__(self, width=450, height=600):
         # Set up the window
         self.vp_main = tk.Tk()
         self.width = width
         self.height = height
         self.vp_main.geometry(str(width) + "x" + str(height))
         self.vp_main.title('vorpy')
+        self.file = ""
+        self.sys_box_size = 2
+        self.sys_resolution = 0.1
+        self.vorpy_directory = os.getcwd()
+        self.output_directory = os.getcwd()
+        self.verts_loaded = False
 
         # Instantiate the System
         self.sys = None
 
         # Set up the strings
-        self.head_str = tk.StringVar(self.vp_main)
-        self.name = tk.StringVar(self.vp_main)
+        self.file_name = tk.StringVar(self.vp_main, "No System Selected")
         self.dd_name = tk.StringVar(self.vp_main)
         self.dd_var = tk.StringVar(self.vp_main)
-        # Get a list of the files in test data
-        pdb_vars = glob.glob("./Data/test_data/*.pdb")
-        gro_vars = glob.glob("./Data/test_data/*.gro")
-        mol_vars = glob.glob("./Data/test_data/*.mol")
-        self.dd_vars = pdb_vars + gro_vars + mol_vars
-        for i in range(len(self.dd_vars)):
-            self.dd_vars[i] = self.dd_vars[i][17:]
+        self.box_size = tk.StringVar(self.vp_main, str(self.sys_box_size))
+        self.resolution = tk.StringVar(self.vp_main, str(self.sys_resolution))
+        self.vta = tk.BooleanVar(self.vp_main)
+        self.parallelize = tk.BooleanVar(self.vp_main)
+        self.output_dir_str = tk.StringVar(self.vp_main, os.getcwd()[:12] + ' ... ' + os.getcwd()[-12:])
 
-        # Give strings names
-        self.head_str.set("Vorpy")
-        self.name.set("No File Selected")
-        self.dd_name.set("Toy Systems")
-        self.dd_var.set(self.dd_vars[0])  # default value
+        self.output_all = tk.BooleanVar(self.vp_main, True)
+        self.output_sys = tk.BooleanVar(self.vp_main, False)
+        self.output_atoms = tk.BooleanVar(self.vp_main, False)
+        self.output_mols = tk.BooleanVar(self.vp_main, False)
+        self.output_surfs = tk.BooleanVar(self.vp_main, False)
+        self.output_analysis = tk.BooleanVar(self.vp_main, False)
+        self.output_verts = tk.BooleanVar(self.vp_main, False)
+        self.output_pdb = tk.BooleanVar(self.vp_main, False)
 
-        # Text boxes:
-        self.head = tk.Label(text="VorPy", font=('Helvetica bold', 40))
-        self.filename = tk.Label(self.vp_main, textvariable=self.name, font=('Times New Roman', 20))
+        # Set up the main frame
+        f = tk.Frame(self.vp_main)
+        f.place(relx=0.5, rely=0.5, anchor='c')
+        # Header:
+        tk.Label(f, text="VorPy", font=('Times New Roman bold', 40)).grid(row=0, column=0, columnspan=3)
+        tk.Label(f, text="").grid(row=1)
+        # Input info
+        tk.Label(f, text="Load", font=('Times New Roman bold', 20)).grid(row=2, column=0, columnspan=2, sticky='w')
+        tk.Label(f, text="System: ", font=('Times New Roman', 15)).grid(row=3, column=0, sticky='w')
+        tk.Label(f, textvariable=self.file_name, font=('Times New Roman', 15))\
+            .grid(row=3, column=1, columnspan=2, sticky='w')
+        tk.Button(f, text="Load Vertices", command=self.add_vertices).grid(row=4, column=0, sticky='e')
+        tk.Button(f, text="Build System", command=self.build_sys_button).grid(row=4, column=1)
+        tk.Button(f, text="Load System", command=self.load_sys_button).grid(row=4, column=2, sticky='w')
+        tk.Label(f, text="").grid(row=5)
 
-        # Buttons:
-        self.get_file = tk.Button(text="Load Molecule", command=self.load_molecule_button)
-        self.make_network = tk.Button(text="Build Network", command=self.build_network_button)
-        self.make_meshes = tk.Button(text="Build Surfaces", command=self.build_meshes_button)
-        self.exit = tk.Button(text="Exit", command=self.vp_main.destroy)
-        self.atoms_butt = tk.Button(text="Atoms", command=self.show_atoms)
-        self.verts_butt = tk.Button(text="Vertices", command=self.show_verts)
+        # Settings
+        tk.Label(f, text='Settings', font=('Times New Roman bold', 20)).grid(row=6, column=0, columnspan=2, sticky='w')
+        tk.Label(f, text="Container Size: ").grid(row=7, column=0, sticky='w')
+        tk.Entry(f, textvariable=self.box_size).grid(row=7, column=1, sticky='w')
+        tk.Label(f, text="Resolution: ").grid(row=8, column=0, sticky='w')
+        tk.Entry(f, textvariable=self.resolution).grid(row=8, column=1, sticky='w')
+        tk.Checkbutton(f, text="Voronota System ", variable=self.vta, onvalue=True, offvalue=False)\
+            .grid(row=9, column=0, sticky='w')
+        tk.Checkbutton(f, text="Parallelize ", variable=self.parallelize, onvalue=True, offvalue=False)\
+            .grid(row=10, column=0, sticky='w')
+        tk.Label(f, text="").grid(row=11)
 
-        # Plot
-        self.fig = Figure(figsize=(self.width / 100, self.height / 100), dpi=50)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.vp_main)
+        # Outputs
+        tk.Label(f, text="Outputs", font=("Times New Roman bold", 20)).grid(row=12, column=0, columnspan=2, sticky='w')
+        tk.Label(f, text="Output Directory: ").grid(row=13, column=0, sticky='w')
+        tk.Label(f, textvariable=self.output_dir_str).grid(row=13, column=1, sticky='w')
+        tk.Button(f, text="Change", command=self.change_output_directory).grid(row=13, column=2, sticky='e')
 
-        # Drop down menu
-        self.dd = tk.OptionMenu(self.vp_main, self.dd_var, *self.dd_vars)
+        tk.Label(f, text="Output Files").grid(row=14, column=0, sticky='w')
+        tk.Checkbutton(f, text="All", variable=self.output_all, onvalue=True, offvalue=False)\
+            .grid(row=15, column=0, sticky='w')
+        tk.Checkbutton(f, text="System", variable=self.output_sys, onvalue=True, offvalue=False)\
+            .grid(row=15, column=1, sticky='w')
+        tk.Checkbutton(f, text="System pdb ", variable=self.output_pdb, onvalue=True, offvalue=False) \
+            .grid(row=15, column=2, sticky='w')
+        tk.Checkbutton(f, text="Atom Cells", variable=self.output_atoms, onvalue=True, offvalue=False)\
+            .grid(row=16, column=0, sticky='w')
+        tk.Checkbutton(f, text="Surfaces", variable=self.output_surfs, onvalue=True, offvalue=False) \
+            .grid(row=16, column=1, sticky='w')
+        tk.Checkbutton(f, text="Molecule Interfaces", variable=self.output_mols, onvalue=True, offvalue=False)\
+            .grid(row=16, column=2, sticky='w')
+        tk.Checkbutton(f, text="Analysis ", variable=self.output_analysis, onvalue=True, offvalue=False)\
+            .grid(row=17, column=0, sticky='w')
+        tk.Checkbutton(f, text="Vertices ", variable=self.output_verts, onvalue=True, offvalue=False)\
+            .grid(row=17, column=1, sticky='w')
+        tk.Label(f, text="").grid(row=18)
 
-        # Place the items
-        self.head.place(x=width/2 - width/12, y=height/16)
-        self.filename.place(x=width*19/32, y=7/32*height)
-        self.get_file.place(x=3/16*width, y=3/8*height)
-        self.make_network.place(x=3/16*width, y=1/2*height)
-        self.make_meshes.place(x=3/16*width, y=5/8*height)
-        self.atoms_butt.place(x=3 / 4 * self.width - 3 / 16 * self.width, y=3 / 4 * self.height + 1 / 16 * self.height)
-        self.verts_butt.place(x=3 / 4 * self.width + 1 / 16 * self.width, y=3 / 4 * self.height + 1 / 16 * self.height)
-        self.dd.place(x=40, y=50)
+        # Run program
+        tk.Button(f, text="Build System", font=("Times New Roman bold", 20), command=self.build_network_button) \
+            .grid(row=19, column=0, columnspan=3)
 
         # End the loop
         self.vp_main.mainloop()
 
-    def load_molecule_button(self):
+    def load_sys_button(self):
         # File grabber pop up
         file_path = filedialog.askopenfilename()
         # Create the System
         if file_path:
-            self.sys = System(file_path)
+            self.file = file_path
+            self.sys = self.sys = System(file_path)
+        filename = ""
+        i = -1
+        while self.file[i] != "/":
+            filename = filename + self.file[i]
+            i -= 1
+
+        self.file_name.set(filename[::-1][:-4])
+
+    def build_sys_button(self):
+        pass
+
+    def add_vertices(self):
+        if self.sys is None:
+            ErrorBox("Please select a system to add the vertices to")
+        # File grabber pop up
+        file_path = filedialog.askopenfilename()
+        self.sys.add_verts(file_path)
+        self.verts_loaded = True
+
+    def change_output_directory(self):
+        # File grabber pop up
+        file_path = filedialog.askdirectory()
+        # Create the System
+        if file_path:
+            self.output_directory = file_path
+            self.output_dir_str.set(os.getcwd()[:12] + ' ... ' + os.getcwd()[-12:])
         else:
-            self.sys = System("./Data/test_data/" + self.dd_var.get())
-        # Set the name in reverse order since the letters were added backwards
-        self.name.set(self.sys.file_name)
+            ErrorBox("Directory not changed")
 
     # Build network button function.
     def build_network_button(self):
         # Create the System
-        if not self.sys:
-            self.sys = System("./Data/test_data/" + self.dd_var.get())
+        if self.sys is None:
+            ErrorBox("Please select a system")
+        # Create a new directory of one has not been indicated
+        if self.output_directory[-len(self.sys.name):] != self.sys.name:
+            # If the system doesn;t have a name
+            if self.sys.name == '':
+                self.sys.name = "User_Data"
+            i = 0
+            while True:
+                try:
+                    i_str = str(i)
+                    if i == 0:
+                        i_str = ""
+                    os.mkdir(os.getcwd() + "/" + self.sys.name + i_str)
+                    break
+                except FileExistsError:
+                    i += 1
+            self.output_directory = os.getcwd() + "/" + self.sys.name + i_str
         # Build the network
-        build_network(self.sys)  # Try statement with voronota_verts
+        self.sys.build_network(get_verts=not self.verts_loaded, export_verts=(self.output_verts.get() or self.output_all.get()),
+                               directory=self.output_directory, box_size=float(self.box_size.get()),
+                               min_dist=float(self.resolution.get()))
+        # Analyze the network
+        if self.output_analysis.get() or self.output_all.get():
+            self.sys.analyze()
 
-    def build_meshes_button(self):
-        # Build the meshes
-        build_meshes(self.sys)
-        # Plot the surfaces
-        plot_surfs(surfs=self.sys.net.surfs)
-
-    def show_atoms(self):
-        # Set the default System to the dropdown variable
-        if not self.sys:
-            self.sys = System("./Data/test_data/" + self.dd_var.get())
-        plot_atoms(self.sys.atoms, fig=self.fig)
-        # Create Canvas
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack()
-        # placing the toolbar on the Tkinter window
-        self.canvas.get_tk_widget().place(x=self.width * 7 / 16, y=self.height * 5 / 16)
-
-    def show_verts(self):
-
-        plot_verts(self.sys.net.verts, fig=self.fig)
-        # Create Canvas
-        self.canvas.draw()
-
-        self.canvas.get_tk_widget().pack()
+        # Export the system
+        self.sys.export(directory=self.output_directory, export_all=self.output_all.get(),
+                        export_sys=self.output_sys.get(), export_mols=self.output_mols.get(),
+                        export_atoms=self.output_atoms.get(), export_analysis=self.output_analysis.get(),
+                        export_surfs=self.output_surfs.get())
+        os.chdir(self.vorpy_directory)
 
 
 class ErrorBox:
     """Error box class. Used to indicate if an error has occurred and takes the message as an input"""
     def __init__(self, error_message):
         self.eroot = tk.Tk()
+        self.eroot.geometry("300x75")
 
+        self.error_message = tk.StringVar(self.eroot, error_message)
         self.eroot.title("vorpy")
-        self.error = tk.Label(text=error_message, font="none 14 bold")
+        error_frame = tk.Frame(self.eroot)
+        error_frame.place(relx=0.5, rely=0.45, anchor='c')
 
-        self.error.config(anchor=CENTER)
-        self.error.pack(padx=20, pady=20)
+        tk.Label(error_frame, text="Error!", font=("Times New Roman bold", 15)).grid(row=0, column=0, sticky='w')
+        tk.Label(error_frame, textvariable=self.error_message).grid(row=1, column=0, sticky='e')
+        tk.Button(error_frame, text="OK", command=self.eroot.quit()).grid(row=2, column=0, sticky='e')
+
         # End the loop
         self.eroot.mainloop()
 
 
-Vorpy()
+if __name__ == '__main__':
+    os.chdir("..")
+    Vorpy()
