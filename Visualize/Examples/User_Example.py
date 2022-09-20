6c4350722998d934ccd4b94e806d7d1fbd924e1d
jacke <jackericson98@gmail.com>
2022-09-20 17:19:15 -0400

    Got working with current model

--- Visualize/Examples/User_Example.py
+++ Visualize/Examples/User_Example.py
@@ -10,7 +10,7 @@ m_file = os.getcwd() + "./Data/test_data/Na_W_cluster5.pdb"
 v_file = os.getcwd() + "./Data/test_data/Na5_verts.txt"
 
 # Get the System
-sys = System(m_file, box_size=1.5, min_dist=0.1)
+sys = System(m_file, box_size=1.5, min_dist=0.5)
 sys.add_verts(v_file)
 
 sys.net.connect()
@@ -32,5 +32,5 @@ plot_verts(sys.net.verts, fig=fig, ax=ax, colors=['r' for i in range(len(sys.net
 plot_edges(sys.net.edges, fig=fig, ax=ax)
 # plot_surfs(sys.atoms[0].surfs, fig=fig, ax=ax, alpha=1, simps=True)
 plt.show()
-# sys.analyze()
-# sys.export()
+sys.analyze()
+sys.export(export_all=True)
