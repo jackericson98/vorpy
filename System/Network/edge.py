24e78cb283116f98f212a30bca29dd3424368324
jacke <jackericson98@gmail.com>
2022-09-20 17:11:27 -0400

    Minor changes

--- System/Network/edge.py
+++ System/Network/edge.py
@@ -4,7 +4,8 @@ from System.Network.surface import Surface
 
 class Edge:
     """Edge object. Used to build the network and calculate the surfaces"""
-    def __init__(self, atoms, verts, net, calc_points=True):
+    def __init__(self, atoms, verts, net):
+
         self.atoms = atoms  # List of Atom type objects
         self.verts = verts  # List of Vertex type objects
         self.surfs = []  # List of 2 surfaces attached to the edge
@@ -13,14 +14,12 @@ class Edge:
             self.ndx = [net.atoms.index(atom) for atom in self.atoms]
         self.loc = None  # Location of the center of the 3 atoms that make up the edge
         self.rad = None  # Radius of the inscribed circle of the three atoms
-        self.dir = None  # Direction along the edge from the edge's first vertex
         self.points = []  # List of points on the edge. These points do not include the vertex points
-        if calc_points:  # Build the edge if calc_points is true
-            self.build()
-        self.touches_box = False
 
     # Build edge function. Find points along the edge from its first vertex to its second. Has at least 10 points.
     def build(self):
+        # Reset the edges points
+        self.points = []
         # Get the network's minimum distance
         min_dist = self.net.sys.min_dist
         # Grab the vertex points and make numpy arrays out of them
@@ -28,7 +27,7 @@ class Edge:
         # If the edge is completely straight add 1 point in the middle and return
         if self.atoms[0].rad == self.atoms[1].rad and self.atoms[1].rad == self.atoms[2].rad:
             r = pv1 - pv0
-            num_points = int(np.linalg.norm(r) // min_dist)
+            num_points = int(np.linalg.norm(r) // min_dist) + 2
             for i in range(num_points):
                 self.points.append(pv0 + r * (i / num_points))
             return
@@ -42,15 +41,14 @@ class Edge:
         r_mag = np.linalg.norm(r01)  # Magnitude of the vector between the two vertex points
         rn01 = r01 / r_mag  # Normal to the vector between the vertices
         pc01 = pv0 + 0.5 * rn01 * r_mag  # Center point
-        # Get the center point of the edge
-        circ = calc_circ(self.atoms)
-        c, bn = circ[0], circ[1]
+        # Get the center point of the edge and the bottleneck
+        self.loc, self.rad = calc_circ(self.atoms)
         # Determine if the theoretical center of the edge is inside the vertices or not
         dr = 1
-        if calc_dist(c, pv0) < r_mag or calc_dist(c, pv1) < r_mag:
+        if calc_dist(self.loc, pv0) < r_mag or calc_dist(self.loc, pv1) < r_mag:
             dr = -1
         # Find the vector normal to the projection plane
-        P_norm = dr * np.cross(np.array(c) - np.array(pc01), np.array(pv1) - np.array(pc01))
+        P_norm = dr * np.cross(np.array(self.loc) - np.array(pc01), np.array(pv1) - np.array(pc01))
         # Find the vector perpendicular to the plane's normal (i.e. in the plane) and the vector between vertices
         rpcr = - np.cross(P_norm, rn01)
         rnpcr = rpcr / np.linalg.norm(rpcr)
@@ -88,6 +86,9 @@ class Edge:
 
     # Project method. Projects a point onto the surface using a reference point
     def project(self, rn, pa, surf):
+        # Check to see if the surface has its function values
+        if surf.func is None:
+            surf.calc_func()
         # Get the function values
         f, a0, a1 = surf.func, surf.atoms[0], surf.atoms[1]
         # Finding the a, b, c, values that satisfy at**2 + bt + c = 0
