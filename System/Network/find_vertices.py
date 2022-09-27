5d76d4b74f42ce0b743d79140462a2436e9a43f0
jacke <jackericson98@gmail.com>
2022-09-27 15:28:51 -0400

    Incorperated doublet verification into vertex verifying function.

--- System/Network/find_vertices.py
+++ System/Network/find_vertices.py
@@ -81,13 +81,17 @@ def search_verts(test_lol, my_list):
 
 
 # Verify site function. Compares a vertex to the atoms around to see if they overlap
-def verify_site(vert, net, pass_atoms):
+def verify_site(vert, net, doublet_check=False):
+    # Grad the location and radius of the check vertex
+    loc, rad = vert.loc, vert.rad
+    if doublet_check:
+        loc, rad = vert.loc2, vert.rad2
     # Find the indices of the sub-box for the vertex
-    vi = int((vert.loc[0] - net.box[0][0]) / net.sub_box_size[0])
-    vj = int((vert.loc[1] - net.box[0][1]) / net.sub_box_size[1])
-    vk = int((vert.loc[2] - net.box[0][2]) / net.sub_box_size[2])
+    vi = int((loc[0] - net.box[0][0]) / net.sub_box_size[0])
+    vj = int((loc[1] - net.box[0][1]) / net.sub_box_size[1])
+    vk = int((loc[2] - net.box[0][2]) / net.sub_box_size[2])
     # Get the number of boxes that an overlapping atom could possibly be away from the vertex sub-box
-    atom_range = int(vert.rad / min(net.sub_box_size)) + int(5 / min(net.sub_box_size))
+    atom_range = int(rad / min(net.sub_box_size)) + int(5 / min(net.sub_box_size))
     # Get the atoms in that range
     overlap_test_atoms = net.get_atoms([[vi, vj, vk]], atom_range)
     # Set up the overlap tracker
@@ -95,10 +99,10 @@ def verify_site(vert, net, pass_atoms):
     # Go through the atoms in the overlap test atom list
     for atom2 in overlap_test_atoms:
         # If the atom is one of the vertex atoms move on
-        if atom2 in pass_atoms:
+        if atom2 in vert.atoms:
             continue
         # If the distance between the vertex and the test atom is less than the sum of their radii, they overlap
-        if calc_dist(atom2.loc, vert.loc) < atom2.rad + vert.rad:
+        if calc_dist(atom2.loc, loc) < atom2.rad + rad:
             overlap = True
             break
     # If we make it all the way through the list of close atoms without overlapping it is a viable vertex
@@ -143,8 +147,11 @@ def find_site(net, edge_atoms, vn_1=None):
         # Filter the vertex out if it is too large or not able to be made
         if vert.loc is None or vert.rad > min_rad:
             continue
+        # For doublet cases verify differently
+        if vert.loc2 is not None:
+            pass
         # If the site is verified add it to the list of potential vertices
-        if verify_site(vert, net, edge_atoms + [atom]):
+        if verify_site(vert, net):
             verts.append(vert)
             vert_ndx_list_locs.append(vert_ndx)
     # If no verts have been found return
