1b4b742678cf0c3d67bcff18bc1612111aa68ed3
jacke <jackericson98@gmail.com>
2022-10-09 19:38:22 -0400

    Major Commit:
    1. Got rid of many of the functions that were no longer needed
    2. Renamed get_pdb, get_cif, get_mol, get_gro to read_pdb, read_cif, read_mol, read_gro
    3. Fixed redundant code in read pdb. Now only atom information is sorted. All other information if stored in sys.data
    4. Added import_net (UNTESTED). Used to read the system file put out by the export_net functon interpret the lines, create the objects and connect the network

--- System/input_system.py
+++ System/input_system.py
@@ -1,6 +1,6 @@
 from System.Network.network import *
 from System.atom import Atom, get_radius
-import os
+from System.Network.connect_network import Edge, Surface
 
 
 # Get name method. Extracts the name from the file name
@@ -17,65 +17,38 @@ def get_name(file):
     return filename[::-1][:-4]
 
 
-# Read PDB atom data function. Gets atom information from pdb data line
-def read_pdb_atom(line):
-    # Make sure that the line starts with ATOM
-    if line[:4].lower() != 'atom':
-        return
-    # Create the atom
-    atom = Atom([float(line[30:38]), float(line[38:46]), float(line[46:54])], get_radius(line[76:78]),
-                symbol=line[76:78], res=line[17:20], chain=line[21], res_seq=line[22:26])
-    # If no chain is specified, set the chain to 'None'
-    if atom.chain == ' ':
-        atom.chain = 'Mol'
-    # Return the atom
-    return atom
-
-
 # Get pdb data method. Finds the lines of the file with prefixes and returns them as a list
-def get_pdb_data(sys, word):
+def read_pdb(sys):
     # Get the file information
     sys.file = open(sys.file_address).readlines()
     sys.sys_file_name = get_name(sys.file_address)
-    # Special case for Atom lines
-    if word.lower() == 'atom':
-        atoms = []
+    atoms = []
+    data = []
     # Go through each line in the file and check if the first word is the word we are looking for
-        for i in range(len(sys.file)):
-            line = sys.file[i]
-            if line and line[:4].lower() == 'atom':  # Check if the line starts with atom
-                atom = read_pdb_atom(line)
-                atoms.append(atom)
-        return atoms
-    # Standard case
-    else:
-        data = []
-        for i in range(len(sys.file)):
-            if word == sys.file[i][:len(word)]:  # check the first len(word) letters
-                # Add the split test_data to our list and remove the word at the beginning of the list
-                data.append(sys.file[i].split()[1:])
-    return data
+    for i in range(len(sys.file)):
+        line = sys.file[i]
+        word = line[:4].lower()
+        if line and word == 'atom':  # Check if the line starts with atom
+            # Create the atom
+            atom = Atom([float(line[30:38]), float(line[38:46]), float(line[46:54])], get_radius(line[76:78]),
+                        symbol=line[76:78], res=line[17:20], chain=line[21], res_seq=line[22:26], name=line[12:16],
+                        ocp=line[54:60], t_fact=line[60:66], seg_id=line[72:76], charge=line[78:80])
+            # If no chain is specified, set the chain to 'None'
+            if atom.chain == ' ':
+                atom.chain = 'Mol'
+            # Add the atom to the
+            atoms.append(atom)
+        else:
 
-
-# Get pdb method. Finds the atoms and
-def get_pdb(sys):
-    # .PDB file type standards.
-    keys = ['HEADER', 'TITLE', 'COMPOUND', 'SOURCE', 'KEYWDS', 'EXPDTA', 'AUTHOR', 'REVDAT', 'JRNL', 'REMARK',
-            'DBREF', 'SEQADV', 'FORMUL', 'SEQRES', 'HELIX', 'SHEET', 'CRYST', 'ORIG', 'SCALE', 'TER', 'HETATM',
-            'MASTER']
-    # Define the keys
-    pdb_stds = ['header', 'title', 'compound', 'source', 'key_words', 'exp_data', 'author', 'revisions', 'journal',
-                'remarks', 'debrief', 'seq_adv', 'formula', 'residues', 'helix', 'sheet', 'crystal', 'origin',
-                'scale', 'terminals', 'het_atom', 'master']
-    # Set the keys
-    for i in range(len(pdb_stds)):
-        sys.info[keys[i]] = get_pdb_data(sys, pdb_stds[i])
-    # Grab the lines that start with ATOM and create Atom objects
-    sys.atoms = get_pdb_data(sys, 'ATOM')
+            for i in range(len(sys.file)):
+                if word == sys.file[i][:len(word)]:  # check the first len(word) letters
+                    # Add the split test_data to our list and remove the word at the beginning of the list
+                    data.append(sys.file[i].split()[1:])
+    return atoms, data
 
 
 # Get cif function. Finds the data in a cif file
-def get_cif(sys):
+def read_cif(sys):
     # Get the system file
     sys.file = open(sys.file_address).readlines()
     num = int(sys.file[0][4:])
@@ -90,7 +63,7 @@ def get_cif(sys):
 
 
 # Get gro method. Finds data in a gro file
-def get_gro(sys):
+def read_gro(sys):
     sys.file = open(sys.file_address).readlines()
     sys.info['header'] = sys.file[0]
     # Go through each line in the file and create an atom object
@@ -99,66 +72,17 @@ def get_gro(sys):
 
 
 # Get mol method. Finds data in a mol file
-def get_mol(sys):
+def read_mol(sys):
     sys.file = open(sys.file_address).readlines()
     for line in sys.file:
         if len(line) > 6:
             sys.atoms.append(Atom([line[0], line[1], line[2]], get_radius(line[3]), symbol=line[3]))
 
 
-# Add vertices function. Takes in a system and a file with vertices in it and adds the verts to the system
-def add_verts(sys, file_address):
-    # Reset the network and open the network
-    sys.net.verts, sys.net.surfs, sys.net.edges = [], [], []
-    vert_file = open(file_address).readlines()
-    # Go through each of the vertices file
-    for i in range(len(vert_file)):
-        # Set up the line variable and split it
-        line = vert_file[i]
-        line = line.split()
-        # Set uo the line2 variable
-        line2 = None
-        # If there is another line after this one, check it for the same atoms
-        if i + 1 < len(vert_file):
-            line2 = vert_file[i + 1]
-            line2 = line2.split()
-        atoms = [sys.atoms[int(line[_])] for _ in range(4, 8)]
-        # Check if the next line has the same atom indices as the current line
-        if line2 is not None and atoms == [sys.atoms[int(line2[_])] for _ in range(4, 8)]:
-            print("Doublet")
-            # Doublet vertex
-            my_vert = Vertex(atoms, location=[float(line[0]), float(line[1]), float(line[2])], radius=float(line[3]),
-                             net=sys.net, doublet=True, loc2=[float(line2[0]), float(line2[1]),
-                             float(line2[2])], rad2=float(line2[3]))
-            # Skip the next line
-            i += 1
-        else:
-            # Regular vertex
-            my_vert = Vertex(atoms, location=[float(line[0]), float(line[1]), float(line[2])], radius=float(line[3]),
-                             net=sys.net)
-        # Add the vertex to the system
-        sys.net.verts.append(my_vert)
-
-
-# Add vertices function. Takes in a system and a file with vertices in it and adds the verts to the system
-def add_grant_verts(sys, file_address):
-    # Reset the network and open the network
-    sys.net.verts, sys.net.surfs, sys.net.edges = [], [], []
-    vert_file = open(file_address).readlines()
-    # Go through each of the vertices file
-    for line in vert_file[1:]:
-        line = line.split(',')
-        atoms = [sys.atoms[int(line[i]) - 1] for i in range(4)]
-        loc = [float(line[-3][1:]), float(line[-2]), float(line[-1][:-2])]
-        rad = calc_dist(atoms[0].loc, loc) - atoms[0].rad
-        vert = Vertex(atoms, location=loc, radius=rad, net=sys.net)
-        sys.net.verts.append(vert)
-
-
 # Add Voronota data method. Takes in voronota data and adds it to the System
 def add_vta_data(sys, ball_file, vert_file):
     # Set the voronota system indicator to True
-    sys.net.flat_faces = True
+    sys.myNet.flat_faces = True
     # Create the System and load the files
     vert_file = open(vert_file).readlines()
     ball_file = open(ball_file).readlines()
@@ -176,6 +100,180 @@ def add_vta_data(sys, ball_file, vert_file):
         # Add the vertex data
         loc, rad = [float(data[4]), float(data[5]), float(data[6])], float(data[7])
         atoms = [balls[int(data[0])], balls[int(data[1])], balls[int(data[2])], balls[int(data[3])]]
-        myVert = Vertex(atoms=atoms, net=sys.net, location=loc, radius=rad)
-        sys.net.verts.append(myVert)
+        myVert = Vertex(atoms=atoms, net=sys.myNet, location=loc, radius=rad)
+        sys.myNet.verts.append(myVert)
 
+
+# Import network function. Imports vorpy-created text document and creates network objects
+def import_net(net, filename):
+
+    # Open the file
+    file = open(filename).readlines()
+
+    # Go through the file, line by line
+    for i in range(len(file)):
+
+        ####################################### Prepare the line #######################################################
+
+        # Get the line
+        line = file[i]
+        # Check for empty lines
+        if len(line) == 0:
+            continue
+        # Split the information in the file
+        line.split()
+        # If there is another line after this one, check it for the same atoms
+        if len(file) > i + 1 and len(file[i + 1]) > 0:
+            line2 = file[i + 1]
+            line2.split()
+
+        ######################################### Get Objects ##########################################################
+
+        # Network
+
+        if line[0].lower() == 'netw':
+
+            # Load the network information
+            net.min_dist = float(line[1])
+            net.beta_val = float(line[2])
+            net.box_size = float(line[3])
+            net.sol_verts = bool(line[4])
+            net.curved_faces = bool(line[5])
+            net.flat_faces = bool(line[6])
+
+        # Atoms
+
+        # Check to see if the line is an atom line
+        elif line[0].lower() == "atom":
+            # Get the atom from the network
+            myAtom = net.atoms[int(line[1])]
+            # Set attributes for the atom
+            myAtom.box = [int(_) for _ in line[2:5]]
+            myAtom.cell_vol = float(line[5])
+            # Get the indices of the objects by checking going through the next 3 lines
+            myAtom.load_ndxs = [[int(_) for _ in line[1:]] for line in file[i + 1: i + 4]]
+            # Skip the next 3 lines
+            i += 3
+
+        # Vertices
+
+        # Check for if the line is a vertex line
+        elif line[0].lower() == "vert":
+            # Get the indices of the atoms in the vertex and then the atoms themselves
+            ndxs = [int(_) for _ in line[1:5]]
+            atoms = [net.atoms[ndx] for ndx in ndxs]
+            # Get the location and radius of the vertex
+            loc, rad = [float(_) for _ in line[5:8]], float(line[8])
+            # Get the doublet information
+            dub, loc2, rad2 = bool(line[9]), [float(_) for _ in line[-4:-1]], float(line[-1])
+            # Set up the default vertex
+            myVert = Vertex(atoms=atoms, net=net, location=loc, radius=rad, doublet=dub, loc2=loc2, rad2=rad2)
+            # Get the indices of the objects by checking going through the next 2 lines
+            myVert.load_ndxs = [[int(_) for _ in line[1:]] for line in file[i + 1: i + 3]]
+            # Skip the next 2 lines
+            i += 2
+            # Add the vertex to the network
+            net.verts.append(myVert)
+
+        # Edges
+
+        # Check for if the line is an edge line or not
+        elif line[0].lower() == "edge":
+            # Get the indices and atoms for the edges
+            ndxs = [int(_) for _ in line[1:4]]
+            atoms = [net.atoms[ndx] for ndx in ndxs]
+            # Create the edge
+            myEdge = Edge(atoms, net)
+            # Get the indices of the objects by checking going through the next 2 lines
+            myEdge.load_ndxs = [[int(_) for _ in line[1:]] for line in file[i + 1: i + 3]]
+            # Skip the next 2 lines
+            i += 2
+            # Set up the points list
+            points = []
+            # Get the points for the edge
+            while file[i + 1][0].lower() == 'edpt':
+                points.append([float(_) for _ in file[i + 1][1:]])
+                i += 1
+            # Add the points, location and radius to the edge
+            myEdge.points, myEdge.loc, myEdge.rad = points, [float(_) for _ in line[4:7]], float(line[7])
+            myEdge.pv0, myEdge.pv1 = [float(_) for _ in line[8:11]], [float(_) for _ in line[11:14]]
+            myEdge.doublet = bool(line[14])
+            # Add the edge to the network
+            net.edges.append(myEdge)
+
+        # Surfaces
+
+        # Check for if the line is an edge line or not
+        elif line[0].lower() == "surf":
+
+            # Get the indices and atoms for the edges
+            ndxs = [int(_) for _ in line[1:3]]
+            atoms = [net.atoms[ndx] for ndx in ndxs]
+            # Create the surface and add it to the network
+            mySurf = Surface(atoms=atoms, net=net)
+            # Get the surface normal and the surface area
+            mySurf.rn, mySurf.sa = [float(_) for _ in line[3:6]], float(line[6])
+            # Get the indices of the objects by checking going through the next 2 lines
+            mySurf.load_ndxs = [[int(_) for _ in line[1:]] for line in file[i + 1: i + 3]]
+            # Set up the list of points and perimeter points
+            points, tris = [], []
+            # Get the surface points
+            while file[i + 1][0].lower() == 'supt':
+                # Add the points to the perimeter and increment the counter
+                points.append([float(_) for _ in file[i + 1][1:]])
+                i += 1
+
+            # Get the triangles
+            while file[i + 1][0].lower() == 'sutr':
+                # Add the points to the perimeter and increment the counter
+                tris.append([int(_) for _ in file[i + 1][1:]])
+                i += 1
+            # Add the points, triangles and perimeter points
+            mySurf.points, mySurf.tris, mySurf.perimeter = points, tris, points[:int(line[7])]
+            # Add the surface to the network
+            net.surfs.append(mySurf)
+    # Connect the objects
+    connect_input_net(net)
+
+
+# Connect input network function. Connect the network objects using their load index lists
+def connect_input_net(net):
+
+    # Connect the atom objects
+    for atom in net.atoms:
+        # Connect the atom's verts
+        for i in range(len(atom.load_ndxs[0])):
+            atom.verts.append(net.verts[atom.load_ndxs[0][i]])
+        # Connect the atom's edges
+        for i in range(len(atom.load_ndxs[1])):
+            atom.edges.append(net.edges[atom.load_ndxs[1][i]])
+        # Connect the atom's surfaces
+        for i in range(len(atom.load_ndxs[2])):
+            atom.surfs.append(net.surfs[atom.load_ndxs[2][i]])
+
+    # Connect the vertex objects
+    for vert in net.verts:
+        # Connect the atom's verts
+        for i in range(len(vert.load_ndxs[0])):
+            vert.edges.append(net.edges[vert.load_ndxs[0][i]])
+        # Connect the atom's edges
+        for i in range(len(vert.load_ndxs[1])):
+            vert.surfs.append(net.surfs[vert.load_ndxs[1][i]])
+
+    # Connect the edge objects
+    for edge in net.edges:
+        # Connect the edge's vertices
+        for i in range(len(edge.load_ndxs[0])):
+            edge.verts.append(net.verts[edge.load_ndxs[0][i]])
+        # Connect the edge's surfaces
+        for i in range(len(edge.load_ndxs[1])):
+            edge.surfs.append(net.surfs[edge.load_ndxs[1][i]])
+
+    # Connect the surface objects
+    for surf in net.surfs:
+        # Connect the surface's vertices
+        for i in range(len(surf.load_ndxs[0])):
+            surf.verts.append(net.verts[surf.load_ndxs[0][i]])
+        # Connect the surface's edges
+        for i in range(len(surf.load_ndxs[1])):
+            surf.edges.append(net.edges[surf.load_ndxs[1][i]])
