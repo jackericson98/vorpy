a5c6d7d2fc5d360d2710630fc936959ef78836a9
jacke <jackericson98@gmail.com>
2022-11-02 10:32:27 -0400

    Moved radii to system

--- System/atom.py
+++ System/atom.py
@@ -71,15 +71,16 @@ class Atom:
         self.box = []           # Box          :   The grid location of the atom
 
 
-def get_radius(radius, return_symbol=False):
+def get_radius(radius, system, return_symbol=False):
     """
         Finds the radius of the atom from the symbol or vice versa
 
     :param radius: Either the elemental symbol for the atom or it's radius
+    :param system: System to reference radii from
     :param return_symbol: Boolean for whether to return the symbol or not
     :return: The radius of the atom from the symbol or vice versa
     """
-
+    radii = system.radii
     # If indicated we return the symbol of atom that the radius indicates
     if return_symbol:
         # Set the atom type to nothing
@@ -101,18 +102,3 @@ def get_radius(radius, return_symbol=False):
     # If nothing is found to be exact return the closest atom type
     return atom_type
 
-
-radii = [['h' , 'he', 'li', 'be', 'b' , 'c' , 'n' , 'o' , 'f' , 'ne', 'na', 'mg', 'al', 'si', 'p' , 's' , 'cl', 'ar',
-          'k' , 'ca', 'sc', 'ti', 'v' , 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br', 'kr',
-          'rb', 'sr', 'y' , 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i' , 'xe',
-          'cs', 'ba', 'la', 'hf', 'ta', 'w' , 're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn',
-          'fr', 'ra', 'ac', 'rf', 'db', 'sg', 'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og',
-          'ce', 'pr', 'nd', 'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu',
-          'th', 'pa', 'u' , 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr'],
-         [1.30, 1.40, 0.76, 0.45, 1.92, 1.80, 1.60, 1.50, 1.33, 1.54, 1.02, 0.72, 0.60, 2.10, 1.90, 1.90, 1.81, 1.88,
-          1.38, 1.00, None, None, None, None, None, None, None, None, None, None, 0.62, 0.73, 0.58, 1.90, 1.83, 2.02,
-          1.52, 1.18, None, None, None, None, None, None, None, None, None, None, 1.93, 2.17, 2.06, 2.06, 2.20, 2.16,
-          1.67, 1.35, None, None, None, None, None, None, None, None, None, None, 1.96, 2.02, 2.07, 1.97, 2.02, 2.20,
-          3.48, 2.83, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-          None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-          None, None, None, None, None, None, None, None, None, None, None, None, None, None]]
