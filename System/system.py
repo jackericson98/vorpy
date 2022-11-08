8acb69bfd7649b5b0861d4e0ceedbb4ce2e4a528
jacke <jackericson98@gmail.com>
2022-11-07 20:49:34 -0500

    Comments out the wazoo, plus some bug fixes/catches

--- System/system.py
+++ System/system.py
@@ -1,4 +1,4 @@
-import os
+# import os
 import time
 
 from System.input import *
@@ -7,65 +7,85 @@ from System.Network.network import *
 
 
 class System:
-    """Class used to import files of all types and return a System"""
-    def __init__(self, atoms=None, mols=None, sol=None, residues=None, data=None, name=None, base_file=None,
-                 frame_files=None, nets=None, net_files=None, ndx_files=None, gui=None):
+    def __init__(self, atoms=None, net=None, mols=None, sol=None, residues=None, data=None, name=None, base_file=None,
+                 vert_file=None, net_file=None, index_file=None):
+        """
+        Class used to import files of all types and return a System
+        :param name: (str) Name describing the system
+        :param atoms: List holding the atom objects
+        :param mols: List of molecule atom object lists
+        :param sol: List of solution atom objects
+        :param residues: List of residue atom object lists
+        :param data: Additional data provided by the base file
+        :param base_file: Base system file address
+        :param vert_file: Vertex data file address in vorpy format
+        :param net_file: Network data file address in vorpy format
+        :param index_file: Index file address in GROMACS index format
+        """
 
         # Names
-        self.name = name                      # Name             :    Name describing the system
-        self.atom_names = None                # Atom Names       :    List holding the names of the atoms in the system
-        self.mol_names = None                 # Residue Names    :    List of molecule names
-        self.res_names = None                 # Residue Names    :    List of residue names
-        self.ndx_names = None                 # Index Names      :    List of names of indices corresponding to ndxs
+        self.name = name                    # Name             :   Name describing the system
+        self.atom_names = None              # Atom Names       :   List holding the names of the atoms in the system
+        self.mol_names = None               # Residue Names    :   List of molecule names
+        self.res_names = None               # Residue Names    :   List of residue names
+        self.ndx_names = None               # Index Names      :   List of names of indices corresponding to ndxs
 
         # Data
-        self.net = Network(self, atoms)       # Network          :    Network object holding the primary network
-        self.nets = nets                      # Networks         :    Different networks for other frames
-        self.atoms = atoms                    # Atoms            :    List holding the atom objects
-        self.mols = mols                      # Molecules        :    List of molecules
-        self.residues = residues              # Residues         :    List of residues (lists of atoms)
-        self.sol = sol                        # Solution         :    List of solution molecules (lists of atoms)
-        self.ndxs = None                      # Indices          :    List of lists indices of atoms
+        self.net = net                      # Network          :   Network object holding the primary network
+        self.atoms = atoms                  # Atoms            :   List holding the atom objects
+        self.mols = mols                    # Molecules        :   List of molecules
+        self.residues = residues            # Residues         :   List of residues (lists of atoms)
+        self.sol = sol                      # Solution         :   List of solution molecules (lists of atoms)
+        self.ndxs = None                    # Indices          :   List of lists indices of atoms
+        self.radii = my_radii               # Radii            :   List of
 
         # Set up the file attributes
-        self.data = data                      # Data             :    Additional data provided by the base file
-        self.base_file = base_file            # Base file        :    Primary file address
-        self.net_file = net_files            # Network files    :    Network files for multiple frames
-        self.ndx_file = ndx_files            # Index files      :    File addresses for index file in GROMACS format
-        self.frame_files = frame_files        # Frame files      :    File addresses for different frames (.gro,.pdb)
-        self.output_directory = None          # Output Directory :    Output directory for the export files
-        self.vorpy_directory = os.getcwd()    # Vorpy Directory  :    Directory that vorpy is running out of
+        self.data = data                    # Data             :   Additional data provided by the base file
+        self.base_file = base_file          # Base file        :   Primary file address
+        self.vert_file = vert_file          # Vertex file      :   Address to the vertices of the primary system
+        self.net_file = net_file            # Network files    :   Network files for multiple frames
+        self.ndx_file = index_file          # Index file       :   File addresses for index file in GROMACS index format
+        out_dir = os.getcwd() + "/Data/User_data/"
+        self.output_directory = out_dir     # Output Directory :   Output directory for the export files
+        self.vorpy_directory = os.getcwd()  # Vorpy Directory  :   Directory that vorpy is running out of
 
         # Gui
-        self.gui = gui                       # GUI               :    GUI Vorpy object that can be updated through sys
-        self.radii = my_radii
+        self.gui = None                     # GUI              :   GUI Vorpy object that can be updated through sys
 
-    # Load system method. Chooses the correct file type from the file provided
     def load_sys(self, file):
-
+        """
+        Sets the base file for the system using one of the import file functions
+        :param file: .pdb, .gro, .mol, .cif
+        :return:
+        """
         # If a file is given read the file and set the system attributes
         if file:
+            # Set the file
             self.base_file = file
             self.name = get_name(file)
-            # Check the file type
+            # Read PDB file
             if file[-3:] == "pdb":
                 self.atoms, self.data = read_pdb(self)
+            # Read CIF file
             elif file[-3:] == "cif":
                 read_cif(self)
+            # Read GRO file
             elif file[-3:] == "gro":
                 read_gro(self)
+            # Read MOL file
             elif file[-3:] == "mol":
                 read_mol(self)
             else:
-                print("Wrong file Loser!")
                 return
-        # Set the output directory
-        if self.output_directory is None:
-            set_output_dir(self)
         # Sort the atoms
         self.sort_atoms()
 
     def load_verts(self, filename):
+        """
+        Loads vorpy specific vertices file from the system level
+        :param filename:
+        :return:
+        """
         read_verts(self.net, filename)
 
     def load_net(self, net_file, verts_only=False):
@@ -75,15 +95,15 @@ class System:
         :param verts_only:
         """
         # If no file has been loaded before, create the main network
-        if self.nets is None:
-            self.net_file = [net_file]
-            reat_net(self.net, net_file, verts_only=verts_only)
-        else:
-            self.net_file.append(net_file)
-            self.nets.append(Network(self, self.atoms))
-            reat_net(self.nets[-1], net_file, verts_only=verts_only)
+        self.net_file = net_file
+        self.net = Network(self, atoms=self.atoms)
+        read_net(self.net, net_file, verts_only=verts_only)
 
     def load_ndx(self):
+        """
+        Reads GROMACS index files from the system level
+        :return:
+        """
         read_ndx(self)
 
     # Build System method.
@@ -105,8 +125,8 @@ class System:
     def random_system(self, anums=30, dmax=15, rmax=1):
         """
         Creates a System with atoms placed in random locations with random radii
-        :param anums:
-        :param dmax:
+        :param anums: Integer for the number of atoms in the system
+        :param dmax: Maximum distance from the center for the atoms
         :param rmax:
         :return:
         """
@@ -115,8 +135,11 @@ class System:
             # Choose a random set of 3 numbers between dmax and -dmax. Choose a random radius between 0 and rmax
             self.atoms.append(Atom(np.random.rand(3)*2*dmax - dmax, np.random.rand()*rmax))
 
-    # Sort atoms method. Used to put atoms in their correct molecules and residues
     def sort_atoms(self):
+        """
+        Used to put atoms in their correct molecules and residues
+        :return:
+        """
         # Set up the chain names list
         self.mols, self.mol_names, self.atom_names, self.sol = [], [], [], []
         # Go through each of the atoms in the system adding the atoms to their respective chains
@@ -152,8 +175,23 @@ class System:
             else:
                 self.residues[self.res_names.index(res_name)].append(atom)
 
-    # Build network function. Allows user to build the network from the system object.
-    def build_network(self):
+    def build_network(self, output=True, max_vert=None, box_size=None, min_dist=None, sol_verts=None):
+        """
+        Allows user to build the network from the system object.
+        :return:
+        """
+        # Check to see if a network exists
+        if self.net is None:
+            self.net = Network(sys=self, atoms=self.atoms)
+        # Check for input values for the network build
+        if max_vert is not None:
+            self.net.max_vert = max_vert
+        if box_size is not None:
+            self.net.box_size = box_size
+        if min_dist is not None:
+            self.net.min_dist = min_dist
+        if sol_verts:
+            self.net.sol_verts = sol_verts
         # Instantiate the timer variables
         self.net.my_time, self.net.cpu_time = 0, 0
         # Start the timer
@@ -162,7 +200,7 @@ class System:
         self.net.atoms = self.atoms
         # Set the settings info
         if self.gui is not None:
-            self.net.min_dist, self.net.beta_val = self.gui.sys_res_flt.get(), self.gui.sys_alpha_value.get()
+            self.net.min_dist, self.net.max_vert = self.gui.sys_res_flt.get(), self.gui.sys_max_vert.get()
             self.net.box_size, self.net.sol_verts = self.gui.sys_box_x_flt.get(), self.gui.sol_verts.get()
         # Sort the atoms in the network
         self.net.sort_atoms()
@@ -172,6 +210,9 @@ class System:
             self.net.name = "Main"
             # Find the vertices
             self.net.find_verts()
+            # Check to see if there are vertices
+            if self.net.verts is None or len(self.net.verts) <= 4:
+                return
             # Connect the network
             self.net.connect()
         # Build the edges in the network
@@ -183,15 +224,25 @@ class System:
         # Stop the timer and measure the time
         stop = time.perf_counter()
         self.net.my_time = stop - start
-        self.analysis_prep()
+        if output:
+            self.analysis_prep()
 
-    # Export network method. Exports the values calculated by the network
     def export_net(self):
+        """
+        Exports the values calculated by the network
+        :return:
+        """
         # Export the network
         export_net(self.net)
 
-    # Export selection method. Exports either a single group's body or two group's bodies and the interface between them
     def export_selection(self, group1, group2=None, info=True):
+        """
+        Exports either a single group's body or two group's bodies and the interface between them
+        :param group1: Group object holding a set of atoms for cell analysis
+        :param group2: Group object holding a set of atoms for interface with group 1 analysis
+        :param info: Boolean for whether to export the information file
+        :return:
+        """
         # Change to the designated output directory
         os.chdir(self.output_directory)
         # Export the first group's body
@@ -201,12 +252,18 @@ class System:
             export_body(group1, info_file=info)
             export_iface([group1, group2], info_file=info)
 
-    # Set output directory method. Links set output directory to the system
     def set_output_directory(self):
+        """
+        Links set output directory to the system
+        :return:
+        """
         set_output_dir(self)
 
-    # Analysis preparation method. Prepares the output directory and system for output. Keeps things consistent
     def analysis_prep(self):
+        """
+        Prepares the output directory and system for output. Keeps things consistent
+        :return:
+        """
         # Set the output directory
         self.set_output_directory()
         # Export the network
@@ -228,37 +285,10 @@ class System:
             write_surfs([surf], "surf_" + str(surf.ndx[0]) + "_" + str(surf.ndx[1]), my_color)
 
 
+##################################################### Atomic Radii #####################################################
+
+
 my_radii = [['h' , 'he', 'li', 'be', 'b' , 'c' , 'n' , 'o' , 'f' , 'ne', 'na', 'mg', 'al', 'si', 'p' , 's' , 'cl', 'ar',
-             'k' , 'ca', 'sc', 'ti', 'v' , 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br', 'kr',
-             'rb', 'sr', 'y' , 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i' , 'xe',
-             'cs', 'ba', 'la', 'hf', 'ta', 'w' , 're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn',
-             'fr', 'ra', 'ac', 'rf', 'db', 'sg', 'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og',
-             'ce', 'pr', 'nd', 'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu',
-             'th', 'pa', 'u' , 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr'],
-            [1.30, 1.40, 0.76, 0.45, 1.92, 1.80, 1.60, 1.50, 1.33, 1.54, 1.02, 0.72, 0.60, 2.10, 1.90, 1.90, 1.81, 1.88,
-             1.38, 1.00, None, None, None, None, None, None, None, None, None, None, 0.62, 0.73, 0.58, 1.90, 1.83, 2.02,
-             1.52, 1.18, None, None, None, None, None, None, None, None, None, None, 1.93, 2.17, 2.06, 2.06, 2.20, 2.16,
-             1.67, 1.35, None, None, None, None, None, None, None, None, None, None, 1.96, 2.02, 2.07, 1.97, 2.02, 2.20,
-             3.48, 2.83, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-             None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-             None, None, None, None, None, None, None, None, None, None, None, None, None, None]]
-
-vta_rads = [['h' , 'he', 'li', 'be', 'b' , 'c' , 'n' , 'o' , 'f' , 'ne', 'na', 'mg', 'al', 'si', 'p' , 's' , 'cl', 'ar',
-             'k' , 'ca', 'sc', 'ti', 'v' , 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br', 'kr',
-             'rb', 'sr', 'y' , 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i' , 'xe',
-             'cs', 'ba', 'la', 'hf', 'ta', 'w' , 're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn',
-             'fr', 'ra', 'ac', 'rf', 'db', 'sg', 'bh', 'hs', 'mt', 'ds', 'rg', 'cn', 'nh', 'fl', 'mc', 'lv', 'ts', 'og',
-             'ce', 'pr', 'nd', 'pm', 'sm', 'eu', 'gd', 'tb', 'dy', 'ho', 'er', 'tm', 'yb', 'lu',
-             'th', 'pa', 'u' , 'np', 'pu', 'am', 'cm', 'bk', 'cf', 'es', 'fm', 'md', 'no', 'lr'],
-            [1.30, 1.40, 0.76, 0.45, 1.92, 1.80, 1.60, 1.50, 1.33, 1.54, 1.02, 0.72, 0.60, 2.10, 1.90, 1.90, 1.81, 1.88,
-             1.38, 1.00, None, None, None, None, None, None, None, None, None, None, 0.62, 0.73, 0.58, 1.90, 1.83, 2.02,
-             1.52, 1.18, None, None, None, None, None, None, None, None, None, None, 1.93, 2.17, 2.06, 2.06, 2.20, 2.16,
-             1.67, 1.35, None, None, None, None, None, None, None, None, None, None, 1.96, 2.02, 2.07, 1.97, 2.02, 2.20,
-             3.48, 2.83, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-             None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-             None, None, None, None, None, None, None, None, None, None, None, None, None, None]]
-
-pym_rads = [['h' , 'he', 'li', 'be', 'b' , 'c' , 'n' , 'o' , 'f' , 'ne', 'na', 'mg', 'al', 'si', 'p' , 's' , 'cl', 'ar',
              'k' , 'ca', 'sc', 'ti', 'v' , 'cr', 'mn', 'fe', 'co', 'ni', 'cu', 'zn', 'ga', 'ge', 'as', 'se', 'br', 'kr',
              'rb', 'sr', 'y' , 'zr', 'nb', 'mo', 'tc', 'ru', 'rh', 'pd', 'ag', 'cd', 'in', 'sn', 'sb', 'te', 'i' , 'xe',
              'cs', 'ba', 'la', 'hf', 'ta', 'w' , 're', 'os', 'ir', 'pt', 'au', 'hg', 'tl', 'pb', 'bi', 'po', 'at', 'rn',
