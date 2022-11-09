6829f03df22475b3751ebd69497c6362b284ff00
jacke <jackericson98@gmail.com>
2022-11-09 10:37:46 -0500

    Initial commit. To run parallel network solves

---
+++ Visualize/Examples/User_Example1.py
@@ -0,0 +1,19 @@
+import os
+from System.system import System
+os.chdir("../..")
+
+# Set up a list of files to analyze
+files = ["protein_ligand_complex.pdb"]
+my_dir = os.getcwd()
+# Analyze each of the files
+for file in files:
+    os.chdir(my_dir)
+    # Get the System
+    sys = System()
+
+    sys.load_sys(file="C:/Users/jacke/PycharmProjects/vorpy/Data/test_data/" + file)
+    sys.build_network(sol_verts=True, min_dist=0.2, max_vert=10, box_size=2)
+
+
+
+    print(sys.name + " Completed")
