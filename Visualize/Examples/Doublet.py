30d2351f0f71e50b5019cf8e2462b5c2994b2ca4
jacke <jackericson98@gmail.com>
2022-11-09 10:32:54 -0500

    Not working. Meant to help visualize doublet phenomena

--- Visualize/Examples/Doublet.py
+++ Visualize/Examples/Doublet.py
@@ -1,4 +1,4 @@
-from System.system import System, Atom
+from System.system import System, Atom, get_radius
 from Visualize.visualize import *
 
 # Create the outer atoms
@@ -8,9 +8,15 @@ outer_atoms = [Atom(_, r) for _ in outer_atoms_locs]
 
 # Case 1: 1 surface, 2 edges
 
-c1_atoms = [Atom([-1.001, 0.001, 0], 0.95), Atom([1, 0, 0], 0.95), Atom([0, 2, 0], 1.5), Atom([0, -2, 0], 1.5)]
+# ATOM     16  O6  MOL     1      19.710  19.200  11.260  1.00  0.00           O
+# ATOM    256  OW  SOL   192      18.970  17.660  13.210  1.00  0.00           O
+# ATOM    257  HW1 SOL   192      19.210  17.810  12.290  1.00  0.00           H
+# ATOM    333  HW2 SOL   254      19.210  19.680  13.760  1.00  0.00           H
+
+c1_atoms = [Atom([-1.01, -0.001, 0], 0.95), Atom([1, 0, 0], 0.95), Atom([0, 2, 0], 1.5), Atom([0, -2, 0], 1.5)]
+# c1_atoms = [Atom([19.710, 19.200, 11.260], 1.5), Atom([18.970, 17.660, 13.210], 1.5), Atom([19.210, 17.810, 12.290], 1.3), Atom([19.210, 19.680, 13.760], 1.3)]
 c1_sys = System(c1_atoms + outer_atoms)
-c1_sys.build_network(output=False, max_vert=100, box_size=1.5, min_dist=0.5)
+c1_sys.build_network(output=False, max_vert=100, box_size=100, min_dist=0.5)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
