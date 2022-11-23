66d7386463f27ae27ae94cb17d09c5a7c6621bf7
jacke <jackericson98@gmail.com>
2022-11-23 10:49:13 -0500

    Deleted

--- Visualize/biopython.py
+++
@@ -1,7 +0,0 @@
-from Bio.PDB import *
-import nglview as nv
-import ipywidgets
-
-pdb_parser = PDBParser()
-structure = pdb_parser.get_structure("PHA-L", "../Data/test_data/Na5.pdb")
-view = nv.show_biopython(structure)
