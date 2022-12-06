e12d31e909d54949a3ab212d154c042097030924
jacke <jackericson98@gmail.com>
2022-12-06 14:32:49 -0500

    Set new vals for test files

--- Visualize/Examples/User_Example1.py
+++ Visualize/Examples/User_Example1.py
@@ -4,7 +4,7 @@ from System.system import System
 os.chdir("../..")
 
 # Set up a list of files to analyze
-files = ["DB1976.pdb"]
+files = ["Complex1_frame1.pdb"]
 my_dir = os.getcwd()
 # Analyze each of the files
 for file in files:
@@ -12,7 +12,7 @@ for file in files:
     os.chdir(my_dir)
     # Get the System
     sys = System(os.getcwd() + "/Data/test_data/" + file)
-    sys.build_network(sol_verts=False, surf_res=0.2, max_vert=10, box_size=1.5)
+    sys.build_network(sol_verts=True, surf_res=0.1, max_vert=20, box_size=2.5)
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
     plot_atoms(sys.atoms[0:4], fig=fig, ax=ax)
