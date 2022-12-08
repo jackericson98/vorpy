d8ceff0199b39d2800c9ff0778dd1106b5b955bc
jacke <jackericson98@gmail.com>
2022-12-07 21:13:45 -0500

    Rearranged the attributes to make more sense

--- System/atom.py
+++ System/atom.py
@@ -46,10 +46,20 @@ class Atom:
     def __init__(self, location=None, radius=None, system=None, element="", chain="", residue="", res_seq="", name="",
                  ocp="", t_fact="", seg_id="", charge=""):
 
-        # Inherent traits
+        # Calculated Traits
         self.loc = location     # Location     :   Set the location of the center of the sphere
         self.rad = radius       # Radius       :   Set the radius for the sphere object. Default is 1
+        self.cell_vol = 0       # Cell Volume  :   Volume of the voronoi cell for the atom
+        self.box = []           # Box          :   The grid location of the atom
+
+        # Network connections
         self.sys = system       # System       :   Set the atom's system attribute
+        self.verts = []         # Vertices     :   List of Vertex type objects
+        self.surfs = []         # Surfaces     :   List of Surface type objects
+        self.edges = []         # Edges        :   List of Edge type objects
+        self.load_ndxs = []     # Load indices :   Holds the object indices for when the system is loaded back in
+
+        # Inherent traits
         self.element = element  # Symbol       :   Element of the atom
         self.chain = chain      # Chain        :   Molecule chain the atom is a part of
         self.res = residue      # Residue      :   Residue of the molecule that the atom is a part of
@@ -60,15 +70,8 @@ class Atom:
         self.seg_id = seg_id    # Segment ID   :   Segment identifier for the atom
         self.charge = charge    # Charge       :   Charge of the atom
 
-        # Network connections
-        self.verts = []         # Vertices     :   List of Vertex type objects
-        self.surfs = []         # Surfaces     :   List of Surface type objects
-        self.edges = []         # Edges        :   List of Edge type objects
-        self.load_ndxs = []     # Load indices :   Holds the object indices for when the system is loaded back in
-
         # Calculated traits
-        self.cell_vol = 0       # Cell Volume  :   Volume of the voronoi cell for the atom
-        self.box = []           # Box          :   The grid location of the atom
+
 
 
 def get_radius(radius, system, return_symbol=False):
