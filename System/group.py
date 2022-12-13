011a194a65a56da3a9394495f5719485387646ed
jacke <jackericson98@gmail.com>
2022-12-12 21:26:07 -0500

    Added bff to init, added ndx names, atom names, res names, mol names. Added the ndx_search feature to the surface adding section of get_info and skipped repeats. Instantiated the atoms list in add sele

--- System/group.py
+++ System/group.py
@@ -1,8 +1,9 @@
+from System.calcs import *
 
 
 class Group:
     """Group class. Used to hold selections of atoms and do analysis on it"""
-    def __init__(self, net, atoms=None, name=None, mols=None, residues=None, my_atoms=None, ndxs=None):
+    def __init__(self, net, atoms=None, name=None, mols=None, residues=None, my_atoms=None, ndxs=None, bff=None):
 
         self.net = net                 # Network            :    Network of the System
         self.atoms = atoms             # Atoms              :    List of Atom type objects for the edge
@@ -10,16 +11,23 @@ class Group:
         self.select_strs = []          # Previous String    :    String from the last group of atoms selected
         self.name = name               # Name               :    Name of the group
 
+        self.surfs = None              # Surfaces           :    All surfaces associated with the group
+        self.surf_ndxs = None          # Surface indices    :    Atom indices of the surfaces associated with the group
         self.body_surfs = None         # Body surfaces      :    The surfaces on the outside of the body
         self.body_sa = None            # Body surface area  :    The surface area of the outer surfaces of the body
         self.body_vol = None           # Body volume        :    The volume of the group's atom's cells
         self.outer_body_atoms = None   # Outer body atoms   :    Body atoms that create outer surfaces
         self.surr_body_atoms = None    # Surrounding atoms  :    Atoms not in the group that create surfaces with it
 
-        self.bff = None                # BFF                :    Other group used for comparison
+        self.bff = bff                # BFF                :    Other group used for comparison
         self.iface_surfs = None        # Interface surfaces :    Surfaces that make the interface
         self.iface_atoms = None        # Interface atoms    :    Atoms in the group in the interface
         self.iface_sa = None           # Surface area       :    Surface area of the interface
+
+        self.mol_names = None          # Molecule Names     :    Names of the molecules added to the group
+        self.res_names = None          # Residue Names      :    Names of the residues added to the group
+        self.atom_names = None         # Atom Names         :    Names of the atoms added to the group
+        self.ndx_names = None          # Index Names        :    Names of the indices added to the group
 
         # Check if the group gets initialized with string versions of common identifiers
         if mols is not None:
@@ -31,15 +39,13 @@ class Group:
         if ndxs is not None:
             self.get_ndx_atoms(ndxs)
 
-
-
     # Get information method. Gathers the information for the group(s) selected
     def get_info(self):
         # Set the name of the group
         if self.name is None:
             self.set_name()
         # Reset the main information variables
-        self.body_surfs, self.outer_body_atoms, self.surr_body_atoms = [], [], []
+        self.surfs, self.surf_ndxs, self.body_surfs, self.outer_body_atoms, self.surr_body_atoms = [], [], [], [], []
         self.body_vol, self.body_sa = 0, 0
         # Go through the atom in the group
         for atom in self.atoms:
@@ -47,6 +53,14 @@ class Group:
             self.body_vol += atom.cell_vol
             # Check the surfaces of each of the atoms to see if they are on the outside or not
             for surf in atom.surfs:
+                # Get the index of the surface
+                my_surf_ndx = ndx_search(self.surf_ndxs, surf.ndx)
+                # Check to see if we have found this surface before
+                if my_surf_ndx < len(self.surf_ndxs) and self.surf_ndxs[my_surf_ndx] == surf.ndx:
+                    continue
+                else:
+                    self.surfs.insert(my_surf_ndx, surf)
+                    self.surf_ndxs.insert(my_surf_ndx, surf.ndx)
                 # Check if the surface's first atom is in the group's list of atoms and the second atom is not
                 if surf.atoms[0] in self.atoms and surf.atoms[1] not in self.atoms:
                     # Add the surface's body atom to the outer body atoms list if it isn't already in it
@@ -99,6 +113,8 @@ class Group:
 
     # Add selection method. Adds a new selection to the group
     def add_sele(self, new_sele, new_str):
+        if self.atoms is None:
+            self.atoms = []
         self.selects.append(new_sele)
         self.select_strs.append(new_str)
         self.atoms += new_sele
@@ -113,14 +129,12 @@ class Group:
     # Get group function. Interprets the strings from below
     def get_mol_atoms(self, mols):
         # Get the molecules from their names
-        mol_atoms = []
         for mol in mols:
             self.atoms += self.net.sys.mols[self.net.sys.mol_names.index(mol)]
 
     # Get group function. Interprets the strings from below
     def get_res_atoms(self, residues):
         # Get the molecules from their names
-        res_atoms = []
         for res in residues:
             self.atoms += self.net.sys.residues[self.net.sys.res_names.index(res)]
 
