f51e6ca60c57de3b01bb9babe8e22594cd0a3a4d
jacke <jackericson98@gmail.com>
2022-12-14 11:37:25 -0500

    Moved from previous directory and no longer exist

--- vorpy_gui.py
+++
@@ -1,6 +0,0 @@
-from Visualize.customtkinter_gui import Vorpy
-import os
-
-if __name__ == "__main__":
-    app = Vorpy()
-    app.mainloop()
--- vorpy_gui1.py
+++
@@ -1,3 +0,0 @@
-from Visualize.tkinter_gui import Vorpy
-
-Vorpy()
