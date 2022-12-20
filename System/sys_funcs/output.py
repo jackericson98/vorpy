d5983f479204b247b03bf53f14fadab5e5d70ede
jacke <jackericson98@gmail.com>
2022-12-20 15:14:24 -0500

    Added export csv net. This will work as the function for exporting csv nets. same as export net, just comma delimited

--- System/sys_funcs/output.py
+++ System/sys_funcs/output.py
@@ -369,6 +369,116 @@ def export_net(net, point_res=None):
     file.close()
 
 
+def export_csv_net(net, point_res=None):
+    # Move to the output directory
+    os.chdir(net.sys.dir)
+    # Create the network file
+    file = open(net.sys.name + "_network.txt", 'w')
+    # Write the general information about the system
+    file.write("NETW, " + str(net.surf_res) + ", " + str(net.max_vert) + ", " + str(net.box_size) + ", " + str(net.my_time)
+               + ", " + str(net.cpu_time) + ", " + str(net.sol_verts) + ", " + str(net.curved_faces) + ", " +
+               str(net.flat_faces) + ", " + str(len(net.verts)) + ", " + str(len(net.edges)) + ", " +
+               str(len(net.surfs)) + "\n")
+    # Set the resolution of the network's output points
+    if point_res is None:
+        point_res = 3
+
+    # Write Objects:
+
+    # Write atoms
+    # Go through the atoms in the network
+    for atom in net.atoms:
+        # Get the atom's box
+        box = [str(_) for _ in atom.box]
+        # Write atoms information: index, box, cell volume
+        file.write("ATOM, " + ", " + str(net.atoms.index(atom)) + ", " + ', '.join(box) + ", " + str(atom.cell_vol) + "\n")
+        # Get the vertex, edge and surface index information
+        vert_ndxs = [str(net.verts.index(vert)) for vert in atom.verts]
+        edge_ndxs = [str(net.edges.index(edge)) for edge in atom.edges]
+        surf_ndxs = [str(net.surfs.index(surf)) for surf in atom.surfs]
+        # Write the object indices
+        file.write("ACON, " + ", ".join(vert_ndxs) + "\n")
+        file.write("ACON, " + ", ".join(edge_ndxs) + "\n")
+        file.write("ACON, " + ", ".join(surf_ndxs) + "\n")
+    # Write a separating line
+    file.write("\n")
+
+    # Write vertices
+    for vert in net.verts:
+        # Get the normal information
+        loc, ndx = [str(round(_, point_res)) for _ in vert.loc], [str(_) for _ in vert.ndx]
+        # Get the doublet information
+        loc2, rad2 = [""], ""
+        if vert.doublet:
+            loc2, rad2 = [str(round(_, point_res)) for _ in vert.loc2], str(round(vert.rad2, point_res))
+        # Write the vertex information
+        file.write("VERT, " + str(net.verts.index(vert)) + ", " + ", ".join(ndx) + ", " + ", ".join(loc) + ", " +
+                   str(vert.rad) + ", " + str(vert.doublet) + ", " + ", ".join(loc2) + ", " + rad2 + '\n')
+        # Get the edge and surface index information
+        edge_ndxs = [str(net.edges.index(edge)) for edge in vert.edges]
+        surf_ndxs = [str(net.surfs.index(surf)) for surf in vert.surfs]
+        # Write the connection information
+        file.write("VCON, " + ", ".join(edge_ndxs) + "\n")
+        file.write("VCON, " + ", ".join(surf_ndxs) + "\n")
+    # Write a separating line
+    file.write("\n")
+
+    # Write edges
+    for edge in net.edges:
+        # If the edge location is None get a location
+        if edge.loc is None:
+            edge.loc, edge.rad = calc_circ(edge.atoms)
+        # Get the atom's box
+        ndx, loc = [str(_) for _ in edge.ndx], [str(round(_, point_res)) for _ in edge.loc]
+        # Make sure the points are interpretable
+        if edge.pv1 is None:
+            edge.pv0, edge.pv1 = [np.inf, np.inf, np.inf], [np.inf, np.inf, np.inf]
+        rad, pv0, pv1 = str(round(edge.rad, point_res)) + " ", [str(round(_, point_res)) for _ in edge.pv0], \
+                        [str(round(_, point_res)) for _ in edge.pv1]
+        # Write Edge information: index, location, radius, end points
+        file.write("EDGE, " + str(net.edges.index(edge)) + ", " + ", ".join(ndx) + ", " + ", ".join(loc) + ", " + rad +
+                   ", ".join(pv0) + ", " + ", ".join(pv1) + ", " + str(edge.doublet) + "\n")
+        # Get the vertex, edge and surface index information
+        vert_ndxs = [str(net.verts.index(vert)) for vert in edge.verts]
+        surf_ndxs = [str(net.surfs.index(surf)) for surf in edge.surfs]
+        # Write the object indices
+        file.write("ECON, " + ", ".join(vert_ndxs) + "\n")
+        file.write("ECON, " + ", ".join(surf_ndxs) + "\n")
+        # Check to make sure the edge has points
+        if edge.points is None:
+            edge.build()
+        # Go through the points along the edge
+        for point in edge.points:
+            # Add the points of the edge to the edge file
+            file.write("EPNT, " + str(round(point[0], point_res)) + ", " + str(round(point[1], point_res)) + ", " +
+                       str(round(point[2], point_res)) + "\n")
+    # Write a separating line
+    file.write("\n")
+
+    # Write surfaces
+    for surf in net.surfs:
+        # Write the main edge information
+        ndx, rn = [str(_) for _ in surf.ndx], [str(_) for _ in surf.rn]
+        file.write("SURF, " + str(net.surfs.index(surf)) + ", " + ", ".join(ndx) + ", " + ", ".join(rn) + ", " +
+                   str(surf.sa) + ", " + str(len(surf.perimeter)) + '\n')
+        # Get the vertex, edge and surface index information
+        vert_ndxs = [str(net.verts.index(vert)) for vert in surf.verts]
+        edge_ndxs = [str(net.edges.index(edge)) for edge in surf.edges]
+        # Write the object indices
+        file.write("SCON, " + ", ".join(vert_ndxs) + "\n")
+        file.write("SCON, " + ", ".join(edge_ndxs) + "\n")
+        # Go through the points along the perimeter of the surface
+        for i in range(len(surf.points)):
+            # Add the points of the edge to the edge file
+            file.write("SPNT, " + ", ".join([str(round(_, point_res)) for _ in surf.points[i]]) + "\n")
+        # Go through the triangles in the surface's list of triangles
+        for i in range(len(surf.tris)):
+            # Add the triangles to the list of surface triangles
+            file.write("STRI, " + ", ".join([str(_) for _ in surf.tris[i]]) + "\n")
+    file.close()
+
+
+
 ############################################ Pymol Scripts #############################################################
 
 
