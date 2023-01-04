543ff5fe47ae5f079788c2af5be1968d440731eb
jacke <jackericson98@gmail.com>
2023-01-03 22:38:09 -0500

    Changed the input variable to system from network for group object and added dir, layer_atoms, layer_surfs. Fixed get_layers method (was find_sol_layers)

--- System/sys_objs/group.py
+++ System/sys_objs/group.py
@@ -1,15 +1,16 @@
-from System.sys_funcs.calcs import *
+from System.sys_funcs.output import *
 
 
 class Group:
     """Group class. Used to hold selections of atoms and do analysis on it"""
-    def __init__(self, net, atoms=None, name=None, mols=None, residues=None, my_atoms=None, ndxs=None, bff=None):
+    def __init__(self, sys, atoms=None, name=None, mols=None, residues=None, my_atoms=None, ndxs=None, bff=None):
 
-        self.net = net                 # Network            :    Network of the System
+        self.sys = sys                 # Network            :    Network of the System
         self.atoms = atoms             # Atoms              :    List of Atom type objects for the edge
         self.selects = []              # Previous Selection :    List of the previously selected atoms
         self.select_strs = []          # Previous String    :    String from the last group of atoms selected
         self.name = name               # Name               :    Name of the group
+        self.dir = None                # Directory          :    Directory holding the group export info
 
         self.surfs = None              # Surfaces           :    All surfaces associated with the group
         self.surf_ndxs = None          # Surface indices    :    Atom indices of the surfaces associated with the group
@@ -18,8 +19,10 @@ class Group:
         self.body_vol = None           # Body volume        :    The volume of the group's atom's cells
         self.outer_body_atoms = None   # Outer body atoms   :    Body atoms that create outer surfaces
         self.surr_body_atoms = None    # Surrounding atoms  :    Atoms not in the group that create surfaces with it
+        self.layer_atoms = None        # Layer atoms        :    List of lists of atoms corresponding to layers
+        self.layer_surfs = None        # Layer Surfaces     :    List of lists of surfaces corresponding to layers
 
-        self.bff = bff                # BFF                :    Other group used for comparison
+        self.bff = bff                 # BFF                :    Other group used for comparison
         self.iface_surfs = None        # Interface surfaces :    Surfaces that make the interface
         self.iface_atoms = None        # Interface atoms    :    Atoms in the group in the interface
         self.iface_sa = None           # Surface area       :    Surface area of the interface
@@ -119,9 +122,11 @@ class Group:
                         self.iface_sa += surf.sa
             # Set the bff's surface area
             self.bff.iface_sa = self.iface_sa
+        # Get the layers
+        self.get_layers()
 
     def set_name(self):
-        self.name = self.net.sys.name + "_" + "_".join(self.select_strs) + "group"
+        self.name = self.sys.name + "_" + "_".join(self.select_strs) + "group"
 
     # Add selection method. Adds a new selection to the group
     def add_sele(self, new_sele, new_str):
@@ -142,7 +147,7 @@ class Group:
     def get_mol_atoms(self, mols):
         # Get the molecules from their names
         for mol in mols:
-            mol_atoms = self.net.sys.mols[self.net.sys.mol_names.index(int(mol))]
+            mol_atoms = self.sys.mols[self.sys.mol_names.index(int(mol))]
             for atom in mol_atoms:
                 self.atoms.append(atom)
 
@@ -150,7 +155,7 @@ class Group:
     def get_res_atoms(self, residues):
         # Get the molecules from their names
         for res in residues:
-            res_atoms = self.net.sys.residues[self.net.sys.res_names.index(int(res))]
+            res_atoms = self.sys.residues[self.sys.res_names.index(int(res))]
             for atom in res_atoms:
                 self.atoms.append(atom)
 
@@ -158,57 +163,83 @@ class Group:
     def get_atom_atoms(self, atoms):
         # Get the molecules from their names
         for atom in atoms:
-            self.atoms.append(self.net.sys.atoms[int(atom)])
+            self.atoms.append(self.sys.atoms[int(atom)])
 
     # Get group function. Interprets the strings from below
     def get_ndx_atoms(self, ndxs):
         # Get the molecules from their names
         for ndx in ndxs:
-            ndx_atoms = self.net.sys.ndxs[self.net.sys.ndx_names.index(ndx)]
+            ndx_atoms = self.sys.ndxs[self.sys.ndx_names.index(ndx)]
             for atom in ndx_atoms:
                 self.atoms.append(atom)
 
-    # Get Sol
-    def find_sol_layers(self, mol=None):
-        # We want a function that creates full solute layers
-        # This variable holds the atoms for their respective layers
-        layers_atoms = []
-        # This variable holds the surfaces associated with the layers
-        layers = []
-        # Holds the current set of atoms being surrounded
-        current_layer, next_layer = None, None
-        sol_atoms = self.net.sys.sol.copy()
-        if sol_atoms is None:
-            return
-        i = 0
-        # Keep adding layers until the sol atoms are gone
-        while len(sol_atoms) > 0 and i < 10:
-            # Set the current layer to what was the next layer
-            current_layer, next_layer = next_layer, []
-            # If the current layer is none, this is the first layer and we are calculating the molecule's sol layer
-            if current_layer is None:
-                # If no molecule is provided go off of the system's molecule list
-                if mol is None:
-                    current_layer = []
-                    for my_mol in self.net.sys.mols:
-                        if self.net.sys.mol_names[self.net.sys.mols.index(my_mol)].lower() == 'sol':
-                            continue
-                        current_layer += my_mol
-                else:
-                    current_layer = mol
-            # Set up the shell storage for the surfaces
-            shell = []
-            # Check each surface to see if it is in the current layer
-            for surf in self.surfs:
-                if surf.atoms[0] in current_layer and surf.atoms[1] in sol_atoms:
-                    my_atom = sol_atoms.pop(sol_atoms.index(surf.atoms[1]))
-                    next_layer.append(my_atom)
-                    shell.append(surf)
-            # Add the correct variables
-            layers.append(shell)
-            layers_atoms.append(current_layer)
 
-            i += 1
-        # Add the last layer of atoms
-        layers.append(next_layer)
-        return layers, layers_atoms
+    def get_layers(self, max_layers=50):
+        # Make sure that the group has atoms
+        if self.atoms is None:
+            return
+        # Set up the layer surfs and layer atoms list variables
+        counter, self.layer_atoms, layer_atoms_ndxs, self.layer_surfs = 0, [self.atoms, []], [[self.sys.atoms.index(_) for _ in self.atoms], []], [[]]
+        # Set up the loop to keep adding layers
+        while counter < max_layers:
+            # Go through the atoms in the last layer
+            for atom in self.layer_atoms[-2]:
+                # Go through the surfaces in the atom's list of surfaces
+                for surf in atom.surfs:
+                    if surf in self.layer_surfs[-1] or (len(self.layer_surfs) >= 2 and surf in self.layer_surfs[-2]):
+                        continue
+                    elif surf.ndx[0] in layer_atoms_ndxs[-2] and surf.ndx[1] in layer_atoms_ndxs[-2]:
+                        continue
+                    self.layer_surfs[-1].append(surf)
+                    if surf.ndx[0] in layer_atoms_ndxs[-2] and surf.ndx[1] not in layer_atoms_ndxs[-2]:
+                        self.layer_atoms[-1].append(self.sys.atoms[surf.ndx[1]])
+                        layer_atoms_ndxs[-1].append(surf.ndx[1])
+
+                    if surf.ndx[1] in layer_atoms_ndxs[-2] and surf.ndx[0] not in layer_atoms_ndxs[-2]:
+                        self.layer_atoms[-1].append(self.sys.atoms[surf.ndx[0]])
+                        layer_atoms_ndxs[-1].append(surf.ndx[0])
+
+            # If there is nothing to add leave the layers loop
+            if len(self.layer_surfs[-1]) == 0:
+                self.layer_surfs.pop()
+                break
+
+            # Create the new layer lists
+            self.layer_surfs.append([])
+            self.layer_atoms.append([])
+            layer_atoms_ndxs.append([])
+            counter += 1
+
+
+
+    def exports(self, atoms=True, shell=True, fill=True, surfaces=True, layers=True):
+        # Create the output directory inside the system's directory
+        if self.dir is None:
+            self.dir = self.sys.dir + "/" + self.name
+            os.mkdir(self.dir)
+        os.chdir(self.dir)
+        # If the user wants to export the atoms for the group
+        if atoms:
+            write_pdb(atoms=self.atoms, name=self.name)
+        # If the user wants to export the shell for the group
+        if shell:
+            write_surfs(surfs=self.layer_surfs[0], file_name=self.name + "_shell")
+        # If the user wants a filled shell for the group
+        if fill:
+            write_surfs(surfs=self.surfs, file_name=self.name + "_fill")
+        # If the user wants separate surfaces for the group
+        if surfaces:
+            os.mkdir(self.dir + "/surfaces")
+            os.chdir(self.dir + "/surfaces")
+            for surf in self.surfs:
+                write_surfs([surf], file_name="_".join([str(_) for _ in surf.ndx]))
+            os.chdir(self.dir)
+        # If the user wants layers
+        if layers:
+            os.mkdir(os.getcwd() + "/layers")
+            os.chdir(os.getcwd() + "/layers")
+            for i in range(len(self.layer_surfs)):
+                write_pdb(self.layer_atoms[i + 1], name=str(i) + "_atoms", sys=self.sys)
+                write_surfs(self.layer_surfs[i], file_name=str(i) + "_surfs")
+        os.chdir(self.sys.dir)
+
