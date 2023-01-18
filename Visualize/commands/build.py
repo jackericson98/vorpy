30e1889402d398e642ea0ac1d8a6b3acce6ea85d
jacke <jackericson98@gmail.com>
2023-01-18 10:21:51 -0500

    Minor changes

--- Visualize/commands/build.py
+++ Visualize/commands/build.py
@@ -10,13 +10,13 @@ def build(sys):
     """
     # If no system has been loaded tell the user to screw off
     if len(sys.atoms) == 0:
-        print("No atoms in the system. Use the \'load\' command or type \'h\' for help")
+        print("no atoms in the system. use the \'load\' command or type \'h\' for help")
         return
     # Check to see if a network has been added
     if sys.net is None:
         sys.net = Network(sys=sys, atoms=sys.atoms)
     # Once the build command is used, the user is greeted with the build settings and asked if they are ready to build
-    print("Settings - surf_res = {:.2f} \u208B,  max_vert  = {:.2f} \u208B,  box_size = {:.2f} x,  build_surfs = {}, flat_surfs = {}"
+    print("settings - surf_res = {:.2f} \u208B,  max_vert  = {:.2f} \u208B,  box_size = {:.2f} x,  build_surfs = {}, flat_surfs = {}"
           .format(sys.net.surf_res, sys.net.max_vert, sys.net.box_size, sys.net.build_surfs, sys.net.flat_surfs))
     # The user is prompted to start the build - This could say eta and other build qualities
     pre_build_confirmation = input("confirm >>>   ")
@@ -24,7 +24,7 @@ def build(sys):
     if pre_build_confirmation.lower() in ys:
         sys.net.build()
     elif pre_build_confirmation.lower() in ns:
-        print("Use the \'set\' command to change a setting and a value. Type \'h\' for help")
+        print("use the \'set\' command to change a setting and a value or type \'h\' for help")
         return
     elif pre_build_confirmation.lower() in helps:
         help_()
