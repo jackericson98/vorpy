a99183809065ac3cab703dd6e72ecf8cdb2ad0ec
jacke <jackericson98@gmail.com>
2023-01-18 11:17:51 -0500

    Minor changes

--- System/sys_objs/residue.py
+++ System/sys_objs/residue.py
@@ -2,8 +2,18 @@ from System.sys_funcs.calcs import *
 
 
 class Residue:
-    def __init__(self, atoms=None, sys=None, mol=None, sequence=None, seg_id=None):
+    def __init__(self, atoms=None, name=None, sys=None, mol=None, sequence=None, seg_id=None):
+        """
+        Residue Object for holding specific residue information
+        :param atoms:
+        :param name:
+        :param sys:
+        :param mol:
+        :param sequence:
+        :param seg_id:
+        """
         self.atoms = atoms
+        self.name = name
         self.sys = sys
         self.mol = mol
         self.seq = sequence
