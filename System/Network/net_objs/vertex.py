0fb1ff7220c82c1e47785990a5e13d84860e0761
jacke <jackericson98@gmail.com>
2023-01-23 23:14:42 -0500

    Sped up the vertex finding process by a bunch

--- System/Network/net_objs/vertex.py
+++ System/Network/net_objs/vertex.py
@@ -37,39 +37,67 @@ class Vertex:
         # The real location and radius of the base sphere
         locs = np.array(self.atoms[0].loc), np.array(self.atoms[1].loc), np.array(self.atoms[2].loc), \
                np.array(self.atoms[3].loc)
-        R1, R2, R3, R4 = self.atoms[0].rad, self.atoms[1].rad, self.atoms[2].rad, self.atoms[3].rad
-
+        r0, R2, R3, R4 = self.atoms[0].rad, self.atoms[1].rad, self.atoms[2].rad, self.atoms[3].rad
+        r0_2 = r0 ** 2
         # Find the recalculated location of the atoms
         l0, l1, l2, l3 = locs[0], locs[1] - locs[0], locs[2] - locs[0], locs[3] - locs[0]
         # Calculate our System of linear equations coefficients
-        a1, b1, c1, d1, f1 = 2 * l1[0], 2 * l1[1], 2 * l1[2], 2 * (R2 - R1), R1 ** 2 - R2 ** 2 + l1[0] ** 2 + l1[
+        a1, b1, c1, d1, f1 = 2 * l1[0], 2 * l1[1], 2 * l1[2], 2 * (R2 - r0), r0_2 - R2 ** 2 + l1[0] ** 2 + l1[
             1] ** 2 + l1[2] ** 2
-        a2, b2, c2, d2, f2 = 2 * l2[0], 2 * l2[1], 2 * l2[2], 2 * (R3 - R1), R1 ** 2 - R3 ** 2 + l2[0] ** 2 + l2[
+        a2, b2, c2, d2, f2 = 2 * l2[0], 2 * l2[1], 2 * l2[2], 2 * (R3 - r0), r0_2 - R3 ** 2 + l2[0] ** 2 + l2[
             1] ** 2 + l2[2] ** 2
-        a3, b3, c3, d3, f3 = 2 * l3[0], 2 * l3[1], 2 * l3[2], 2 * (R4 - R1), R1 ** 2 - R4 ** 2 + l3[0] ** 2 + l3[
+        a3, b3, c3, d3, f3 = 2 * l3[0], 2 * l3[1], 2 * l3[2], 2 * (R4 - r0), r0_2 - R4 ** 2 + l3[0] ** 2 + l3[
             1] ** 2 + l3[2] ** 2
-        A, B, C, d, f = [a1, a2, a3], [b1, b2, b3], [c1, c2, c3], [d1, d2, d3], [f1, f2, f2]
-        # Calculate the ranks of the matrices
-        ABC_rank = np.linalg.matrix_rank(np.array([A, B, C]))
-        m_rank = np.linalg.matrix_rank(np.array([A, B, C, d]))
-        f_rank = np.linalg.matrix_rank(np.array([A, B, C, d, f]))
         # Calculate the F values
         F = a1 * b2 * c3 - a1 * b3 * c2 - a2 * b1 * c3 + a2 * b3 * c1 + a3 * b1 * c2 - a3 * b2 * c1
+        F_2 = F ** 2
         F10 = b1 * c2 * f3 - b1 * c3 * f2 - b2 * c1 * f3 + b2 * c3 * f1 + b3 * c1 * f2 - b3 * c2 * f1
         F11 = -b1 * c2 * d3 + b1 * c3 * d2 + b2 * c1 * d3 - b2 * c3 * d1 - b3 * c1 * d2 + b3 * c2 * d1
         F20 = -a1 * c2 * f3 + a1 * c3 * f2 + a2 * c1 * f3 - a2 * c3 * f1 - a3 * c1 * f2 + a3 * c2 * f1
         F21 = a1 * c2 * d3 - a1 * c3 * d2 - a2 * c1 * d3 + a2 * c3 * d1 + a3 * c1 * d2 - a3 * c2 * d1
         F30 = a1 * b2 * f3 - a1 * b3 * f2 - a2 * b1 * f3 + a2 * b3 * f1 + a3 * b1 * f2 - a3 * b2 * f1
         F31 = -a1 * b2 * d3 + a1 * b3 * d2 + a2 * b1 * d3 - a2 * b3 * d1 - a3 * b1 * d2 + a3 * b2 * d1
+        # Calculate the ranks of the matrices
+        m_rank, f_rank = 3, 3
+        if F == 0:
+            my_mtx = [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3], [d1, d2, d3], [f1, f2, f3]]
+            m_rank = np.linalg.matrix_rank(np.array(my_mtx[:-1]))
+            if m_rank != 3:
+                f_rank = np.linalg.matrix_rank(np.array(my_mtx))
+        # # The real location and radius of the base sphere
+        # l0 = np.array(self.atoms[0].loc)
+        # locs = np.array([self.atoms[0].loc] + [np.array(_.loc) - l0 for _ in self.atoms[1:]])
+        # # Get the radii
+        # r0 = self.atoms[0].rad
+        # rads = np.array([self.atoms[0].rad, self.atoms[1].rad, self.atoms[2].rad, self.atoms[3].rad])
+        # # Calculate our System of linear equations coefficients
+        # as_ = 2 * locs[1:, 0]
+        # bs = 2 * locs[1:, 1]
+        # cs = 2 * locs[1:, 2]
+        # ds = 2 * np.array([_ - r0 for _ in rads[1:]])
+        # fs = [r0 ** 2 - rads[i] ** 2 + sum([_ ** 2 for _ in locs[i]]) for i in range(1, 4)]
+        # # Calculate the ranks of the matrices
+        # m_rank = np.linalg.matrix_rank([as_, bs, cs, ds])
+        # f_rank = np.linalg.matrix_rank([as_, bs, cs, ds, fs])
+        #
+        # F = np.linalg.det(np.array([as_, bs, cs]))
+        # F_2 = F ** 2
+        # F10 = np.linalg.det(np.array([fs, bs, cs]))
+        # F11 = np.linalg.det(np.array([-ds, bs, cs]))
+        # F20 = np.linalg.det(np.array([as_, fs, cs]))
+        # F21 = np.linalg.det(np.array([as_, -ds, cs]))
+        # F30 = np.linalg.det(np.array([as_, bs, fs]))
+        # F31 = np.linalg.det(np.array([as_, bs, -ds]))
+
+
         verts = []
         xs, ys, zs, Rs = [], [], [], []
-
         # Case 1:
-        if ABC_rank == 3 and m_rank == 3 and f_rank == 3:
+        if F != 0 and m_rank == 3 and f_rank == 3:
             # Calculate the radius polynomial coefficients
-            a = ((F11 ** 2 + F21 ** 2 + F31 ** 2) / F ** 2) - 1
-            b = (2 * (F10 * F11 + F20 * F21 + F30 * F31) / F ** 2) - 2 * R1
-            c = ((F10 ** 2 + F20 ** 2 + F30 ** 2) / F ** 2) - R1 ** 2
+            a = ((F11 ** 2 + F21 ** 2 + F31 ** 2) / F_2) - 1
+            b = 2 * (((F10 * F11 + F20 * F21 + F30 * F31) / F_2) - r0)
+            c = ((F10 ** 2 + F20 ** 2 + F30 ** 2) / F_2) - r0 ** 2
             # If the discriminant is positive, find the real positive roots of the quadratic
             if round(-4 * a * c + b ** 2, 10) >= 0:
                 Rs = [R for R in np.roots([a, b, c]) if np.isreal(R)]
@@ -81,11 +109,11 @@ class Vertex:
                 # Move the vertex back to the actual location of the atoms
                 verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
         # Case 2:
-        elif ABC_rank == 2 and m_rank == 3 and f_rank == 3 and F > 0:
+        elif np.linalg.matrix_rank([[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]]) == 2 and m_rank == 3 and f_rank == 3 and F > 0:
             # Calculate the _ polynomial coefficients
-            a = F ** 2 + F11 ** 2 + F21 ** 2 - F31 ** 2
-            b = 2 * (F10 * F11 + F20 * F21 - F30 * F31 - F * F31 * R1)
-            c = F10 ** 2 + F20 ** 2 - (F30 + F * R1)
+            a = F_2 + F11 ** 2 + F21 ** 2 - F31 ** 2
+            b = 2 * (F10 * F11 + F20 * F21 - F30 * F31 - F * F31 * r0)
+            c = F10 ** 2 + F20 ** 2 - (F30 + F * r0)
             roots, verts = [], []
             # Check the discriminant
             disc = -4 * a * c + b ** 2
@@ -93,21 +121,21 @@ class Vertex:
                 roots = [root for root in np.roots([a, b, c]) if np.isreal(root)]
             # Case 2 subcases:
             # Case 2.1
-            if np.linalg.matrix_rank([A, B, d]) == 3:
+            if F31 != 0:
                 # Go through each radius and calculate the vertex
                 for z in roots:
                     x, y, R = F10 / F + z * F11 / F, F20 / F + z * F21 / F, F30 / F + z * F31 / F
                     # Move the vertex back to the actual location of the atoms
                     verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
             # Case 2.2
-            elif np.linalg.matrix_rank([A, d, C]) == 3:
+            elif F21 != 0:
                 # Go through each radius and calculate the vertex
                 for y in roots:
                     x, R, z = F10 / F + y * F11 / F, F20 / F + y * F21 / F, F30 / F + y * F31 / F
                     # Move the vertex back to the actual location of the atoms
                     verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
             # Case 2.3
-            elif np.linalg.matrix_rank([d, B, C]):
+            elif F11 != 0:
                 # Go through each radius and calculate the vertex
                 for x in roots:
                     R, y, z = F10 / F + x * F11 / F, F20 / F + x * F21 / F, F30 / F + x * F31 / F
