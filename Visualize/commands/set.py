1596a67a0751e58762e0ef69c2af85b8c13efc0b
jacke <jackericson98@gmail.com>
2023-02-21 18:15:47 -0500

    Fixed the sett function

--- Visualize/commands/set.py
+++ Visualize/commands/set.py
@@ -2,15 +2,19 @@ from Visualize.commands.interpret import *
 from System.Network.network import Network
 
 
-def sett(sys, usr_npt):
+def sett(sys, usr_npt, vorpy2_set=False):
     """
     Set the network parameters
+    :param vorpy2_set:
     :param sys: System object holding all values
     :param usr_npt:
     :return:
     """
+    # First filter out the set command if given
+    if usr_npt[0].lower() in set_cmds:
+        usr_npt.pop(0)
     # If the user only enters "set" ask them to enter a setting and a value
-    if len(usr_npt) == 1:
+    if len(usr_npt) == 0:
         # Get the setting. This has the value back up built in
         my_set = get_set()
         if my_set is None:
@@ -20,24 +24,19 @@ def sett(sys, usr_npt):
         else:
             my_val = get_val(setting=my_set[0])
     # If the user enters a setting, but no value get the value
-    elif len(usr_npt) == 2:
+    elif len(usr_npt) == 1:
         # Make sure the setting is correct
-        my_set = get_set(usr_npt[1])
+        my_set = get_set(usr_npt[0])
         # If None is returned, the user wants to quit, and we'll oblige
         if my_set is None:
             return
-        # If the user provided the value as well as the setting check the value
-        if len(my_set) == 2:
-            my_val = get_val(setting=my_set[0], val=my_set[1])
-        # Otherwise, make them get the value
-        else:
-            my_val = get_val(setting=my_set[0])
+        my_val = get_val(setting=my_set)
     # If the user enters a setting and a value
-    elif len(usr_npt) <= 3:
-        my_set = get_set(usr_npt[1])
+    elif len(usr_npt) <= 2:
+        my_set = get_set(usr_npt[0])
         if my_set is None:
             return
-        my_val = get_val(my_set, usr_npt[2])
+        my_val = get_val(my_set, usr_npt[1])
     else:
         invalid_input(usr_npt)
         return
@@ -50,7 +49,8 @@ def sett(sys, usr_npt):
         # Check to see if the value is correct
         try:
             sys.net.surf_res = float(my_val)
-            print(u"surface resolution set to {} \u212B".format(my_val))
+            if not vorpy2_set:
+                print(u"surface resolution set to {} \u212B".format(my_val))
         except ValueError:
             print("\"{}\" is an invalid input for the surface resolution setting. Enter a float value (From 0.01 to 1 A, recommended 0.1 A)".format(my_val))
     # Set the maximum vertex radius
@@ -58,7 +58,8 @@ def sett(sys, usr_npt):
         # Check to see if the value is correct
         try:
             sys.net.max_vert = float(my_val)
-            print(u"maximum vertex radius set to {} \u212B".format(my_val))
+            if not vorpy2_set:
+                print(u"maximum vertex radius set to {} \u212B".format(my_val))
         except ValueError:
             print("\"{}\" is an invalid input for the maximum vertex radius setting. Enter a float value (From 0.10 to 20 A, recommended 7 A)".format(my_val))
     # Set the box multiplier
@@ -66,14 +67,16 @@ def sett(sys, usr_npt):
         # Check to see if the value is correct
         try:
             sys.net.box_size = float(my_val)
-            print("box size multiplier set to {} x".format(my_val))
+            if not vorpy2_set:
+                print("box size multiplier set to {} x".format(my_val))
         except ValueError:
             print("\"{}\" is an invalid input for the box size multiplier setting. Enter a float value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     # Set the solute vertices
     elif my_set in build_surfses:
         try:
             sys.net.build_surfs = bool(my_val)
-            print("build surfaces set to {}".format(sys.net.build_surfs))
+            if not vorpy2_set:
+                print("build surfaces set to {}".format(sys.net.build_surfs))
         except ValueError:
             print("\"{}\" is an invalid input for the build surfaces setting. Enter a True/False value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     # Set the flat surfaces
@@ -81,7 +84,8 @@ def sett(sys, usr_npt):
         # Check to see if the value is correct
         try:
             sys.net.flat_surfs = bool(my_val)
-            print("flat surfaces set to {}".format(sys.net.flat_surfs))
+            if not vorpy2_set:
+                print("flat surfaces set to {}".format(sys.net.flat_surfs))
         except ValueError:
             print("\"{}\" is an invalid input for the flat surfaces setting. Enter a True/False value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     # Check for a quit request
