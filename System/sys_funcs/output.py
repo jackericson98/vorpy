53ca2ba3909a48bce98eaebe84ee84d7dac77c37
jacke <jackericson98@gmail.com>
2023-02-28 14:57:54 -0500

    Added .off file option to write verts, added write edges, fixed colors issue in write surfs

--- System/sys_funcs/output.py
+++ System/sys_funcs/output.py
@@ -1,5 +1,6 @@
 from System.sys_funcs.calcs import *
 from System.sys_objs.atom import Atom, get_radius
+from System.sys_funcs.draw import draw_edge, draw_vert
 import os
 import csv
 
@@ -88,7 +89,7 @@ def write_pdb(atoms, name, sys=None, directory=None):
                 ser_num = " " * (5 - len(str(i+1))) + str(i + 1)
                 name = a.name + " " * (4 - len(a.name))
                 res = " " * (3 - len(a.mol_class)) + a.mol_class
-                chain = str(a.mol) + " " * (1 - len(a.mol))
+                chain = str(a.chain) + " " * (1 - len(a.chain))
                 if chain == "ZZ" or chain == 'MOL':
                     chain = "  "
                 res_seq = " " * (3 - len(a.res_seq)) + a.res_seq
@@ -105,7 +106,7 @@ def write_pdb(atoms, name, sys=None, directory=None):
         os.chdir(start_dir)
 
 
-def write_verts(verts, file_name, atom_type=None, directory=None):
+def write_verts(verts, file_name, atom_type=None, directory=None, pdb=False, spheres=False, color=None):
     """
     Creates a pdb file for vertex representation
     :param verts:
@@ -114,6 +115,9 @@ def write_verts(verts, file_name, atom_type=None, directory=None):
     :param directory:
     :return:
     """
+    # If no color is given, make the color random
+    if color is None:
+        color = [1, 0, 0]
     # Check to see if a directory is given
     if directory is not None:
         os.chdir(directory)
@@ -122,9 +126,108 @@ def write_verts(verts, file_name, atom_type=None, directory=None):
     # If no surfaces are provided return
     if verts is None or len(verts) == 0:
         return
-    vert_atoms = [Atom(location=verts[i].loc, element=atom_type, mol_class="SOL", res_seq=str(i), name=atom_type, ) for i in range(len(verts))]
-    # Write the pdb with the atom objects from the verts
-    write_pdb(atoms=vert_atoms, name=file_name, directory=directory)
+    if pdb:
+        vert_atoms = [Atom(location=verts[i].loc, element=atom_type, mol_class="SOL", res_seq=str(i), name=atom_type, ) for i in range(len(verts))]
+        # Write the pdb with the atom objects from the verts
+        write_pdb(atoms=vert_atoms, name=file_name, directory=directory)
+    else:
+
+        num_verts, num_tris = 0, 0
+        for vert in verts:
+            draw_vert(vert, sphere=spheres)
+            if spheres:
+                # Go through and create each edge
+                for i in range(len(verts)):
+                    num_verts += len(verts[i].loc_points) + len(vert.sphere_points)
+                    num_tris += len(verts[i].loc_tris) + len(vert.sphere_tris)
+            else:
+                num_verts = 8 * len(verts)
+                num_tris = 8 * len(verts)
+        # Create the file
+        with open(file_name + ".off", 'w') as file:
+            # Count the number of triangles and vertices there are
+            # Write the numbers into the file
+            file.write("OFF\n" + str(num_verts) + " " + str(num_tris) + " 0\n\n\n")
+            # Go through the surfaces and add the points
+            for i in range(len(verts)):
+                sphere_points = []
+                if spheres:
+                    sphere_points = verts[i].sphere_points
+                # Go through the points on the surface
+                for point in verts[i].loc_points + sphere_points:
+                    # Add the point to the system file and the surface's file (rounded to 4 decimal points)
+                    str_point = [str(round(float(point[_]), 4)) for _ in range(3)]
+                    file.write(str_point[0] + " " + str_point[1] + " " + str_point[2] + '\n')
+
+            num_verts, tri_count = 0, 0
+            # Go through each surface and add the faces
+            for i in range(len(verts)):
+                vert = verts[i]
+                sphere_tris = []
+                if spheres:
+                    sphere_tris = verts[i].sphere_tris
+                tri_list = vert.loc_tris + sphere_tris
+                # Go through the triangles in the surface
+                for j in range(len(tri_list)):
+                    # Get the triangle and colors
+                    tri = tri_list[j]
+                    # Add the triangle to the system file and the surface's file
+                    str_tri = [str(tri[_] + num_verts) for _ in range(3)]
+                    file.write("3 " + str_tri[0] + " " + str_tri[1] + " " + str_tri[2] + " " + str(color[0]) + " " +
+                               str(color[1]) + " " + str(color[2]) + "\n")
+                # Keep counting triangles for the system file
+                if vert.sphere_points is None:
+                    sphere_points_len = 0
+                else:
+                    sphere_points_len = len(vert.sphere_points)
+                num_verts += len(vert.loc_points) + sphere_points_len
+
+
+def write_edges(edges, file_name, color=None, directory=None):
+    # Check to see if a directory is given
+    if directory is not None:
+        os.chdir(directory)
+    # If no surfaces are provided return
+    if edges is None or len(edges) == 0:
+        return
+    # If no color is given, make the color random
+    if color is None:
+        color = [1, 1, 1]
+    # Check that the edge has been drawn
+    for edge in edges:
+        if edge.draw_points is None or edge.draw_tris is None:
+            draw_edge(edge)
+    num_verts, num_tris = 0, 0
+    # Go through and create each edge
+    for i in range(len(edges)):
+        num_verts += len(edges[i].points) * 3
+        num_tris += (len(edges[i].points) - 1) * 6
+    # Create the file
+    with open(file_name + ".off", 'w') as file:
+        # Count the number of triangles and vertices there are
+        # Write the numbers into the file
+        file.write("OFF\n" + str(num_verts) + " " + str(num_tris) + " 0\n\n\n")
+        # Go through the surfaces and add the points
+        for i in range(len(edges)):
+            # Go through the points on the surface
+            for point in edges[i].draw_points:
+                # Add the point to the system file and the surface's file (rounded to 4 decimal points)
+                str_point = [str(round(float(point[_]), 4)) for _ in range(3)]
+                file.write(str_point[0] + " " + str_point[1] + " " + str_point[2] + '\n')
+        num_verts, tri_count = 0, 0
+        # Go through each surface and add the faces
+        for i in range(len(edges)):
+            edge = edges[i]
+            # Go through the triangles in the surface
+            for j in range(len(edge.draw_tris)):
+                # Get the triangle and colors
+                tri = edge.draw_tris[j]
+                # Add the triangle to the system file and the surface's file
+                str_tri = [str(tri[_] + num_verts) for _ in range(3)]
+                file.write("3 " + str_tri[0] + " " + str_tri[1] + " " + str_tri[2] + " " + str(color[0]) + " " +
+                           str(color[1]) + " " + str(color[2]) + "\n")
+            # Keep counting triangles for the system file
+            num_verts += len(edge.draw_points)
 
 
 def write_surfs(surfs, file_name, color=False, directory=None):
@@ -174,10 +277,7 @@ def write_surfs(surfs, file_name, color=False, directory=None):
                 colors = surf.tri_colors
                 if colors is not None:
                     # If the surface is flat, average out the colors
-                    if surf.flat:
-                        color = [[0, 0, 0]]*len(colors)
-                    else:
-                        color = colors[j]
+                    color = colors[j]
                 # Add the triangle to the system file and the surface's file
                 str_tri = [str(tri[_] + num_verts) for _ in range(3)]
                 file.write("3 " + str_tri[0] + " " + str_tri[1] + " " + str_tri[2] + " " + str(color[0]) + " " +
