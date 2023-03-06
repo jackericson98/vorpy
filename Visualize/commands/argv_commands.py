6a50c6a5393d78539fa36f61133f4572db3ff1d7
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2023-03-06 17:38:26 -0500

    Initial commit. Allows the user to bypass the interface

---
+++ Visualize/commands/argv_commands.py
@@ -0,0 +1,33 @@
+from Visualize.commands.load import load
+from Visualize.commands.set import sett
+from Visualize.commands.group import group
+from System.sys_objs.group import Group
+
+
+def interpret_argvs(my_sys):
+    # Load the atom file
+    load(my_sys, ["", my_sys.argv[1]])
+    # Separate the rest of the argv args
+    my_args = my_sys.argv[2:]
+    my_group = None
+    while my_args:
+        arg = my_args.pop(0)
+        if arg.lower() == '-s' and len(my_args) >= 2:
+            setting = my_args.pop(0)
+            value = my_args.pop(0)
+            sett(my_sys, [setting, value], vorpy2_set=False)
+        elif arg.lower() == '-g' and len(my_args) >= 1:
+            grouping = my_args.pop(0)
+            if len(my_args) >= 1 and my_args[0][0] != '-':
+                my_ndx = my_args.pop(0)
+                my_group = group(my_sys, ["", grouping, my_ndx])
+    if my_group is None:
+        my_group = Group(sys=my_sys, mols=my_sys.mols[:-1], name=my_sys.name + "_no_SOL")
+    print(
+        u"{} Build Settings - surf_res = {:.2f} \u208B,  max_vert  = {:.2f} \u208B,  box_multi = {:.2f} x,  build_surfs = {}, "
+        u"surf_type = {}".format(my_group.name, my_sys.net.surf_res, my_sys.net.max_vert, my_sys.net.box_size,
+                                 my_sys.net.build_surfs, 'voronoi' if my_sys.net.type == 'vor' else 'flat'))
+
+    my_sys.net.build(my_group=my_group, build_surfs=True, output=True, print_actions=True)
+
+
