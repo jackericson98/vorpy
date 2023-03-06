69a49988b1e5ccde896a2621354c0d327eb45132
jacke <jackericson98@gmail.com>
2023-03-06 12:05:28 -0500

    Added comment formatting and moved some functions out. Held back print statements without sys permission

--- System/system.py
+++ System/system.py
@@ -1,12 +1,9 @@
-import os.path
-
 from System.sys_funcs.input import *
+from System.sys_funcs.read_net import read_net
+from System.sys_funcs.sort_atoms import sort_atoms
 from System.sys_funcs.output import *
 from Visualize.mpl_visualize import *
-from System.sys_objs.group import Group
-from System.sys_objs.molcule import Molecule
-from System.sys_objs.residue import Residue
-import numpy as np
+from numpy import seterr, random
 
 
 class System:
@@ -35,8 +32,8 @@ class System:
         # Data
         self.net = None                     # Network             :   Network object holding the primary network
         self.atoms = atoms                  # Atoms               :   List holding the atom objects
-        self.user_atoms = atoms             # User Atoms          :   List of user provided locations and radii
-        self.mols = None                    # Molecules           :   List of molecules
+        self.user_atoms = atoms             # User Atoms          :   User provided locations and radii
+        self.mols = None                    # Molecules           :   Molecule objects from the system
         self.residues = None                # Residues            :   List of residues (lists of atoms)
         self.sol = None                     # Solution            :   List of solution molecules (lists of atoms)
         self.sol_name = "None"              # Solute Name         :   Name for the solute from the atoms
@@ -48,31 +45,37 @@ class System:
         # Set up the file attributes
         self.data = None                    # Data                :   Additional data provided by the base file
         self.base_file = file               # Base file           :   Primary file address
-        self.ball_file = None
+        self.ball_file = None               # Ball file           :   Balls used to create vertices from Voronota
         self.vert_file = verts_file         # Vertex file         :   Address to the vertices of the primary system
         self.net_file = network_file        # Network files       :   Network files for multiple frames
         self.ndx_file = index_file          # Index file          :   File addresses for index file in GROMACS format
         self.frame_files = frame_files      # Frame files         :   Files storing atom movements
         self.dir = output_directory         # Output Directory    :   Output directory for the export files
         self.vpy_dir = root_dir             # Vorpy Directory     :   Directory that vorpy is running out of
+        self.max_atom_rad = 0               # Max atom rad        :   Largest radius of the system for reference
+
         # Gui
-        self.gui = gui                     # GUI                 :   GUI Vorpy object that can be updated through sys
+        self.gui = gui                      # GUI                 :   GUI Vorpy object that can be updated through sys
+        self.print_actions = False          # Print actions Bool  :   Tells the system to print or not
 
-        # Initiate the system
-        if self.base_file is not None:
-            self.load_files()
-        np.seterr(divide='ignore', invalid='ignore')
+        # # Initiate the system
+        self.__load_files__()
 
-    def load_files(self):
+        seterr(divide='ignore', invalid='ignore')
+
+    def __load_files__(self):
         """
         Create the system and make sure the files added in __init__ are added to the system
         :return:
         """
+
         # Load the system
         if self.base_file is not None:
             self.load_sys()
         elif self.user_atoms is not None:
             self.load_sys_atoms()
+        else:
+            return
 
         # Load the network
         if self.net_file is not None:
@@ -81,18 +84,9 @@ class System:
         # Load the index file
         if self.ndx_file is not None:
             self.load_ndx()
-        # Instantiate the major variables
-        if self.atoms is None:
-            self.atoms = []
-        if self.residues is None:
-            self.residues = []
-        if self.mols is None:
-            self.mols = []
-        if self.ndxs is None:
-            self.ndxs = []
-        if self.data is None:
-            self.data = []
-        self.name = get_name(self.base_file)
+
+        # Get the name
+        self.name = os.path.basename(self.base_file)[:-4]
 
     def load_sys(self, file=None):
         """
@@ -101,76 +95,90 @@ class System:
         :return:
         """
         # If a file is given read the file and set the system attributes
-        if file is None:
+        if file is not None:
             # Set the file
-            file = self.base_file
-        else:
             self.base_file = file
+
         # Set the name of the system
-        self.name = get_name(file)
+        self.name = path.basename(self.base_file)[:-4]
+
         # Read PDB file
         if self.base_file[-3:] == "pdb":
-            file_data = read_pdb(self)
-            if file_data is not None:
-                self.atoms, self.data = file_data
-            else:
-                return
+            read_pdb(self)
+
         # Read CIF file
         elif self.base_file[-3:] == "cif":
             read_cif(self)
+
         # Read GRO file
         elif self.base_file[-3:] == "gro":
             read_gro(self)
+
         # Read MOL file
         elif self.base_file[-3:] == "mol":
             read_mol(self)
-        else:
-            return
+
         # Sort the atoms
-        self.sort_atoms()
+        sort_atoms(self)
+
+        # If the system wants its actions printed
+        if self.print_actions:
+            print("{} loaded - {} atoms, {} chains, {} residues"
+                  .format(self.name, len(self.atoms), len(self.mols), len(self.residues)))
 
     def load_verts(self, file=None, vta_ball_file=None):
         """
         Loads vorpy specific vertices file from the system level
-        :param vta_ball_file:
-        :param file:
-        :return:
+        :param vta_ball_file: Voronota Ball file, triggers voronota reading of the verts file
+        :param file: Main verts file that could be vorpy generated or voronota generated
+        :return: Sets the vertex values for the network
         """
+        # Check for a loaded vertex file
         if file is not None:
             self.vert_file = file
+
         # Check to see if the network has been created yet or not
         if self.net is None:
             self.net = Network(atoms=self.atoms, sys=self)
+
+        # If a ball file is loaded as well, this is a voronota deal
         if vta_ball_file is None:
             read_verts(self.net, self.vert_file)
         else:
             read_vta_data(self, vert_file=file, ball_file=vta_ball_file)
 
+        # If the system wants its actions printed
+        if self.print_actions:
+            print("{} vertices loaded - {} vertices".format(self.name, len(self.net.verts)))
+
     def load_net(self, file=None):
         """
         Used to load a network that was previously calculated
-        :param file:
+        :param file: Network file for loading
         """
-        # Set the output directory if None has been set yet
-        if self.dir is None:
-            self.set_output_directory()
         # If no file has been loaded before, create the main network
         if file is not None:
             self.net_file = file
-        # Create the network
-        self.net = Network(self, atoms=self.atoms)
-        # read_net(self.net, self.net_file, verts_only=verts_only)
-        read_net(self)
-        print("\rnetwork loaded - {} verts, {} surfs\n".format(len(self.net.verts), len(self.net.surfs)), end="")
+        # Read the network file
+        read_net(self, integrate=self.net is not None)
+
+        # Print if the system requires
+        if self.print_actions:
+            print("\r{} network loaded - {} verts, {} surfs\n"
+                  .format(self.name, len(self.net.verts), len(self.net.surfs)), end="")
 
     def load_ndx(self, file=None):
         """
         Reads GROMACS index files from the system level
-        :return:
+        :return: Creates group objects for the system
         """
+        # Read the ndx file
         read_ndx(self, file=file)
 
-    # Build System method.
+        # If the system wants its actions printed
+        if self.print_actions:
+            print("{} indices loaded - {} indices total".format(self.name, len(self.ndxs)))
+
     def load_sys_atoms(self):
         """
         Takes in a list of atomic values and creates atom objects for the system to interpret
@@ -207,63 +215,9 @@ class System:
         # Create the atoms
         for i in range(anums):
             # Choose a random set of 3 numbers between dmax and -dmax. Choose a random radius between 0 and rmax
-            self.atoms.append(Atom(location=np.random.rand(3)*2*dmax - dmax, radius=np.random.rand()*rmax, index=i))
+            self.atoms.append(Atom(location=random.rand(3)*2*dmax - dmax, radius=random.rand()*rmax, index=i))
 
-    def sort_atoms(self):
-        """
-        Used to put atoms in their correct molecules and residues
-        :return:
-        """
-        # Set up the chain names list
-        self.mols, self.mol_names, self.atom_names, = [], [], []
-        # Go through each of the atoms in the system adding the atoms to their respective chains
-        for atom in self.atoms:
-
-            # Set the atom's name
-            self.atom_names.append(atom.element + str(self.atoms.index(atom)))
-            # Add the solution
-            if atom.mol_class.lower() == 'sol':
-                if self.sol is None:
-                    self.sol = Molecule(atoms=[atom])
-                    atom.mol = self.sol
-                else:
-                    self.sol.atoms.append(atom)
-                    atom.mol = self.sol
-            else:
-                # If no chain is specified, set the chain to 'None'
-                mol_name = atom.mol_class + atom.chain
-                # If the atom's chain does not exist add it to the list of chains
-                if mol_name not in self.mol_names:
-                    my_mol = Molecule(atoms=[atom], name=mol_name)
-                    self.mols.append(my_mol)
-                    self.mol_names.append(mol_name)
-                    atom.mol = my_mol
-                else:
-                    my_mol = self.mols[self.mol_names.index(mol_name)]
-                    my_mol.atoms.append(atom)
-                    atom.mol = my_mol
-        # Add the solution to the molecules list
-        if self.sol is not None:
-            self.mols.append(Molecule(atoms=self.sol))
-            self.mol_names.append("SOL")
-        # Set up the residues names list
-        self.residues, self.res_names = [], []
-        # Set up the residues
-        for atom in self.atoms:
-            res_name = atom.mol_class + atom.res_seq
-            # If the residue name does not exist, add it
-            if res_name not in self.res_names:
-                my_res = Residue(atoms=[atom], sequence=atom.res_seq, seg_id=atom.seg_id, mol=atom.mol, name=res_name)
-                self.residues.append(my_res)
-                self.res_names.append(res_name)
-                atom.res = my_res
-                atom.mol.resids.append(my_res)
-            else:
-                my_res = self.residues[self.res_names.index(res_name)]
-                my_res.atoms.append(atom)
-                atom.res = my_res
-
-    def build_network(self, surf_res=None, max_vert=None, box_size=None, sol_verts=True, output=True, flat_surfs=False,
+    def build_network(self, surf_res=None, max_vert=None, box_size=None, sol_verts=True, output=True,
                       calc_verts=True):
         """
         Allows user to build the network from the system object.
@@ -274,7 +228,7 @@ class System:
             self.net = Network(self, atoms=self.atoms)
         # Build the network
         self.net.build(surf_res=surf_res, max_vert=max_vert, box_size=box_size, build_surfs=sol_verts, output=output,
-                       flat_surfs=flat_surfs, calc_verts=calc_verts)
+                       calc_verts=calc_verts)
 
     def export_verts(self):
         """
@@ -291,37 +245,12 @@ class System:
         # Export the network
         export_net(self.net)
 
-    def export_selection(self, group1, group2=None, info=True):
-        """
-        Exports either a single group's body or two group's bodies and the interface between them
-        :param group1: Group object holding a set of atoms for cell analysis
-        :param group2: Group object holding a set of atoms for interface with group 1 analysis
-        :param info: Boolean for whether to export the information file
-        :return:
-        """
-        # Change to the designated output directory
-        os.chdir(self.dir)
-        # Check for an interface request
-        interface = False
-        if group2 is not None:
-            interface = True
-            # Set the bff
-            if group1.bff is not group2:
-                group1.bff = group2
-                group1.iface_atoms = None
-            # Calculate the interface
-            if group1.iface_atoms is None or len(group1.iface_atoms) == 0:
-                group1.get_iface(bff=group2)
-        # Export the first group's body
-        group1.export(info=info, iface=interface)
-
-    def set_output_directory(self):
+    def set_output_directory(self, directory=None):
         """
         Links set output directory to the system
         :return:
         """
-        set_output_dir(self)
-        os.chdir(self.dir)
+        set_output_dir(self, dir_name=directory)
 
     def exports(self, network=False, pdb=False, surfaces=False, full_network_object=False, no_sol_network_object=False,
                 alter_atoms_script=False, info=False):
@@ -329,66 +258,9 @@ class System:
         Prepares the output directory and system for output. Keeps things consistent
         :return:
         """
-        # Check to see if the pdb directory is suitable
-        if self.dir is None:
-            if os.path.dirname(self.base_file)[-9:] != 'test_data':
-                self.dir = os.path.dirname(self.base_file)
-            else:
-                self.set_output_directory()
-        if network:
-            os.chdir(self.dir)
-            # Export the network
-            self.export_net()
-        if pdb:
-            if not os.path.exists(self.dir + '/sys'):
-                os.mkdir(self.dir + "/sys")
-            os.chdir(self.dir + "/sys")
-            # Export a pdb file for the system
-            write_pdb(self.atoms, self.name, self)
-            os.chdir(self.dir)
-        if surfaces:
-            if not os.path.exists(self.dir + '/surfs'):
-                os.mkdir(self.dir + "/surfs")
-            # Export a pdb file for the system
-            for surf in self.net.surfs:
-                write_surfs(surfs=[surf], file_name="_".join(surf.ndx), directory=self.dir + "/surfs")
-            os.chdir(self.dir)
-        if full_network_object and self.net.build_surfs:
-            if not os.path.exists(self.dir + '/sys'):
-                os.mkdir(self.dir + "/sys")
-            os.chdir(self.dir + "/sys")
-            # Export a full system
-            export_mySys(self)
-        # Write the alter atoms script
-        if alter_atoms_script:
-            if not os.path.exists(self.dir + '/sys'):
-                os.mkdir(self.dir + "/sys")
-            os.chdir(self.dir + "/sys")
-            set_pymol_atoms(self)
-        # If the user wants the surfaces of the system without the SOL
-        if no_sol_network_object:
-            # Create the group
-            no_sol = Group(sys=self, mols=self.mols[:-1], name=self.name + "_shell")
-            no_sol.exports(shell=True, info=True)
-        # If the information is requested, export it
-        if info:
-            os.chdir(self.dir + "/sys")
-            export_net_info(self.net)
-        os.chdir(self.dir)
-
-
-    def show_sys(self, info=True, show=False, fig=None, ax=None):  # Needs to be way more extensive
-        if show:
-            pass
-        # Info section
-        if info:
-            # Print the header
-            print("{} System information:".format(self.name))
-            # Print the information
-            print("System Object Counts:\n  Atoms    : {}\nMolecules: {}\n".format(len(self.atoms), len(self.mols)))
-
-        # Show section
-        plot_atoms(self.atoms, fig=fig, ax=ax)
+        # Export the system (/System/sys_funcs/output)
+        export_sys(self, network=network, pdb=pdb, surfaces=surfaces, full_network_object=full_network_object,
+                   no_sol_network_object=no_sol_network_object, alter_atoms_script=alter_atoms_script, info=info)
 
     def show_net(self, info=True, full_net=False, verts=False, edges=False, surfs=False, system=False):
         # Empty Network
