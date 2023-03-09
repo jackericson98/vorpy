b094a9fc129eec49256530f458c3bdad5278fb86
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2023-03-08 23:05:51 -0500

    Commented to hell, deleted unused methods

--- System/Network/net_objs/surface.py
+++ System/Network/net_objs/surface.py
@@ -7,50 +7,58 @@ import csv
 
 class Surface:
     """Surface object. Holds the mesh data. Used to analyze interfaces between atoms."""
-    def __init__(self, atoms=None, net=None, edges=None, verts=None, doublet=False, points=None, tris=None, perimeter=None,
-                 normal=None, sa=0, curvature=None, function=None, load_ndxs=None, file=None, resolution=None, center=None,
-                 distance=None, tri_colors=None, color_scheme=None, color_map=None):
+    def __init__(self, atoms=None, net=None, edges=None, verts=None, doublet=False, points=None, tris=None,
+                 perimeter=None, normal=None, sa=0, curvature=None, function=None, file=None, resolution=None,
+                 center=None, tri_colors=None, color_scheme=None, color_map=None, ndx=None):
 
-        # If no network was given have a catch
-        self.ndx = None
-        self.net = net
-        if net is not None and net.atoms is not None and atoms is not None:
-            ndx = [net.atoms.index(atom) for atom in atoms]
-            ndx.sort()
-            self.ndx = ndx          # Index            : Indices of the atoms of the surface
-            self.net = net          # Network          : Network of the System
-        self.atoms = atoms          # Atoms            : Atoms of the surface
-        self.verts = verts          # Vertices         : Vertices of the surface
-        self.edges = edges          # Edges            : Edges of the surface
-        self.load_ndxs = load_ndxs  # Load indices     : List of object load indices
-        self.file = file
+        # Network objects
+        self.net = net                  # Network         : Network of the System
+        self.atoms = atoms              # Atoms           : List Atom objects for the surface
+        self.verts = verts              # Vertices        : List of Vertex objects for surface
+        self.edges = edges              # Edges           : List of Edge objects for the surface
 
-        self.func = function        # Surface function : Holds the coefficients of the function describing the surf
-        self.perimeter = perimeter  # Perimeter        : The points around the edges of the surface (IN ORDER)
-        self.points = points        # Points           : The points that make up the surface
-        self.rings = None           # Rings            : Lists of points in rings toward the center
-        self.ring_tris = None       # Ring Triangles   : Triangulation of each set of concentric ring points
-        self.flat_points = []       # Flattened points : Points projected into 2d based off of the surface normal
-        self.pflat_points = []      # Flat perimeter   : Flattened points around the perimeter
-        self.tris = tris            # Triangles        : A list of connections between the points
-        self.tri_colors = tri_colors
-        self.color_scheme = color_scheme
-        self.color_map = color_map
-        self.res = resolution       # Resolution       : The resolution with which to build the surface
-        self.sa = sa                # Surface Area     : The surface area of the
-        self.curv = curvature       # Curvature        : The curvature of the surface between the
-        self.norm = normal          # Surface Normal   : Normal to the center of the surface
-        self.loc = center           # Location         : Center point of the hyperboloid the surface is made from
-        self.dist = distance        # Distance         : Distance from the center of the surface to its atoms
-        self.com = None             # Center of mass   : The point toward which all building paths travel
-        self.doublet = doublet      # Doublet          : Indicates whether a surface is a part of a doublet or not
-        self.flat = False           # Flat             : Whether the surface is flat or not
+        # Main descriptor attributes
+        self.ndx = ndx                  # Index           : Indices of the atoms of the surface
+        self.file = file                # File            : File address for the reference file holding points and tris
+        self.func = function            # Function        : Holds the coefficients of the function describing the surf
+
+        # Points and tris
+        self.points = points            # Points          : The points that make up the surface
+        self.flat_points = []           # Flat points     : Points projected into 2d based off of the surface normal
+        self.perimeter = perimeter      # Perimeter       : The points around the edges of the surface (IN ORDER)
+        self.pflat_points = []          # Flat perimeter  : Flattened points around the perimeter
+        self.tris = tris                # Triangles       : A list of connections between the points
+
+        # Coloring attributes
+        self.tri_colors = tri_colors    # Tri colors      : Holds the color mapped color for each triangle
+        self.scheme = color_scheme      # Color Scheme    : Holds the method by which the color map is mapped
+        self.color_map = color_map      # Color Map       : Holds the map applied to the triangles on the surface
+        self.res = resolution           # Resolution      : The resolution with which to build the surface
+
+        # Calculation attributes
+        self.sa = sa                    # Surface Area    : The surface area of the
+        self.curv = curvature           # Curvature       : The curvature of the surface between the
+        self.norm = normal              # Surface Normal  : Normal to the center of the surface
+        self.loc = center               # Location        : Center point of the hyperboloid the surface is made from
+        self.com = None                 # Center of mass  : The point toward which all building paths travel
+        self.doublet = doublet          # Doublet         : Indicates whether a surface is a part of a doublet or not
+        self.flat = False               # Is Flat?        : Whether the surface is flat or not
+
         # Make sure that a0 is the atom with the smaller radius
         if self.atoms is not None:
             if self.atoms[0].rad > self.atoms[1].rad:
                 self.atoms[0], self.atoms[1] = self.atoms[1], self.atoms[0]
 
+        # Create and sort the atom's indices
+        if atoms is not None:
+            self.ndx = [atom.num for atom in atoms]
+            self.ndx.sort()
+
     def calc_func(self):
+        """
+        Calculates the coefficients for the surface between the two atoms
+        :return: The surface has the correct self.func attribute
+        """
         # Make sure that a0 is the atom with the smaller radius
         if self.atoms[0].rad > self.atoms[1].rad:
             self.atoms[0], self.atoms[1] = self.atoms[1], self.atoms[0]
@@ -79,10 +87,14 @@ class Surface:
         self.func = ABC + DEF + GHI + [J] + [K] + list(d)
 
     def read_file(self, file=None):
+        """
+        Reads the file holding the points and the triangles for the surface
+        :param file: Specifies the address for the build file
+        :return: The surfaces points and triangles are set
+        """
         # Check to see if the file exists
         if file is None and self.file is not None:
             file = self.file
-
         # Check that the provided file works as an address on its own
         if os.path.exists(file):
             file_address = file
@@ -92,7 +104,6 @@ class Surface:
         # Last brute force a location if the file name is incorrect
         else:
             return
-
         # Read an off file
         if file_address[-3:].lower() == 'off':
             # Open the file
@@ -130,6 +141,10 @@ class Surface:
 
     # Calculate curvature method
     def calc_curv(self):
+        """
+        Calculates the curvature of the surface
+        :return: The curvature attribute is filled
+        """
         # Check to see that the function has been calculated or not
         if self.func is None:
             self.calc_func()
@@ -150,13 +165,13 @@ class Surface:
         self.sa = sa
 
     # Build method. Makes the mesh for the surface and calculates the simplices between them
-    def build(self, res=None, flat=None, color=False):
-        # Check to see if the user provided a flat indication
-        if flat is None:
-            # Check if the surface is to be flat
-            flat = self.flat
-        if self.flat != flat:
-            self.flat = flat
+    def build(self, res=None, color=False):
+        """
+        Main build method for constructing surfaces
+        :param res: Specifies the resolution the surface is to be constructed with
+        :param color: Bool to color the surf or not
+        :return: The surfaces points and triangles are filled
+        """
         # Check to see if the file exists
         if self.file is not None:
             self.read_file()
@@ -184,47 +199,20 @@ class Surface:
         if color:
             self.color_tris()
 
-    # Build vta surface function
-    def build_vta(self):
-        # Instantiate the points attribute
-        self.points = []
-        # Add the vertex points to the surface's list of points
-        for vert in self.verts:
-            self.points.append(vert.loc)
-        self.perimeter = self.points
-        # Calculate the center of the surface
-        self.loc = calc_com(points=self.points)
-        self.points.append(self.loc)
-        # Calculate the vector between the atoms
-        self.norm = np.array(self.atoms[1].loc) - np.array(self.atoms[0].loc)
-        # Calculate the simplices
-        find_simps(self)
-
-    def find_flat_points(self):
-        # Check to see if the surface is flat or not.
-        if self.flat:
-            self.tris = [[i, (i + 1) % len(self.perimeter), len(self.points) - 1] for i in range(len(self.perimeter))]
-            return
-        # Copy the surface points
-        points = self.points.copy()
-        # Move all surf points toward the origin via center point
-        for i in range(len(points)):
-            points[i] = np.array(points[i]) - np.array(self.loc)
-        # Calculate the angles to rotate the center point around
-        nps = rotate_points(self.norm, points)
-        # Get the 2d version of the points and their Delaunay tesselation
-        nps = np.array(nps)
-        # Add the flat points to the surface's list of flat points
-        self.flat_points = [nps[i, :2] for i in range(len(self.points))]
-
-    # Color points method. Returns a list of colors corresponding to values at that point.
-    def color_tris(self, color_scheme='dist', color_map='inferno'):
+    def color_tris(self, color_scheme='dist', color_map='inferno', inverse=False):
+        """
+        Colors the triangles in the surface based on the specified coloring scheme and map
+        :param inverse: Inverts the color of the color map
+        :param color_scheme: Determines how the colors will be mapped
+        :param color_map: Determines the actual colors of the triangles
+        :return: The triangles in the surface are colored
+        """
         # Set up the color map
         my_cmap = cm.get_cmap(color_map)
         self.color_map = color_map
         # Default is distance based color map
         if color_scheme == 'dist':
-            self.color_scheme = color_scheme
+            self.scheme = color_scheme
             # Set up the distances
             dists = []
             tri_dists = []
@@ -243,9 +231,13 @@ class Surface:
             for i in range(len(self.tris)):
                 # Find the maximum distance point of the triangles
                 tri_dists.append(min([dists[_] for _ in self.tris[i]]))
-            self.tri_colors = [my_cmap((_ - min_dist) / (max_dist - min_dist)) for _ in tri_dists]
+            if inverse:
+                my_dists = [1 - ((_ - min_dist) / (max_dist - min_dist)) for _ in tri_dists]
+            else:
+                my_dists = [(_ - min_dist) / (max_dist - min_dist) for _ in tri_dists]
+            self.tri_colors = [my_cmap(_) for _ in my_dists]
         elif color_scheme == 'ins_out':
-            self.color_scheme = color_scheme
+            self.scheme = color_scheme
             # Set up a list of tracking
             inside_array = []
             # Go through the points in the surface
@@ -265,5 +257,5 @@ class Surface:
                     my_map.append(my_cmap(0.75))
             self.tri_colors = my_map
         elif color_scheme == 'curv':
-            self.color_scheme = color_scheme
+            self.scheme = color_scheme
             pass
