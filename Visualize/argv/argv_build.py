3e195f778b83e862ca1403876fded330a9689c68
jacke <jackericson98@gmail.com>
2023-03-17 22:03:51 -0400

    Initial commit: Build the network

---
+++ Visualize/argv/argv_build.py
@@ -0,0 +1,10 @@
+from System.Network.network import Network
+
+
+def argv_build(my_sys, usr_npt):
+    # Check for a network
+    if my_sys.net is None:
+        my_sys.net = Network(sys=my_sys, atoms=my_sys.atoms)
+
+    for my_group in my_sys.groups:
+        my_sys.net.build(my_group=my_group, output=False)
