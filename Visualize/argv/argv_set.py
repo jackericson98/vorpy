2fc8ae5fe652fa01e9621a75ad10b9482f707365
jacke <jackericson98@gmail.com>
2023-03-19 16:12:41 -0400

    Fixed settings issue

--- Visualize/argv/argv_set.py
+++ Visualize/argv/argv_set.py
@@ -1,4 +1,5 @@
-from Visualize.cmnd.set import sett, my_settings
+from Visualize.cmnd.set import sett
+from Visualize.cmnd.commands import my_settings
 
 
 def argv_sett(my_sys, usr_npt):
@@ -8,7 +9,7 @@ def argv_sett(my_sys, usr_npt):
         # Pop the file descriptor
         descriptor = my_npt[0]
         # Check to see that it is a descriptor
-        if descriptor.lower() not in my_settings or len(usr_npt) == 0:
+        if descriptor.lower() not in my_settings or len(my_npt) == 0:
             return
         # Load the file
-        sett(my_sys, [descriptor, my_npt[1]], vorpy2_set=False)
+        sett(my_sys, my_npt, vorpy2_set=False)
