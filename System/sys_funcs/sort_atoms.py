b65232e42f49cab6291058ab59275c9d9dbf55ca
jacke <jackericson98@gmail.com>
2023-03-19 23:32:40 -0400

    Revamped completely

--- System/sys_funcs/sort_atoms.py
+++ System/sys_funcs/sort_atoms.py
@@ -1,53 +1,44 @@
-from System.sys_objs.molcule import Molecule
 from System.sys_objs.residue import Residue
+from System.sys_objs.chain import Chain, Sol
 
 
 def sort_atoms(sys):
-    # Set up the chain names list
-    sys.mols, sys.mol_names, sys.atom_names, = [], [], []
-    # Go through each of the atoms in the system adding the atoms to their respective chains
+    # Go through the atoms in the system
+    resids, chains, sys.residues, sys.chains = {}, {}, [], []
+    # Go through the atoms in the system
     for atom in sys.atoms:
-
-        # Set the atom's name
-        sys.atom_names.append(atom.element + str(sys.atoms.index(atom)))
-        # Add the solution
-        if atom.mol_class.lower() == 'sol':
-            if sys.sol is None:
-                sys.sol = Molecule(atoms=[atom])
-                atom.mol = sys.sol
-            else:
-                sys.sol.atoms.append(atom)
-                atom.mol = sys.sol
+        # Create the chain and residue dictionaries
+        res_name, chn_name = atom.residue + atom.res_seq, atom.chain
+        # If the chain has been made before
+        if chn_name in chains:
+            # Get the chain from the dictionary and add the atom
+            my_chn = chains[chn_name]
+            my_chn.add_atom(atom)
+            atom.chn = my_chn
+        # Create the chain
         else:
-            # If no chain is specified, set the chain to 'None'
-            mol_name = atom.mol_class + atom.chain
-            # If the atom's chain does not exist add it to the list of chains
-            if mol_name not in sys.mol_names:
-                my_mol = Molecule(atoms=[atom], name=mol_name)
-                sys.mols.append(my_mol)
-                sys.mol_names.append(mol_name)
-                atom.mol = my_mol
+            # If the chain is the sol chain
+            if atom.residue.lower() == 'sol':
+                my_chn = Sol(atoms=[atom], residues=[], name=atom.chain)
+                sys.sol = my_chn
+            # If the chain is not sol create a regular chain object
             else:
-                my_mol = sys.mols[sys.mol_names.index(mol_name)]
-                my_mol.atoms.append(atom)
-                atom.mol = my_mol
-    # Add the solution to the molecules list
-    if sys.sol is not None:
-        sys.mols.append(Molecule(atoms=sys.sol))
-        sys.mol_names.append("SOL")
-    # Set up the residues names list
-    sys.residues, sys.res_names = [], []
-    # Set up the residues
-    for atom in sys.atoms:
-        res_name = atom.mol_class + atom.res_seq
-        # If the residue name does not exist, add it
-        if res_name not in sys.res_names:
-            my_res = Residue(atoms=[atom], sequence=atom.res_seq, seg_id=atom.seg_id, mol=atom.mol, name=res_name)
-            sys.residues.append(my_res)
-            sys.res_names.append(res_name)
-            atom.res = my_res
-            atom.mol.resids.append(my_res)
-        else:
-            my_res = sys.residues[sys.res_names.index(res_name)]
+                my_chn = Chain(atoms=[atom], residues=[], name=atom.chain)
+                sys.chains.append(my_chn)
+            # Set the chain in the dictionary and give the atom it's chain
+            chains[chn_name] = my_chn
+            atom.chn = my_chn
+        # Assign the atoms and create the residues
+        if res_name in resids:
+            my_res = resids[res_name]
             my_res.atoms.append(atom)
             atom.res = my_res
+        else:
+            my_res = Residue(atoms=[atom], name=atom.residue, sequence=atom.res_seq, chain=atom.chn)
+            atom.chn.residues.append(my_res)
+            resids[res_name] = my_res
+            atom.res = my_res
+            if atom.residue.lower() != 'sol':
+                sys.residues.append(my_res)
+            else:
+                sys.sol.residues.append(my_res)
