465ecd2b0a92b0c2a179ef7a2e290a8df7acb2d5
jacke <jackericson98@gmail.com>
2023-03-19 23:32:14 -0400

    Minor changes

--- Visualize/cmnd/set.py
+++ Visualize/cmnd/set.py
@@ -51,7 +51,8 @@ def sett(sys, usr_npt, vorpy2_set=False):
             if not vorpy2_set:
                 print(u"surface resolution set to {} \u212B".format(my_val))
         except ValueError:
-            print("\"{}\" is an invalid input for the surface resolution setting. Enter a float value (From 0.01 to 1 A, recommended 0.1 A)".format(my_val))
+            print("\"{}\" is an invalid input for the surface resolution setting. Enter a float value "
+                  "(From 0.01 to 1 A, recommended 0.1 A)".format(my_val))
     # Set the maximum vertex radius
     elif my_set in max_verts:
         # Check to see if the value is correct
@@ -60,7 +61,8 @@ def sett(sys, usr_npt, vorpy2_set=False):
             if not vorpy2_set:
                 print(u"maximum vertex radius set to {} \u212B".format(my_val))
         except ValueError:
-            print("\"{}\" is an invalid input for the maximum vertex radius setting. Enter a float value (From 0.10 to 20 A, recommended 7 A)".format(my_val))
+            print("\"{}\" is an invalid input for the maximum vertex radius setting. Enter a float value "
+                  "(From 0.10 to 20 A, recommended 7 A)".format(my_val))
     # Set the box multiplier
     elif my_set in box_sizes:
         # Check to see if the value is correct
@@ -69,7 +71,8 @@ def sett(sys, usr_npt, vorpy2_set=False):
             if not vorpy2_set:
                 print("box size multiplier set to {} x".format(my_val))
         except ValueError:
-            print("\"{}\" is an invalid input for the box size multiplier setting. Enter a float value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
+            print("\"{}\" is an invalid input for the box size multiplier setting. Enter a float value "
+                  "(From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     # Set the solute vertices
     elif my_set in build_surfses:
         try:
@@ -77,7 +80,8 @@ def sett(sys, usr_npt, vorpy2_set=False):
             if not vorpy2_set:
                 print("build surfaces set to {}".format(sys.net.build_surfs))
         except ValueError:
-            print("\"{}\" is an invalid input for the build surfaces setting. Enter a True/False value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
+            print("\"{}\" is an invalid input for the build surfaces setting. Enter a True/False value "
+                  "(From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     # Set the flat surfaces
     elif my_set in net_types:
         # Check to see if the value is correct
@@ -86,7 +90,8 @@ def sett(sys, usr_npt, vorpy2_set=False):
             if not vorpy2_set:
                 print("network type set to {}".format(sys.net.type))
         except ValueError:
-            print("\"{}\" is an invalid input for the flat surfaces setting. Enter a True/False value (From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
+            print("\"{}\" is an invalid input for the flat surfaces setting. Enter a True/False value "
+                  "(From 1.0 to 10.0 X, recommended 1.5 X)".format(my_val))
     elif my_set in surf_colors:
         sys.net.surf_col = my_val
         print("surface color set to {}".format(my_val))
@@ -101,7 +106,7 @@ def sett(sys, usr_npt, vorpy2_set=False):
                 old_rad = atom.rad
                 atom.rad = my_val[1]
                 counter += 1
-        sys.radii[1][sys.radii[0].index(my_val[0])] = my_val[1]
+        sys.radii[my_val[0]] = my_val[1]
         print(u"{} atoms changed from {} to {}".format(counter, old_rad, my_val[1]))
     # Check for a quit request
     elif my_set.lower() in quits:
