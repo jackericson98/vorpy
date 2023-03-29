f7aec98a8feafaa15f9381896a8c67443dfeeb4e
jacke <jackericson98@gmail.com>
2023-03-29 10:33:56 -0400

    Added color_tris

--- System/sys_funcs/draw.py
+++ System/sys_funcs/draw.py
@@ -1,5 +1,120 @@
 import numpy as np
 from System.sys_funcs.calcs import calc_dist
+from System.Network.net_funcs.build_edge import build_edge
+import matplotlib as mpl
+
+
+def color_tris(surf, color_scheme=None, color_map=None, inverse=False):
+    """
+    Colors the triangles in the surface based on the specified coloring scheme and map
+    :param inverse: Inverts the color of the color map
+    :param color_scheme: Determines how the colors will be mapped
+    :param color_map: Determines the actual colors of triangles
+    :return: The triangles in the surface are colored
+    """
+    if color_scheme is None:
+        color_scheme = surf.net.surf_scm
+    if color_map is None:
+        color_map = surf.net.surf_col
+    # Set up the color map
+    my_cmap = mpl.colormaps.get_cmap(color_map)
+    surf.color_map = color_map
+    # Set the color scheme for the surface
+    surf.scheme = color_scheme
+    # Default is distance based color map
+    if color_scheme == 'dist':
+
+        # Check if the tri_dists have been calculated before
+        if surf.tri_dists is None or len(surf.tri_dists) == 0 or len(surf.tri_dists) != len(surf.tris):
+            # Set up the distances
+            dists = []
+            surf.tri_dists = []
+            max_dist, min_dist = 0, np.inf
+            # Provide value for the points
+            for point in surf.points:
+                # Calculate the distance
+                my_dist = calc_dist(point, surf.loc)
+                dists.append(my_dist)
+                # Record the minimum and maximum distances
+                if my_dist < min_dist:
+                    min_dist = my_dist
+                elif my_dist > max_dist:
+                    max_dist = my_dist
+            # Go through the triangles in the surface
+            for i in range(len(surf.tris)):
+                # Find the maximum distance point of the triangles
+                surf.tri_dists.append(max([dists[_] for _ in surf.tris[i]]))
+            # Normalize the tri_dists
+            surf.tri_dists = [(_ - min_dist) / (max_dist - min_dist) for _ in surf.tri_dists]
+        surf.tri_colors = [my_cmap(_) for _ in surf.tri_dists]
+
+    elif color_scheme == 'ins_out':
+
+        # Check if the tri_dists have been calculated before
+        if surf.tri_ins_out is None or len(surf.tri_ins_out) == 0 or len(surf.tri_ins_out) != len(surf.tris):
+            # Set up a list of tracking
+            inside_array = []
+            # Go through the points in the surface
+            for point in surf.points:
+                # Calculate the distance between the point and the atom
+                my_dist = calc_dist(point, surf.atoms[0].loc)
+                if my_dist < surf.atoms[0].rad:
+                    inside_array.append(True)
+                else:
+                    inside_array.append(False)
+            # Now add the triangles
+            surf.tri_ins_out = []
+            for tri in surf.tris:
+                if inside_array[tri[0]] and inside_array[tri[1]] and inside_array[tri[2]]:
+                    surf.tri_ins_out.append(my_cmap(0.25))
+                else:
+                    surf.tri_ins_out.append(my_cmap(0.75))
+        surf.tri_colors = surf.tri_ins_out
+    elif color_scheme == 'curv':
+        # Check if the surface is flat
+        if surf.flat:
+            surf.tri_curvs = [0] * len(surf.tris)
+        elif surf.tri_curvs is None or len(surf.tri_curvs) == 0 or len(surf.tri_curvs) != len(surf.tris):
+            # Get the function coefficients
+            A, B, C, D, E, F, G, H, I, J = surf.func[:10]
+            curvs = []
+            min_curv, max_curv = np.inf, 0
+            # Get the curvature for each point
+            for point in surf.points:
+                # Label the points
+                x, y, z = point
+                # Get the gradient of the surface at the point
+                delf = [2 * A * x + D * y + F * z + G, 2 * B * y + D * x + E * z + H, 2 * C * z + E * y + F * x + I]
+                # Calculate the norm of the gradient
+                denominator = np.linalg.norm(delf) ** 4
+                # Calculate the determinant of the hessian matrix and the gradient matrix
+                numerator = np.linalg.det([[2 * A, D, F, delf[0]], [D, 2 * B, E, delf[1]], [F, E, 2 * C, delf[2]],
+                                           delf + [0]])
+                # Get the curvature
+                curv = - numerator / denominator
+                if curv > max_curv:
+                    max_curv = curv
+                if curv < min_curv:
+                    min_curv = curv
+                curvs.append(curv)
+            # Set up the tri_curvs list
+            surf.tri_curvs = []
+            # Go through the curvature values for each point
+            for i in range(len(surf.tris)):
+                # Get the triangle
+                tri = surf.tris[i]
+                # Get the curvatures
+                my_curvs = [curvs[_] for _ in tri]
+                # Find the maximum curvature point
+                curv_val = max(my_curvs)
+                # Add the curve value to the surface's list of curvatures
+                surf.tri_curvs.append(curv_val)
+            # Normalize the tri_curvs
+            surf.tri_curvs = [1 - (_ - min_curv) / (max_curv - min_curv) for _ in surf.tri_curvs]
+            # Set the curvature for the surface
+            surf.curv = max_curv
+        # Set the colors
+        surf.tri_colors = [my_cmap(_) for _ in surf.tri_curvs]
 
 
 def draw_line(points, radius=0.02, color=None, edge_org=None):
@@ -56,7 +171,7 @@ def draw_grid(net, color=None):
 def draw_edge(edge, radius=0.02, color=None):
     # Make sure the edge is built already
     if edge.points is None:
-        edge.build()
+        build_edge(edge)
 
     # Get the edge direction to point away from
     rads = [_.rad for _ in edge.atoms]
@@ -64,47 +179,4 @@ def draw_edge(edge, radius=0.02, color=None):
 
     # Calculate the lines
     edge.draw_points, edge.draw_tris = draw_line(edge.points, radius, color=color, edge_org=min_atom.loc)
-
-
-# Draw Vertices Function. Takes in a vertex and updates the loc_points, loc_tris, sphere_points, sphere_points attribute
-def draw_vert(vert, radius=0.05, resolution=0.1, color=None, diamond=True, edge_segs=False, sphere=False):
-    # Get the location of the vertex
-    loc = np.array(vert.loc)
-    vert.loc_points = []
-    vert.loc_tris = []
-    # If a diamond was requested
-    if diamond:
-        # Draw the point
-        xp, xn = loc + np.array([radius, 0, 0]), loc - np.array([radius, 0, 0])
-        yp, yn = loc + np.array([0, radius, 0]), loc - np.array([0, radius, 0])
-        zp, zn = loc + np.array([0, 0, radius]), loc - np.array([0, 0, radius])
-        # Connect the points
-        vert.loc_points = [xp, xn, yp, yn, zp, zn]
-        vert.loc_tris = [[0, 2, 4], [0, 2, 5], [0, 3, 4], [0, 3, 5], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5]]
-    # If a vertex object of the edge segments was requested
-    elif edge_segs:
-        segs = []
-        for edge in vert.edges:
-            if calc_dist(loc, edge.points[0]) < calc_dist(loc, edge.points[-1]):
-                segs.append(edge.points[:2])
-            else:
-                segs.append(edge.points[-2:])
-        # Go through the segments in the list and draw them
-        for seg in segs:
-            segment = draw_line(seg, radius=radius, color=color)
-            vert.loc_points += segment[0]
-            vert.loc_tris += segment[1]
-
-    # If the user wants a sphere
-    elif sphere:
-        # Set the resolution of the spheres
-        circ = 2 * np.pi * vert.rad
-        f = max(int(circ / resolution), 3)
-        # Find u, v values that span phi and theta
-        u, v = np.mgrid[0:2 * np.pi:f * resolution * 2j, 0:np.pi:f * resolution * 1j]
-        # Plot each sphere
-        # Get x, y, z data for the wireframe
-        x = vert.rad * np.cos(u) * np.sin(v) + loc[0]
-        y = vert.rad * np.sin(u) * np.sin(v) + loc[1]
-        z = vert.rad * np.cos(v) + loc[2]
 
