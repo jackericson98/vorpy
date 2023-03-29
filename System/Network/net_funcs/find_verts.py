e672d2363e23ae7c53bebfa37362fd9255a10740
jacke <jackericson98@gmail.com>
2023-03-29 10:34:52 -0400

    Added calc vert and calc flat vert

--- System/Network/net_funcs/find_verts.py
+++ System/Network/net_funcs/find_verts.py
@@ -1,8 +1,162 @@
 from System.sys_funcs.calcs import ndx_search, get_time
 from System.Network.net_objs.vertex import Vertex
 from System.Network.net_objs.edge import Edge
+from System.Network.net_funcs.build_edge import get_edge_loc
 from numpy import sqrt, array, square
+import numpy as np
 import time
+
+
+# Calculate vertex function. Takes in 4 atoms, calculates the loc and rad of the inscribed sphere and adds the
+def calc_vert(vert):
+    # The real location and radius of the base sphere
+    locs = np.array(vert.atoms[0].loc), np.array(vert.atoms[1].loc), np.array(vert.atoms[2].loc), \
+        np.array(vert.atoms[3].loc)
+    r0, r1, r2, r3 = vert.atoms[0].rad, vert.atoms[1].rad, vert.atoms[2].rad, vert.atoms[3].rad
+    r0_2 = r0 ** 2
+    # Find the recalculated location of the atoms
+    l0, l1, l2, l3 = locs[0], locs[1] - locs[0], locs[2] - locs[0], locs[3] - locs[0]
+    # Calculate our System of linear equations coefficients
+    a1, b1, c1, d1, f1 = 2 * l1[0], 2 * l1[1], 2 * l1[2], 2 * (r1 - r0), r0_2 - r1 ** 2 + l1[0] ** 2 + l1[
+        1] ** 2 + l1[2] ** 2
+    a2, b2, c2, d2, f2 = 2 * l2[0], 2 * l2[1], 2 * l2[2], 2 * (r2 - r0), r0_2 - r2 ** 2 + l2[0] ** 2 + l2[
+        1] ** 2 + l2[2] ** 2
+    a3, b3, c3, d3, f3 = 2 * l3[0], 2 * l3[1], 2 * l3[2], 2 * (r3 - r0), r0_2 - r3 ** 2 + l3[0] ** 2 + l3[
+        1] ** 2 + l3[2] ** 2
+    # Calculate the F values
+    F = a1 * b2 * c3 - a1 * b3 * c2 - a2 * b1 * c3 + a2 * b3 * c1 + a3 * b1 * c2 - a3 * b2 * c1
+    F_2 = F ** 2
+    F10 = b1 * c2 * f3 - b1 * c3 * f2 - b2 * c1 * f3 + b2 * c3 * f1 + b3 * c1 * f2 - b3 * c2 * f1
+    F11 = -b1 * c2 * d3 + b1 * c3 * d2 + b2 * c1 * d3 - b2 * c3 * d1 - b3 * c1 * d2 + b3 * c2 * d1
+    F20 = -a1 * c2 * f3 + a1 * c3 * f2 + a2 * c1 * f3 - a2 * c3 * f1 - a3 * c1 * f2 + a3 * c2 * f1
+    F21 = a1 * c2 * d3 - a1 * c3 * d2 - a2 * c1 * d3 + a2 * c3 * d1 + a3 * c1 * d2 - a3 * c2 * d1
+    F30 = a1 * b2 * f3 - a1 * b3 * f2 - a2 * b1 * f3 + a2 * b3 * f1 + a3 * b1 * f2 - a3 * b2 * f1
+    F31 = -a1 * b2 * d3 + a1 * b3 * d2 + a2 * b1 * d3 - a2 * b3 * d1 - a3 * b1 * d2 + a3 * b2 * d1
+    # Calculate the ranks of the matrices
+    m_rank, f_rank = 3, 3
+    if F == 0:
+        my_mtx = [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3], [d1, d2, d3], [f1, f2, f3]]
+        m_rank = np.linalg.matrix_rank(np.array(my_mtx[:-1]))
+        if m_rank != 3:
+            f_rank = np.linalg.matrix_rank(np.array(my_mtx))
+    verts = []
+    xs, ys, zs, Rs = [], [], [], []
+    # Case 1:
+    if F != 0:
+        # Calculate the radius polynomial coefficients
+        a = ((F11 ** 2 + F21 ** 2 + F31 ** 2) / F_2) - 1
+        b = 2 * (((F10 * F11 + F20 * F21 + F30 * F31) / F_2) - r0)
+        c = ((F10 ** 2 + F20 ** 2 + F30 ** 2) / F_2) - r0 ** 2
+        # If the discriminant is positive, find the real positive roots of the quadratic
+        if -4 * a * c + b ** 2 >= 0:
+            Rs = [R for R in np.roots([a, b, c]) if np.isreal(R)]
+        # Instantiate the verts array
+        verts = []
+        # Go through each radius and calculate the vertex
+        for R in Rs:
+            x, y, z = F10 / F + R * F11 / F, F20 / F + R * F21 / F, F30 / F + R * F31 / F
+            # Move the vertex back to the actual location of the atoms
+            verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
+    # Case 2:
+    elif a1 * b2 - a2 * b1 != 0 and m_rank == 3 and f_rank == 3 and F > 0:
+        # Calculate the _ polynomial coefficients
+        a = F_2 + F11 ** 2 + F21 ** 2 - F31 ** 2
+        b = 2 * (F10 * F11 + F20 * F21 - F30 * F31 - F * F31 * r0)
+        c = F10 ** 2 + F20 ** 2 - (F30 + F * r0)
+        roots, verts = [], []
+        # Check the discriminant
+        disc = -4 * a * c + b ** 2
+        if disc > 0:
+            roots = [root for root in np.roots([a, b, c]) if np.isreal(root)]
+        # Case 2 subcases:
+        # Case 2.1
+        if F31 != 0:
+            # Go through each radius and calculate the vertex
+            for z in roots:
+                x, y, R = F10 / F + z * F11 / F, F20 / F + z * F21 / F, F30 / F + z * F31 / F
+                # Move the vertex back to the actual location of the atoms
+                verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
+        # Case 2.2
+        elif F21 != 0:
+            # Go through each radius and calculate the vertex
+            for y in roots:
+                x, R, z = F10 / F + y * F11 / F, F20 / F + y * F21 / F, F30 / F + y * F31 / F
+                # Move the vertex back to the actual location of the atoms
+                verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
+        # Case 2.3
+        elif F11 != 0:
+            # Go through each radius and calculate the vertex
+            for x in roots:
+                R, y, z = F10 / F + x * F11 / F, F20 / F + x * F21 / F, F30 / F + x * F31 / F
+                # Move the vertex back to the actual location of the atoms
+                verts.append([[x + l0[0], y + l0[1], z + l0[2]], R])
+    loc, rad, loc2, rad2 = None, None, None, None
+    # If one root exists return it
+    if len(verts) == 1:
+        loc, rad = verts[0][0], verts[0][1]
+    # If two roots exist:
+    elif len(verts) == 2:
+        # Get the largest atom's radius
+        max_atom_rad = max([r0, r1, r2, r3])
+        # Set the locations and radii, so that the smaller vertex is first
+        if abs(verts[0][1]) > abs(verts[1][1]):
+            verts[0], verts[1] = verts[1], verts[0]
+        # Set the locations and radii variables
+        locs, rads = [verts[0][0], verts[1][0]], [verts[0][1], verts[1][1]]
+        # If either radii are negative (I'm not sure if this is possible, but let's catch it anyway)
+        if rads[0] < 0 or rads[1] < 0:
+            if rads[0] > 0 or abs(rads[0]) < max_atom_rad:
+                loc, rad = locs[0], rads[0]
+                if rads[1] > 0 or abs(rads[1]) < max_atom_rad:
+                    loc2, rad2 = locs[1], rads[1]
+            elif rads[1] > 0 or abs(rads[1]) < max_atom_rad:
+                loc, rad = locs[1], rads[1]
+        # If both radii are positive we have a doublet. Choose the smaller vertex to be the lead vertex and set loc2
+        else:
+            loc, loc2, rad, rad2 = locs[0], locs[1], rads[0], rads[1]
+    vert.loc, vert.rad, vert.loc2, vert.rad2 = loc, rad, loc2, rad2
+
+
+def calc_flat_vert(self, power=False):
+    """
+    Calculates the flat vertex between 4 atoms by finding the intersection of the mid-point planes between the first
+    atom and the others
+    :param power:
+    :return:
+    """
+    # Get the radii of the atoms in the vertex
+    rads = [_.rad for _ in self.atoms]
+    atom_rads = [x for _, x in sorted(zip(rads, self.atoms), key=lambda pair: pair[0])]
+    # Get the plane equations
+    coeffs = []
+    # Go through the atoms to make the planes
+    for an in atom_rads[1:]:
+        # Get the point between the atoms
+        r = np.array(an.loc) - np.array(atom_rads[0].loc)
+        norm = np.linalg.norm(r)
+        rn = r / norm
+        if power:
+            d0 = 0.5 * (norm ** 2 + atom_rads[0].rad ** 2 - an.rad ** 2) / norm
+            center = atom_rads[0].loc + d0 * rn
+        else:
+            center = 0.5 * r + np.array(atom_rads[0].loc)
+        coeffs.append(rn.tolist() + [np.dot(rn, center)])
+
+    x1, y1, z1, c1 = coeffs[0]
+    x2, y2, z2, c2 = coeffs[1]
+    x3, y3, z3, c3 = coeffs[2]
+
+    disc = z1 * y2 * x3 - y1 * z2 * x3 - z1 * x2 * y3 + x1 * z2 * y3 + y1 * x2 * z3 - x1 * y2 * z3
+    x_numerator = c1 * z2 * y3 - z1 * c2 * y3 - c1 * y2 * z3 + y1 * c2 * z3 + z1 * y2 * c3 - y1 * z2 * c3
+    y_numerator = - c1 * z2 * x3 + z1 * c2 * x3 + c1 * x2 * z3 - x1 * c2 * z3 - z1 * x2 * c3 + x1 * z2 * c3
+    z_numerator = c1 * y2 * x3 - y1 * c2 * x3 - c1 * x2 * y3 + x1 * c2 * y3 + y1 * x2 * c3 - x1 * y2 * c3
+    x, y, z = x_numerator / disc, y_numerator / disc, z_numerator / disc
+    # Get the radius
+    if power:
+        rad = np.sqrt(sum(np.square(np.array([x, y, z]) - np.array(atom_rads[0].loc)))) ** 2 - atom_rads[0].rad ** 2
+    else:
+        rad = np.sqrt(sum(np.square(np.array([x, y, z]) - np.array(atom_rads[0].loc))))
+    self.loc, self.rad = [x, y, z], rad
 
 
 # Find v0 function. Uses the atom finding functions to find a real verified site in the network
@@ -53,7 +207,7 @@ def find_v0(net, a0=None, group_atoms=None):
         for a2 in a2s[j]:
             # Use an edge object as a vehicle for calculating and verifying the inscribed circle
             edge = Edge(atoms=[a0, a1, a2], net=net)
-            edge.get_loc()
+            get_edge_loc(edge)
             # If a circle can be made and the site does not overlap with any other atoms, add it to the list
             if edge.loc is not None and edge.rad < net.max_vert and verify_site(edge, net):
                 verified_circles.append(edge.atoms)
@@ -180,11 +334,11 @@ def find_site(net, edge_atoms, vn_1=None, first=False, group_atoms=None):
         vert = Vertex(edge_atoms + [atom], net=net)
         # Calculate the correct vertex values
         if net.type == 'pow':
-            vert.calc_flat_vert(power=True)
+            calc_flat_vert(vert, power=True)
         elif net.type == 'del':
-            vert.calc_flat_vert(power=False)
+            calc_flat_vert(vert, power=False)
         else:
-            vert.calc_vert()
+            calc_vert(vert)
         # Catch the none location case
         if vert.loc is None:
             continue
@@ -233,7 +387,7 @@ def find_verts(net, a0=None, my_group=None, sniff=False):
     # Find the first verified vertex
     if len(group_atoms) == 4:
         v0 = Vertex(net.atoms, net)
-        v0.calc_vert()
+        calc_vert(v0)
     else:
         v0 = find_v0(net, a0, group_atoms)
     # If no v0 is possible (e.g., a lone atom) return
