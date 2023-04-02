547496143fe07cc06558d2aba98421daf9a39faa
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2023-04-01 23:40:49 -0400

    integrated network into the system

--- test/test_3_vert.py
+++ test/test_3_vert.py
@@ -11,9 +11,7 @@ Tests: calc_vert, verify_site, find_verts
 
 def test_calc_vor_vert():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(sys=my_sys, atoms=my_sys.atoms)
-    my_net.sort_atoms()
-    my_atoms = [my_net.atoms[_] for _ in [15, 255, 256, 332]]
+    my_atoms = [my_sys.atoms[_] for _ in [15, 255, 256, 332]]
     loc, rad, loc2, rad2 = calc_vert(locs=[_.loc for _ in my_atoms], rads=[_.rad for _ in my_atoms])
     assert [round(_, 3) for _ in loc] == [19.939, 18.897, 12.831]
     assert round(rad, 3) == 0.116
@@ -21,19 +19,16 @@ def test_calc_vor_vert():
 
 def test_verify_vor_site():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms)
-    my_net.sort_atoms()
-    my_atoms = [my_net.atoms[_] for _ in [15, 255, 256, 332]]
+    my_atoms = [my_sys.atoms[_] for _ in [15, 255, 256, 332]]
     loc, rad, loc2, rad2 = calc_vert(locs=[_.loc for _ in my_atoms], rads=[_.rad for _ in my_atoms])
-    assert verify_site(loc, rad, [15, 255, 256, 332], my_net)
+    assert verify_site(loc, rad, [15, 255, 256, 332], my_sys.net)
 
 
 def test_find_vor_verts():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms)
     my_group = Group(sys=my_sys, atoms=[my_sys.atoms[15]])
-    my_net.build(output=False, build_surfs=False, print_actions=False, my_group=my_group)
-    assert len(my_net.verts) == 32
+    my_sys.net.build(output=False, build_surfs=False, print_actions=False, my_group=my_group)
+    assert len(my_sys.net.verts) == 32
 
 
 """
@@ -42,9 +37,8 @@ ________________________________________________________Delaunay Calculations___
 
 
 def test_calc_del_vert():
-    my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms, net_type='del')
-    my_net.sort_atoms()
+    my_sys = System("../Data/test_data/EDTA_Mg.pdb", )
+    my_sys.net.type = 'del'
     loc, rad = calc_flat_vert(locs=[_.loc for _ in [my_sys.atoms[_] for _ in [0, 1, 3, 4]]],
                                           rads=[_.rad for _ in [my_sys.atoms[_] for _ in [0, 1, 3, 4]]], power=False)
     assert [round(_, 3) for _ in loc] == [21.353, 21.368, 9.513]
@@ -53,11 +47,10 @@ def test_calc_del_vert():
 
 def test_verify_del_site():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms, net_type='del')
-    my_net.sort_atoms()
+    my_sys.net.type = 'del'
     loc, rad = calc_flat_vert(locs=[_.loc for _ in [my_sys.atoms[_] for _ in [0, 1, 3, 4]]],
                                      rads=[_.rad for _ in [my_sys.atoms[_] for _ in [0, 1, 3, 4]]], power=False)
-    assert verify_site(loc, rad, [0, 1, 3, 4], my_net)
+    assert verify_site(loc, rad, [0, 1, 3, 4], my_sys.net)
 
 
 def test_find_del_verts():
@@ -68,7 +61,6 @@ def test_find_del_verts():
     assert len(my_net.verts) == 32
 
 
-
 """
 __________________________________________________________Power Calculations____________________________________________
 """
@@ -76,8 +68,7 @@ __________________________________________________________Power Calculations____
 
 def test_calc_pow_vert():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms, net_type='pow')
-    my_net.sort_atoms()
+    my_sys.net.type = 'pow'
     my_atoms = [my_sys.atoms[i] for i in [0, 1, 3, 4]]
     loc, rad = calc_flat_vert(locs=[_.loc for _ in my_atoms], rads=[_.rad for _ in my_atoms], power=True)
     assert [round(_, 3) for _ in loc] == [21.233, 21.301, 9.761]
@@ -86,17 +77,16 @@ def test_calc_pow_vert():
 
 def test_verify_pow_site():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms, net_type='pow')
-    my_net.sort_atoms()
+    my_sys.net.type = 'pow'
     my_atoms = [my_sys.atoms[i] for i in [0, 1, 3, 4]]
     loc, rad = calc_flat_vert(locs=[_.loc for _ in my_atoms], rads=[_.rad for _ in my_atoms], power=True)
-    assert verify_site(loc, rad, [0, 1, 3, 4], my_net)
+    assert verify_site(loc, rad, [0, 1, 3, 4], my_sys.net)
 
 
 def test_find_pow_verts():
     my_sys = System("../Data/test_data/EDTA_Mg.pdb")
-    my_net = Network(my_sys, my_sys.atoms, net_type='pow')
+    my_sys.net.type = 'pow'
     my_group = Group(sys=my_sys, atoms=[my_sys.atoms[0]])
-    my_net.build(output=False, build_surfs=False, print_actions=False, my_group=my_group)
-    assert len(my_net.verts) == 20
+    my_sys.net.build(output=False, build_surfs=False, print_actions=False, my_group=my_group)
+    assert len(my_sys.net.verts) == 20
 
