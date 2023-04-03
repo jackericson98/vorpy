cfa88f9ea3fab54d4351b301ffbfdac09a0a3f36
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2023-04-03 17:29:44 -0400

    Added systems for del and pow nets

--- test/test_1_load.py
+++ test/test_1_load.py
@@ -10,8 +10,26 @@ __________________________________________________Fixtures______________________
 
 
 @pytest.fixture(scope='session')
-def pdb_sys():
+def pdb_sys_vor():
     my_sys = System('../Data/test_data/EDTA_Mg.pdb')
+    my_sys.create_group(atoms=[my_sys.atoms[15]])
+    my_sys.net.build(output=False, my_group=my_sys.groups[0], surf_res=0.5)
+    return my_sys
+
+
+@pytest.fixture(scope='session')
+def pdb_sys_pow():
+    my_sys = System('../Data/test_data/EDTA_Mg.pdb')
+    my_sys.create_group(atoms=[my_sys.atoms[0]])
+    my_sys.net.build(output=False, my_group=my_sys.groups[0], net_type='pow')
+    return my_sys
+
+
+@pytest.fixture(scope='session')
+def pdb_sys_del():
+    my_sys = System('../Data/test_data/EDTA_Mg.pdb')
+    my_sys.create_group(atoms=[my_sys.atoms[0]])
+    my_sys.net.build(output=False, my_group=my_sys.groups[0], net_type='del')
     return my_sys
 
 
@@ -40,9 +58,9 @@ Test the functionality of read_pdb, read_gro, read_mol, read_cif
 
 
 # Load Pdb
-def test_read_pdb(pdb_sys):
-    assert len(pdb_sys.atoms) == 612
-    assert pdb_sys.atoms[30].loc == [20.39, 18.3, 8.23]
+def test_read_pdb(pdb_sys_vor):
+    assert len(pdb_sys_vor.atoms) == 612
+    assert pdb_sys_vor.atoms[30].loc == [20.39, 18.3, 8.23]
 
 #
 # # Load GRO
@@ -97,12 +115,12 @@ Tests the atom sorting schemes for a loaded network
 """
 
 
-def test_net_calc_box(pdb_sys):
-    assert pdb_sys.net.box == [[4.754, 2.664, -5.988], [36.891, 38.341, 25.568]]
-    assert pdb_sys.net.atoms_box == [[10.11, 8.61, -0.729], [31.535, 32.395, 20.309]]
+def test_net_calc_box(pdb_sys_vor):
+    assert pdb_sys_vor.net.box == [[4.754, 2.664, -5.988], [36.891, 38.341, 25.568]]
+    assert pdb_sys_vor.net.atoms_box == [[10.11, 8.61, -0.729], [31.535, 32.395, 20.309]]
 
 
-def test_net_sort_atoms(pdb_sys):
-    assert pdb_sys.net.atoms[45].box == [14, 8, 14]
-    assert pdb_sys.net.sub_box_size == [1.285, 1.427, 1.262]
-    assert len(pdb_sys.net.sub_boxes) == 25
+def test_net_sort_atoms(pdb_sys_vor):
+    assert pdb_sys_vor.net.atoms[45].box == [14, 8, 14]
+    assert pdb_sys_vor.net.sub_box_size == [1.285, 1.427, 1.262]
+    assert len(pdb_sys_vor.net.sub_boxes) == 25
