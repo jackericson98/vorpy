543e649fed056fa1fd28349eb0bcad79e097d4af
jackeWin <jackericson98@gmail.com>
2023-06-27 11:46:18 -0400

    Added sod as a residue type for sol

--- System/sys_funcs/input/input.py
+++ System/sys_funcs/input/input.py
@@ -65,7 +65,7 @@ def read_pdb(sys, file=None):
             # If no chain is specified, set the chain to 'None'
             res_str, chain_str = line[17:20].strip(), line[21]
             if chain_str == ' ':
-                if res_str.lower() in {'sol', 'hoh'}:
+                if res_str.lower() in {'sol', 'hoh', 'sod'}:
                     chain_str = 'SOL'
                 elif res_str.lower() in {'cl', 'mg', 'na', 'k'} and 'SOL' in chains:
                     chain_str = 'SOL'
