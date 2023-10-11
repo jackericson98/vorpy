4dd0557ec3347de9839ca0ae2d241b7bcad1dc4c
jackeWin <jackericson98@gmail.com>
2023-10-11 18:01:41 -0400

    Fixed reach to be accurate

--- System/sys_funcs/calcs/calcs.py
+++ System/sys_funcs/calcs/calcs.py
@@ -448,7 +448,7 @@ def get_atoms(cells, dist=0, my_atoms_matrix=None, my_sub_box_size=None, my_max_
     if my_atoms_matrix is not None:
         atoms_matrix, sub_box_size, max_atom_rad = my_atoms_matrix, my_sub_box_size, my_max_atom_rad
     # Get the reach around the box to grab atoms from
-    reach = int(dist / min(sub_box_size) - max_atom_rad) + 1
+    reach = int((dist + max_atom_rad) / min(sub_box_size)) + 2
     # Grab the number of cells in the grid
     n = atoms_matrix[-1, -1, -1][0]
     # If a single cell is entered
