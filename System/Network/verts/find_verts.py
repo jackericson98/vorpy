3a8bbfa69e57f1b235a0f125edeba52877d27e5e
jackeWin <jackericson98@gmail.com>
2023-10-26 11:31:03 -0400

    Improved the slow to work for del pow faster

--- System/Network/verts/find_verts.py
+++ System/Network/verts/find_verts.py
@@ -1,8 +1,7 @@
 from System.Network.verts.calc_vert import calc_vert
 from System.Network.verts.find_v0 import find_v0
-from System.Network.verts.find_site.slow import find_site
 from System.Network.verts.find_site.fast import find_site_fast_container
-from System.Network.verts.find_site.del_pow import find_site_pow_del
+from System.Network.verts.find_site.del_pow import find_site_pd_container
 from System.sys_funcs.calcs.calcs import get_time, ndx_search
 import time
 from numpy import sqrt
@@ -97,14 +96,14 @@ def find_verts(alocs, arads, max_vert, net_type, check_atoms, a0=None, my_group=
             # Find the next site in the network
             if net_type == 'vor':
                 vert_ndx_pr = find_site_fast_container(edge_atoms=edge_atoms, alocs=alocs, arads=arads, averts=averts,
-                                             vert_ndxs=vert_ndxs, max_vert=max_vert, net_type=net_type,
-                                             vn_1=vert['atoms'],
-                                             vn_1_loc=vert['loc'], group_atoms=group_atoms, metrics=metrics)
+                                                       vert_ndxs=vert_ndxs, max_vert=max_vert, net_type=net_type,
+                                                       vn_1=vert['atoms'], vn_1_loc=vert['loc'],
+                                                       group_atoms=group_atoms, metrics=metrics)
             else:
-                vert_ndx_pr = find_site(edge_atoms=edge_atoms, alocs=alocs, arads=arads, averts=averts,
-                                                vert_ndxs=vert_ndxs, max_vert=max_vert, net_type=net_type,
-                                                vn_1=vert['atoms'],
-                                                vn_1_loc=vert['loc'], group_atoms=group_atoms, metrics=metrics)
+                vert_ndx_pr = find_site_pd_container(edge_atoms=edge_atoms, alocs=alocs, arads=arads,
+                                                     averts=averts, vert_ndxs=vert_ndxs, max_vert=max_vert,
+                                                     net_type=net_type, vn_1=vert['atoms'], vn_1_loc=vert['loc'],
+                                                     group_atoms=group_atoms, metrics=metrics)
             # If the vertex is none continue
             if vert_ndx_pr is None:
                 continue
