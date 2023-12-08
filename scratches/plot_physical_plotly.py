cdbcb27c2d0d827a09fcd4de1014c805be577a63
jackeWin <jackericson98@gmail.com>
2023-12-08 09:58:35 -0500

    New plotting devices

---
+++ scratches/plot_physical_plotly.py
@@ -0,0 +1,61 @@
+import csv
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+
+plot_type = 'physical1'
+
+with open('C:/Users/jacke/PycharmProjects/vorpy/Data/user_data/foam_data.csv', 'r') as my_foam_data:
+    my_data = []
+    # x, y, z1, z2 = [], [], [], []
+    foam_data = csv.reader(my_foam_data)
+    for my_line in foam_data:
+        if len(my_line) <= 1 or plot_type not in my_line[0] or int(my_line[4]) < 100:
+            continue
+        my_data.append({'num': my_line[0], 'avg rad size': float(my_line[2]), 'box size': float(my_line[1]),
+                        'rad std': float(my_line[3]), 'num balls': int(my_line[4]),
+                        'density': float(my_line[5]), 'vol diff vor': 100 * float(my_line[6]),
+                        'sa diff vor': 100 * float(my_line[7]),
+                        'vol diff pow': 100 * float(my_line[8]), 'sa diff pow': 100 * float(my_line[9]),
+                        'num cells': int(my_line[10])})
+
+
+lists = {}
+for dp in my_data:
+    # Check if the data has been added before
+    if dp['density'] in lists:
+        lists[dp['density']][0].append(dp['vol diff vor'])
+        lists[dp['density']][1].append(dp['sa diff vor'])
+        # lists[dp['density']][2].append(dp['vol diff pow'])
+        # lists[dp['density']][3].append(dp['sa diff pow'])
+    else:
+        # lists[dp['density']] = [[dp['vol diff vor']], [dp['sa diff vor']], [dp['vol diff pow']],
+        #                                        [dp['sa diff pow']]]
+        lists[dp['density']] = [[dp['vol diff vor']], [dp['sa diff vor']]]
+# for _ in lists:
+#     print(_, [len(lists[_][i]) for i in range(4)])
+
+# Calculate averages across sets at each index
+averages = [
+    [sum(set_values[i]) / len(set_values[i]) for set_values in lists.values()]
+    for i in range(len(lists[0.025]))
+]
+
+# Extract x values for plotting
+x_values = list(lists.keys())
+
+# Create traces for each line
+trace_names = ['Volume', 'Surface Area']
+traces = [go.Scatter(x=x_values, y=avg, mode='lines', name=trace_names[i]) for i, avg in enumerate(averages)]
+
+# Create layout for the plot
+layout = go.Layout(title='Devries Percent Difference AWVd vs. Power for Densities',
+                   xaxis=dict(title='Density'),
+                   yaxis=dict(title='Percent Difference'))
+
+# Create figure
+fig = go.Figure(data=traces, layout=layout)
+
+# Show the plot
+fig.show()
--- scratches/scratch_4.py
+++ scratches/scratch_4.py
@@ -1,17 +1,18 @@
-import plotly.graph_objects as go
-import numpy as np
+import csv
 
-x = np.outer(np.linspace(-2, 2, 30), np.ones(30))
-
-# transpose
-y = x.copy().T
-z = np.cos(x ** 2 + y ** 2)
-print(x, y, z)
-fig = go.Figure(data=[go.Surface(x=x, y=y, z=z)])
-
-fig.update_traces(contours_z=dict(
-    show=True, usecolormap=True,
-    highlightcolor="limegreen",
-    project_z=True))
-
-fig.show()
+with open("C:/Users/jacke/PycharmProjects/vorpy/Data/user_data/BSA_5ORF_CG_coarse/sys/BSA_5ORF_CG_coarse_logs_vor.csv", 'r') as cg_file:
+    current_values = 'info'
+    skip = False
+    atoms, surfaces, edges, vertices = [], [], [], []
+    for i, line in enumerate(cg_file.readlines()):
+        # Skip the headers
+        if skip:
+            skip = False
+            continue
+        if line[0] in ['Atoms', 'Surfaces', 'Edges', 'Vertices']:
+            current_values = {'a': 'Atoms', 's': 'Surfaces', 'e': 'Edges', 'v': 'Vertices'}[line[0]]
+            skip = True
+            continue
+        if current_values == 'a':
+            atoms.append({'index': line[0], 'name': line[1], 'volume': line[2], 'surface area': line[3],
+                          'max curvature': line[4], 'neighbors': line[5:]})
