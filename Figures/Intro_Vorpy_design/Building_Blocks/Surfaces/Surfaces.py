90a7b48998ff2683fb39cac102ff52b1c786e08f
jackeWin <jackericson98@gmail.com>
2024-01-04 14:19:47 -0500

    Finished the Surfaces generating code

--- Figures/Intro_Vorpy_design/Building_Blocks/Surfaces/Surfaces.py
+++ Figures/Intro_Vorpy_design/Building_Blocks/Surfaces/Surfaces.py
@@ -15,24 +15,29 @@ Plotting Surfaces. Choose a surface type from below
     4. Curved Surface - Ball Size Ratio 9:10, Balls Kissing
     5. Curved Surface - Ball Size Ratio 1:2, Balls Kissing
     6. Curved Surface - Ball Size Ratio 9:10, Balls Slightly Overlapping
-    7. Curved Surface -  Ball Size Ratio 1:2, Balls Slightly Overlapping
+    7. Curved Surface - Ball Size Ratio 1:2, Balls Slightly Overlapping
     8. Curved Surface - Ball Size Ratio 9:10, Balls Overlapping
     9. Curved Surface - Ball Size Ratio 1:2, Balls Overlapping
-    10. 
+
 """
 
 # Set the surface type here:
-surface_type = 9
+surface_type = 7
 
 
 # Other settings
 res = 0.5
+show_surf = True
+show_atoms = True
 show_edges = True
 show_verts = False
+show_axes = False
 
 
 # Different surface types
 net_type = 'vor'
+xlocs, rads, title = None, None, None
+
 
 # 1. Flat Surface
 if surface_type == 1:
@@ -99,54 +104,51 @@ my_surf_atoms = [(ar([xlocs[0], 0.0, 0.0]), rads[0]), (ar([xlocs[1], 0.0, 0.0]),
 vert_atoms = [[_ for _ in my_surf_atoms] + [my_vert_atoms[i], my_vert_atoms[(i+1) % 4]] for i in range(4)]
 my_verts = [calc_vert(ar([_[0] for _ in my_atoms]), ar([_[1] for _ in my_atoms])) for my_atoms in vert_atoms]
 
-# ## Plot the verts and their atoms
-# for i in range(4):
-#     fig = plt.figure()
-#     ax = fig.add_subplot(projection='3d')
-#     plot_atoms([_[0] for _ in vert_atoms[i]], [_[1] for _ in vert_atoms[i]], fig=fig, ax=ax)
-#     plot_verts([my_verts[i][0]], [my_verts[i][1]], fig=fig, ax=ax, Show=True, spheres=True)
-
 # Calculate the Edges
 edge_atoms = [[_ for _ in my_surf_atoms] + [my_vert_atoms[(i+1)%4]] for i in range(4)]
 my_edge_verts = [(my_verts[j], my_verts[(j+1)%4]) for j in range(4)]
 my_edges = [build_edge(alocs=ar([_[0] for _ in edge_atoms[i]]), arads=ar([_[1] for _ in edge_atoms[i]]),
                        vlocs=ar([_[0] for _ in my_edge_verts[i]]), res=0.5) for i in range(4)]
 
-# Plot the edges and their atoms
-# for i in range(4):
-#     fig = plt.figure()
-#     ax = fig.add_subplot(projection='3d')
-#     plot_atoms([_[0] for _ in edge_atoms[i]], [_[1] for _ in edge_atoms[i]], fig=fig, ax=ax)
-#     plot_edges([my_edges[i][0]], fig=fig, ax=ax)
-#     plot_verts([_[0] for _ in my_edge_verts[i]], [_[1] for _ in my_edge_verts[i]], fig=fig, ax=ax, Show=True)
-
 # Calculate the surfaces
 my_surf = build_surf(alocs=[_[0] for _ in my_surf_atoms], arads=[_[1] for _ in my_surf_atoms],
                      epnts=[_[0] for _ in my_edges], res=res, net_type=net_type)
 
-# Plot everything
 # Create the figure
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 # Plot the surfaces
-plot_surfs([my_surf[0]], [my_surf[1]], fig=fig, ax=ax, alpha=0.5)
+if show_surf:
+    plot_surfs([my_surf[0]], [my_surf[1]], fig=fig, ax=ax, alpha=0.5)
+
 # Plot the edges
 if show_edges:
     plot_edges([_[0] for _ in my_edges], fig=fig, ax=ax, colors=['k'] * 4, thickness=2)
+
 # Plot the vertices
 if show_verts:
     plot_verts([_[0] for _ in my_verts], [_[1] for _ in my_verts], fig=fig, ax=ax, colors=['r']*4)
 # Plot the atoms
-plot_atoms(alocs=[_[0] for _ in my_surf_atoms], arads=[_[1] for _ in my_surf_atoms], alpha=0.2, fig=fig, ax=ax, res=10)
+if show_atoms:
+    plot_atoms(alocs=[_[0] for _ in my_surf_atoms], arads=[_[1] for _ in my_surf_atoms], alpha=0.3, fig=fig, ax=ax, res=10)
 
 # Set the scales for the figure
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.set_zlim(-5, 5)
 
+# Set the axes lines
+if show_axes:
+    ax.plot([-3, -2], [-3, -3], [0, 0])
+    ax.plot([-3, -3], [-2, -3], [0, 0])
+    ax.plot([-3, -3], [-3, -3], [0, 1])
+
+    # Set the axes labels
+    ax.text(x=-2, y=-3, z=-0.25, s='x')
+    ax.text(x=-3, y=-2, z=-0.25, s='y')
+    ax.text(x=-3, y=-3, z=1, s='z')
+
 # Set the title for the figure
 ax.set_title(title, font=dict(size=20, family='serif'))
 plt.show()
-
-
