8927351c0d400638e84aadf21bc50197b43a9a20
jackeWin <jackericson98@gmail.com>
2024-01-09 10:37:16 -0500

    Edges plot

--- Figures/Intro_Vorpy_design/Building_Blocks/Edges/Edges.py
+++ Figures/Intro_Vorpy_design/Building_Blocks/Edges/Edges.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import array as ar
 import matplotlib.pyplot as plt
 from Visualize.mpl_visualize import plot_atoms, plot_verts, plot_edges, plot_surfs
@@ -19,28 +20,56 @@ Edge plotting code. Choose an edge type below.
 # Choose here
 edge_choice = 5
 
+# Additional Settings
+atom_alpha = 0.4
+vert_alpha = 0.1
+edge_thickness = 2
+show_edge = True
+show_verts = True
+show_vert_spheres = False
+show_atoms = True
+
+# Declare variables
+title = ''
+
+
 # Flat edge
 if edge_choice == 1:
+    locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0]
     rads = 1.0, 1.0, 1.0
+    title = 'Flat Edge - All Balls Equal'
+
 # Semi Curved edge 1
 elif edge_choice == 2:
+    locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0]
     rads = 1.0, 0.75, 0.75
+    title = 'Curved Edge - No Overlap'
+
 # Semi Curved Edge 2
 elif edge_choice == 3:
+    locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0]
     rads = 1.0, 0.75, 0.5
+
 # Semi Curved Edge 3
 elif edge_choice == 4:
-    rads = 3.0, 2.5, 2.5
+    locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0]
+    rads = 1.0, 2.0, 2.0
+    title = 'Curved Edge - Two Overlapping Atoms'
+
 # Semi Curved Edge 4
 elif edge_choice == 5:
-    rads = 3.5, 3.0, 2.5
+    locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0]
+    rads = 2.0, 2.0, 2.5
+    title = 'Curved Edge - All Atoms Overlap'
 
 
 # Set the radii and distances for the surrounding atoms
 r, d = 0.5, 15.0
-my_vert_atoms = [(ar([0.0, d, 0.0]), r), (ar([0.0, -d, 0.0]), r)]
-# Create the edge Atoms
-edge_atoms = [(ar([2.5, 0.0, 0.0]), rads[0]), (ar([-2.5, 0.0, 0.0]), rads[1]), (ar([0.0, 0.0, 2.5]), rads[2])]
+my_vert_atoms = [(ar([0.0, 0.0, d]), r), (ar([0.0, 0.0, -d]), r)]
+
+# Create the edge atoms
+edge_atoms = [(ar(locs[i]), rads[i]) for i in range(3)]
+
 
 # Calculate the vertices
 vert_atoms = [edge_atoms + [_] for _ in my_vert_atoms]
@@ -51,11 +80,25 @@ my_edge = build_edge(alocs=ar([_[0] for _ in edge_atoms]), arads=ar([_[1] for _ 
                      vlocs=ar([_[0] for _ in my_verts]), res=0.5)
 
 
-# Plot everything
+# Make the plot
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-plot_edges([my_edge[0]], fig=fig, ax=ax, colors=['k'], thickness=1)
-plot_verts([_[0] for _ in my_verts], [_[1] for _ in my_verts], fig=fig, ax=ax)
-plot_atoms(alocs=[_[0] for _ in edge_atoms], arads=[_[1] for _ in edge_atoms], alpha=0.3, fig=fig, ax=ax, Show=True)
 
+# Plot the edge
+if show_edge:
+    plot_edges([my_edge[0]], fig=fig, ax=ax, colors=['k'], thickness=edge_thickness)
+# Plot the vertices
+if show_verts:
+    plot_verts([_[0] for _ in my_verts], [_[1] for _ in my_verts], fig=fig, ax=ax, spheres=show_vert_spheres, alpha=vert_alpha)
+# Plot the atoms
+if show_atoms:
+    plot_atoms(alocs=[_[0] for _ in edge_atoms], arads=[_[1] for _ in edge_atoms], alpha=atom_alpha, fig=fig, ax=ax)
 
+# Set the scales for the figure
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
+ax.set_zlim(-5, 5)
+
+# Set the title for the plot
+ax.set_title(title, font=dict(size=20, family='serif'))
+plt.show()
