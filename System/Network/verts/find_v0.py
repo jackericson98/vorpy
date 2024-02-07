2db33bfd8ec22494c2a3020678a535b5484ad0bd
jack <jackericson98@gmail.com>
2024-02-07 15:12:35 -0500

    quick fix for findv0 pow and del

--- System/Network/verts/find_v0.py
+++ System/Network/verts/find_v0.py
@@ -87,6 +87,9 @@ def find_v0(alocs, arads, averts, max_vert, net_type, a0=None, group_atoms=None,
                                                    max_vert=max_vert, mv_inc=max_vert, net_type=net_type,
                                                    group_atoms=group_atoms, metrics=metrics)
             # Check for a real site that is not a doublet
-            if my_vert is not None and my_vert[0]['loc'] is not None and my_vert[0]['loc2'] is None:
+            if my_vert is not None:
+                if net_type == 'vor':
+                    if my_vert[0]['loc'] is not None and my_vert[0]['loc2'] is None:
+                        return my_vert[0]
                 return my_vert[0]
         j += 1
