ce68142436148f45be9fb566e9d918404a51a70a
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2024-02-08 15:20:53 -0500

    comparison of models functionality

---
+++ Data/Analyze/compare_models.py
@@ -0,0 +1,12 @@
+
+
+
+"""
+The point of this file is to be able to take in multiple logs from different solved models and generate compariso data
+
+Things to compare:
+    1. Different solutions - Additively Weighted, Power, Primitive
+    2. Different coarse graining schemes
+    3. Different versions of the same molecule - frames, hybrid, bound, pressurized
+    4. Different groupings - Atom, element type, backbone, side-chains, residues, at
+"""
