1356c1bb78075600cebdaabd3a3503031924d2b7
jackeWin <jackericson98@gmail.com>
2024-02-14 18:18:57 -0500

    Created a function for doublet marking if the vert file does not have them marked already. Able to be used with Voronota but designed for older vert files

---
+++ System/Network/verts/mark_doublets.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def mark_doublets(verts):
+    # Check to see if the input for vertices is in dataframe format or not
+    if isinstance(verts, pd.DataFrame):
+        # Instantiate the vdubs list and averts dictionary
+        vdubs, averts = [], {}
+        # Go through each of the vertices to see of the vertex has been found yet
+        for i, vert in verts.iterrows():
+            # Set the standard doublet value to 0
+            vdub = 0
+            # We only need to add to and check the first atom in each vertex bc the indices are sorted
+            atom = vert['vatoms'][0]
+            # If the atom has a list we are going to check that for the vertex and append to it
+            if atom in averts:
+                # Go through the vertices in the list for the atom
+                for vert_check in averts[atom]:
+                    # If found it is a doublet
+                    if vert['vatoms'] == verts['vatoms'][vert_check]:
+                        vdub = 1
+                # Add the vertex to the atom verts list for that atom
+                averts[atom].append(i)
+            # Create a new list for the atom with the vertex's index
+            else:
+                averts[atom] = [i]
+            # Add the designation for the vertex
+            vdubs.append(vdub)
+
+    else:
+        vdubs = None
+        print("Gimme that sweet sweet dataframe please")
+    return vdubs
