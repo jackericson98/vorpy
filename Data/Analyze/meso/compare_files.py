1a30fa58aa82e56ba458f1dd59fef78001b7753c
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2024-02-16 17:01:49 -0500

    started, but still needs work

--- Data/Analyze/meso/compare_files.py
+++ Data/Analyze/meso/compare_files.py
@@ -31,10 +31,38 @@ def compare_files(pdb_files, log_files, show_overview=True, show_group_data=True
     # Compare inside vs outside:
     if compare_in_out:
         print('\nInside Vs. Outside Data')
-        print('\nAverage Curvature, ')
+        print('\nAverage Curvature Out, Average Curvature In, out vol H, out vol C, out vol ')
+        for i, sys in enumerate(systems):
+            print(in_out_data())
 
 
+def in_out_data(sys, logs):
+    # First we need to designate the atoms in each group that are in the outside and atoms that are on the inside
+    in_atoms, out_atoms = [], []
+    for atom in logs['atoms']:
+        out = False
+        for _ in atom['neighbors']:
+            if _ not in sys.groups[0].atoms:
+                out = True
+        if out:
+            out_atoms.append(atom['num'])
+            atom['in'] = False
+        else:
+            in_atoms.append(atom['num'])
+            atom['in'] = True
+    in_vol, out_vol, in_sa, out_sa, in_vols, out_vols = 0, 0, 0, 0, [], []
+    # Get the data for each atom
+    for atom in logs['atoms']:
+        if atom['in']:
+            in_sa += atom['sa']
+            in_vols.append(atom['vol'])
+        else:
+            out_vols.append(atom['vol'])
+            out_sa += atom['sa']
 
+def residue_data(sys, logs):
+    # We need to sort the atoms into their respective residues
+    amines = {}
 
 
 if __name__ == '__main__':
