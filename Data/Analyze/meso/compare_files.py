5f436a4325f289eb96752f48fd819243b87a83a0
jackericson98 <jackericson98@gmail.com>
2024-02-16 18:42:26 -0500

    poopy stuff

--- Data/Analyze/meso/compare_files.py
+++ Data/Analyze/meso/compare_files.py
@@ -59,10 +59,69 @@ def in_out_data(sys, logs):
         else:
             out_vols.append(atom['vol'])
             out_sa += atom['sa']
+    return {}
+
 
 def residue_data(sys, logs):
     # We need to sort the atoms into their respective residues
-    amines = {}
+
+    amino_acids = {'ALA': {}, 'ARB': {}, 'ASN': {}, 'ASP': {}, 'CYS': {}, 'GLN': {}, 'GLU': {}, 'HIS': {}, 'ILE': {},
+                   'LEU': {}, 'LYS': {}, 'MET': {}, 'PHE': {}, 'PRO': {}, 'SER': {}, 'THR': {}, 'TRP': {}, 'TYR': {},
+                   'VAL': {}, 'GLY': {}, 'ARG': {}}
+
+    nucleic_acids = {'DT': {}, 'DA': {}, 'DG': {}, 'DC': {}, 'DU': {}, 'U': {}, 'G': {}, 'A': {}, 'T': {}, 'C': {}}
+
+    ions = {}
+
+    other = {}
+    # We want to collect data from this: Residue volume, residue surface area, residue average curvature,
+    # residue maximum curvature, inter-residue sa
+
+    # Check if the system is coarse grained or not
+    for res in sys.residues:
+        # Get the logs atoms for the residue
+        res_atoms, res_surfs = [], {'in': [], 'out': []}
+        for atom_info_line in logs['atoms']:
+            # Get the residue atoms information
+            if atom_info_line['num'] in res.atoms:
+                res_atoms.append(atom_info_line)
+        # Get the residue surfaces
+        for surf_info_line in logs['surfs']:
+            # Check of one of the surfaces atoms is in the residue
+            if surf_info_line['atoms'][0] in res.atoms:
+                # Check for the other surface
+                if surf_info_line['atoms'][1] in res.atoms:
+                    res_surfs['in'].append(surf_info_line)
+                else:
+                    res_surfs['out'].append(surf_info_line)
+        # Calculate the volume and surface area
+        vol = sum([_['vol'] for _ in res_atoms])
+        sa = sum([_['sa'] for _ in res_surfs['out']])
+        # Get the curvatures
+        max_curv = max([_['max curvature'] for _ in res_atoms])
+        avg_curv = sum([_['curvature'] for _ in res_surfs['out'] + res_surfs['in']])/len(res_surfs)
+        # Get the maximum curvature between residue and other atom
+        max_surf = max(res_surfs['out'], key=lambda x: x['curvature'])
+
+        # Add the res info for analysis
+        res_info = {'vol': vol, 'sa': sa, 'max curv tot': max_curv, 'avg curv': avg_curv,
+                    'max_contact': [_ for _ in max_surf['atoms'] if _ not in res.atoms][0],
+                    'max curv out': max_surf['curvature']}
+        if res.name in amino_acids:
+            amino_acids[res.name][res.seq] = res_info
+        elif res.name in nucleic_acids:
+            nucleic_acids[res.name][res.seq] = res_info
+        elif res.name in ions:
+            ions[res.name][res.seq] = res_info
+        else:
+            if res.name in other:
+                other[res.name][res.seq] = res_info
+            else:
+                other[res.name] = {res.seq: res_info}
+    return {'amines': amino_acids, 'nucs': nucleic_acids, 'ions': ions, 'other': other}
+
+
+
 
 
 if __name__ == '__main__':
