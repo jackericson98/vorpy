1c5f07b0940fe7f3b339f63fcac92078bef10050
jackericson98 <62311229+jackericson98@users.noreply.github.com>
2024-02-16 16:54:33 -0500

    started, but still needs work

--- Data/Analyze/compare_models.py
+++ Data/Analyze/compare_models.py
@@ -1,4 +1,5 @@
-
+from Data.Analyze.read_logs import read_logs
+from System.sys_funcs.input.pdb import read_pdb
 
 
 """
@@ -10,3 +11,46 @@ Things to compare:
     3. Different versions of the same molecule - frames, hybrid, bound, pressurized
     4. Different groupings - Atom, element type, backbone, side-chains, residues, at
 """
+
+
+def analyze_atoms():
+    """
+
+    """
+    pass
+
+
+def analyze_elements(elements):
+    pass
+
+
+def analyze_residues(residues):
+    pass
+
+
+def compare_by_chain(chains):
+
+    pass
+
+
+def compare_interfaces():
+    pass
+
+
+def compare_models(log_files, pdb_files):
+    """
+    Takes in logs and pdbs and returns a comparison breakdowns
+    e.g. edta vor and edta pow, grouping - residue. Each residue will have their volume, surface area
+    """
+    pass
+
+
+def compare_models_network_type(models, grouping):
+    """
+    Compares vor, pow, del models to one another and produces a grouping level % diff in vol sa and max curvature
+    """
+    # Check to make sure that the models are the same
+    same = True
+    model_name = models[0]['data']['name']
+    for model in models:
+
