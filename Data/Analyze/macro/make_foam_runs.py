47e41186c8c36718cfe8c48f0583d022a0c52302
jackericson98 <jackericson98@gmail.com>
2024-03-01 10:10:50 -0500

    Created an output file for foam_runs

--- Data/Analyze/macro/make_foam_runs.py
+++ Data/Analyze/macro/make_foam_runs.py
@@ -14,4 +14,27 @@ my_dirs_unfiltered = []
 for my_dir in os.listdir(file_directory):
     my_dirs_unfiltered.append(my_dir)
 
-# We want to create a script to run all of these
+with open('C:/Users/i7-8700/PycharmProjects/vorpy/foam_runs.bat', 'w') as foam_file:
+    # We want to create a script to run all of these
+    num_done = 0
+    tot = 0
+    for my_dir in my_dirs_unfiltered:
+        # Get the settings to find the pdb within the directory
+        settings = my_dir.split('_')
+        try:
+            dinky_winky = int(settings[-1])
+            if dinky_winky >= 19:
+                continue
+            new_file = '_'.join(settings[:-1])
+
+        except ValueError:
+            new_file = '_'.join(settings)
+        tot += 1
+        run_dir = file_directory + '/' + my_dir +'/' + new_file + '.pdb'
+        export_dir = file_directory + '/' + my_dir
+        if os.path.exists(run_dir) and not os.path.exists(run_dir + '/vor'):
+            foam_file.write('\npython3 vorpy.py {} -s nt compare -e dir {} -e large'.format(run_dir, export_dir))
+        else:
+            num_done += 1
+
+print('{}/{} finished'.format(num_done, tot))
