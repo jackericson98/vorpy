00247bd24bcf006adbd564d2a42dd20b9769693d
jackericson98 <jackericson98@gmail.com>
2024-03-01 10:04:56 -0500

    Fixed for comparison of outputs

--- Visualize/argv/vpy_argv.py
+++ Visualize/argv/vpy_argv.py
@@ -73,10 +73,11 @@ def argv(my_sys):
         my_sys.net = Network(sys=my_sys, atoms=my_sys.atoms, net_type='pow')
         my_sys.name = my_sys.name + '_pow'
         atoms2 = my_sys.atoms.copy()
+        my_sys.set_output_directory(exports[0][1] + '/pow')
         for my_group in my_sys.groups:
             if len(my_group.atoms) > 0:
                 my_sys.net.build(my_group=my_group, max_vert=max_vert, print_vert_metrics=False, print_actions=False, surf_res=0.7)
-        argv_export(my_sys, exports)
+        argv_export(my_sys, exports, add_on='/pow')
         atom_vals_pow = []
         atom_nums_pow = []
         for i, atom in my_sys.net.atoms.iterrows():
@@ -87,8 +88,7 @@ def argv(my_sys):
                 atom_vals_pow.append({})
         my_sys.net2 = my_sys.net
         my_sys.net = Network(sys=my_sys, atoms=atoms2, net_type='vor')
-        os.mkdir(my_sys.dir + '_vor')
-        my_sys.dir = my_sys.dir + '_vor'
+        my_sys.set_output_directory(exports[0][1] + '/vor')
         os.chdir(my_sys.dir)
         for my_group in my_sys.groups:
             if len(my_group.atoms) > 0:
@@ -112,11 +112,13 @@ def argv(my_sys):
         print(*my_line, end="")
 
         current_dir = os.getcwd()
-        os.chdir('..')
+        os.chdir('../..')
         with open('foam_data.csv', 'a') as foam_file:
             foam_writer = csv.writer(foam_file)
             foam_writer.writerow(my_line)
         os.chdir(current_dir)
+        argv_export(my_sys, exports, add_on='/vor')
+        return
     elif my_sys.net_file is None:
         argv_build(my_sys, builds)
 
