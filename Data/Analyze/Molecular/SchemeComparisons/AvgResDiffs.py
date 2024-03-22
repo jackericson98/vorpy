f48c9501728aea8e7d95769c6b4be17be25cdd2a
jackericson98 <jackericson98@gmail.com>
2024-03-22 17:52:55 -0400

    Works

--- Data/Analyze/Molecular/SchemeComparisons/AvgResDiffs.py
+++ Data/Analyze/Molecular/SchemeComparisons/AvgResDiffs.py
@@ -32,39 +32,60 @@ if __name__ == '__main__':
     my_sys_names = [__.name for __ in systems]
 
     # Create the log file name dictionary
-    my_log_files = {_: {'vor': folder + _.name + '_vor_logs.csv',
-                   'pow': folder + _.name + '_pow_logs.csv',
-                   'del': folder + _.name + '_del_logs.csv'} for _ in my_sys_names}
+    my_log_files = {_: {__: folder + '/' + _ + '_{}_logs.csv'.format(__) for __ in {'vor', 'pow', 'del'}}
+                    for _ in my_sys_names}
 
     # Create the log dictionary
     my_logs = {}
 
     # Get the log information
-    vor_vals, pow_vals, del_vals = [], [], []
+    (pow_vol_avg_diff, del_vol_avg_diff, pow_vol_se, del_vol_se, pow_sa_avg_diff, del_sa_avg_diff, pow_sa_se,
+     del_sa_se) = [], [], [], [], [], [], [], []
     for system in systems:
-        vor_vals.append(residue_data(system, read_logs(my_log_files[system.name]['vor'], return_dict=True), vol=True,
-                                     sa=True))
+        pow_vols, del_vols, pow_sas, del_sas = [], [], [], []
+        # Get the values from the residue function
+        vor_reses = residue_data(system, read_logs(my_log_files[system.name]['vor'], return_dict=True))
+        pow_reses = residue_data(system, read_logs(my_log_files[system.name]['pow'], return_dict=True))
+        del_reses = residue_data(system, read_logs(my_log_files[system.name]['del'], return_dict=True))
+        # Find the percent differences by residue
+        # Classification level
+        for _ in vor_reses:
+            # Sub class level
+            for __ in vor_reses[_]:
+                # Res_seq level
+                for ___ in vor_reses[_][__]:
+                    if vor_reses[_][__][___] == {}:
+                        continue
+                    if vor_reses[_][__][___]['vol'] == 0 or vor_reses[_][__][___]['sa'] == 0:
+                        print(_, __, ___, vor_reses[_][__][___]['vol'], vor_reses[_][__][___]['sa'])
+                        continue
+                    pow_vols.append(abs(vor_reses[_][__][___]['vol'] - pow_reses[_][__][___]['vol']) / vor_reses[_][__][___]['vol'])
+                    del_vols.append(abs(vor_reses[_][__][___]['vol'] - del_reses[_][__][___]['vol']) / vor_reses[_][__][___]['vol'])
+                    pow_sas.append(
+                        abs(vor_reses[_][__][___]['sa'] - pow_reses[_][__][___]['sa']) / vor_reses[_][__][___]['sa'])
+                    del_sas.append(
+                        abs(vor_reses[_][__][___]['sa'] - del_reses[_][__][___]['sa']) / vor_reses[_][__][___]['sa'])
+        # Get the averages
+        pow_vol_avg_diff.append(100 * sum(pow_vols)/len(pow_vols))
+        del_vol_avg_diff.append(100 * sum(del_vols)/len(del_vols))
+        pow_sa_avg_diff.append(100 * sum(pow_sas)/len(pow_sas))
+        del_sa_avg_diff.append(100 * sum(del_sas)/len(del_sas))
+        # Get the standard Errors
+        pow_vol_se.append(np.std(pow_vols)/np.sqrt(len(pow_vols)))
+        del_vol_se.append(np.std(del_vols)/np.sqrt(len(del_vols)))
+        pow_sa_se.append(np.std(pow_sas)/np.sqrt(len(pow_sas)))
+        del_sa_se.append(np.std(del_sas)/np.sqrt(len(del_sas)))
 
-        pow_vals.append(residue_data(system, read_logs(my_log_files[system.name]['pow'], return_dict=True), vol=True,
-                                     sa=True))
+    # Create the dictionary for converting the labels
+    graph_labels = [{'EDTA_Mg': 'EDTA', 'cambrin': 'Cambrin', 'hairpin': 'Hairpin', 'p53tet': 'p53tet',
+                     'streptavidin': 'STVDN', '3zp8_hammerhead': 'H-head', 'NCP': 'NCP'}[_] for _ in my_sys_names]
 
-        del_vals.append(residue_data(system, read_logs(my_log_files[system.name]['del'], return_dict=True), vol=True,
-                                     sa=True))
-
-
-
-    # Choose what we are plotting Vol or SA
-    plotting = 'Volume'
-
-    # Reorganize
-    vor_vals = [my_logs[_]['vor'] for _ in my_sys_names]
-    pow_vals = [my_logs[_]['pow'] for _ in my_sys_names]
-    del_vals = [my_logs[_]['del'] for _ in my_sys_names]
-
-    pow_diff = [100 * abs(vor_vals[i] - pow_vals[i])/vor_vals[i] for i in range(len(vor_vals))]
-
-    del_diff = [100 * abs(vor_vals[i] - del_vals[i])/vor_vals[i] for i in range(len(vor_vals))]
-    # Create the
-    bar([pow_diff, del_diff], x_names=my_sys_names, legend_names=['Power', 'Primitive'],
-        Show=False, y_axis_title='% Difference', x_axis_title='Model', title='Total Molecule Surface Area')
+    # Create the bar graph
+    bar([pow_vol_avg_diff, del_vol_avg_diff], x_names=graph_labels, legend_names=['Power', 'Primitive'],
+        Show=True, y_axis_title='% Difference', x_axis_title='Model', title='Average Residue Volume Difference',
+        errors=[pow_vol_se, del_vol_se])
+    # Create the bar graph
+    bar([pow_sa_avg_diff, del_sa_avg_diff], x_names=graph_labels, legend_names=['Power', 'Primitive'],
+        Show=True, y_axis_title='% Difference', x_axis_title='Model', title='Average Residue Surface Area Difference',
+        errors=[pow_sa_se, del_sa_se])
 
