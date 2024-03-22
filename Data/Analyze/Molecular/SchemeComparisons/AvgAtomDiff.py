6f9443d073b103a295cd7e907c5947cfa59177fc
jackericson98 <jackericson98@gmail.com>
2024-03-22 17:35:07 -0400

    Added error and scaled by 100

--- Data/Analyze/Molecular/SchemeComparisons/AvgAtomDiff.py
+++ Data/Analyze/Molecular/SchemeComparisons/AvgAtomDiff.py
@@ -1,8 +1,8 @@
 import os
+import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 from System.system import System
-import matplotlib.pyplot as plt
 from Data.Analyze.read_logs import read_logs
 from System.Group.group import Group
 from Data.Analyze.plot_templates.bar import bar
@@ -10,7 +10,7 @@ from Data.Analyze.plot_templates.bar import bar
 
 def atoms_per_diff(systems, logs, val='volume'):
     # Create the averages lists
-    avg_pow_diffs, avg_del_diffs = [], []
+    avg_pow_diffs, pow_ses, avg_del_diffs, del_ses = [], [], [], []
     # Go through the loaded systems
     for system in systems:
         sys_name = system.name
@@ -22,10 +22,14 @@ def atoms_per_diff(systems, logs, val='volume'):
         for i in range(len(vor_atoms)):
             pow_diffs.append(abs(vor_atoms[i][val] - pow_atoms[i][val])/vor_atoms[i][val])
             del_diffs.append(abs(vor_atoms[i][val] - del_atoms[i][val])/vor_atoms[i][val])
-        avg_pow_diffs.append(sum(pow_diffs)/len(pow_diffs))
-
-        avg_del_diffs.append(sum(del_diffs)/len(del_diffs))
-    return avg_pow_diffs, avg_del_diffs
+        # Calculate the averages
+        avg_pow_diffs.append(100*sum(pow_diffs)/len(pow_diffs))
+        avg_del_diffs.append(100*sum(del_diffs)/len(del_diffs))
+        # Calculate the standard errors
+        pow_ses.append(100*np.std(pow_diffs)/np.sqrt(len(pow_diffs)))
+        del_ses.append(100*np.std(del_diffs)/np.sqrt(len(del_diffs)))
+    # Return the values
+    return avg_pow_diffs, pow_ses, avg_del_diffs, del_ses
 
 
 if __name__ == '__main__':
@@ -34,6 +38,7 @@ if __name__ == '__main__':
     root.withdraw()
     root.wm_attributes('-topmost', 1)
     folder = filedialog.askdirectory()
+    # Create the systems
     systems = []
     for root, dir, files in os.walk(folder):
         for file in files:
@@ -52,14 +57,13 @@ if __name__ == '__main__':
         for file in files:
             if file[-3:] == 'csv':
                 my_logs[file[:-13]][file[-12:-9]] = folder + '/' + file
-    avg_pow_diffs, avg_del_diffs = atoms_per_diff(systems, my_logs, val='sa')
+    # Get the average differences and the standard errors
+    avg_pow_diffs, pow_ses, avg_del_diffs, del_ses = atoms_per_diff(systems, my_logs, val='sa')
+    # Create the labels manually for the systems in question
     graph_labels = [{'EDTA_Mg': 'EDTA', 'cambrin': 'Cambrin', 'hairpin': 'Hairpin', 'p53tet': 'p53tet',
                      'streptavidin': 'STVDN', '3zp8_hammerhead': 'H-head', 'NCP': 'NCP'}[_] for _ in my_sys_names]
     # Plot the Data
     bar(data=[avg_pow_diffs, avg_del_diffs], title='Average Atom Surface Area % Difference', legend_title='Scheme',
         y_axis_title='% Difference', x_names=graph_labels, legend_names=['Power', 'Primitive'], Show=True,
-        x_axis_title='Model')
-
-
-
+        x_axis_title='Model', errors=[pow_ses, del_ses])
 
