54a8818bb902eed0224e11aca0a1b896d7c46bae
jackeWin <jackericson98@gmail.com>
2024-05-24 15:56:28 -0400

    Creates the run script for multiple foams

--- Data/Analyze/tools/batch/generate_vorpy_runs.py
+++ Data/Analyze/tools/batch/generate_vorpy_runs.py
@@ -28,20 +28,20 @@ for directory in os.listdir(folder):
     if skip_dir:
         continue
     num_tot += 1
-    run_file_num = num_tot // 500
+    run_file_num = num_needed // 75
     # Check to see if the path for the solved exists
     if not path.exists(folder + '/' + directory + '/vor'):
 
         num_needed += 1
         # Generate the pdb file by separating the number if there is one.
-        # try:
-        #     int(my_dir_split[-1])
-        #     pdb_file = '_'.join(my_dir_split[:-1]) + '.pdb'
-        # except ValueError:
-        #     pdb_file = directory + '.pdb'
-        # pdb_file = folder + '/' + directory + '/' + pdb_file
-        # with open('foam_runs_' + str(run_file_num) + '.bat', 'a') as write_file:
-        #     write_file.write('py vorpy.py {} -s nt compare -e dir {} -e large\n'.format(pdb_file, folder + '/' + directory))
+        try:
+            int(my_dir_split[-1])
+            pdb_file = '_'.join(my_dir_split[:-1]) + '.pdb'
+        except ValueError:
+            pdb_file = directory + '.pdb'
+        pdb_file = folder + '/' + directory + '/' + pdb_file
+        with open('foam_runs_' + str(run_file_num) + '.bat', 'a') as write_file:
+            write_file.write('py vorpy.py {} -s nt compare -e dir {} -e large\n'.format(pdb_file, folder + '/' + directory))
         # print('py vorpy.py {} -s nt compare -e dir {} -e large'.format(pdb_file, folder + '/' + directory))
 
 print('Number of directories solved: {}/{} at {}'.format(num_tot - num_needed, num_tot, datetime.datetime.now()))
