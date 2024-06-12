556bd8e1c9f81c1ef6e166d04f10f53a064f93b5
jackeWin <jackericson98@gmail.com>
2024-06-12 17:05:42 -0400

    Added some functionality and better plotting

--- Data/Analyze/IV_Molecular/B_Applications/curvature_assignments.py
+++ Data/Analyze/IV_Molecular/B_Applications/curvature_assignments.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from tkinter import filedialog
 from Data.Analyze.tools.compare.read_logs import read_logs
 import matplotlib.pyplot as plt
+import numpy as np
 
 root = tk.Tk()
 root.withdraw()
@@ -15,39 +16,74 @@ my_logs = filedialog.askopenfilename()
 
 my_logs_info = read_logs(my_logs)
 
+combos = {'HG22': 'HG', 'HG21': 'HG', 'HG23': 'HG', 'HD1': 'HD', 'HD2': 'HD', 'HB1': 'HB', 'HG11': 'HG', 'CG1': 'CG',
+          'CG2': 'CG', 'HG12': 'HG', 'HG13': 'HG', 'HW1': 'HW', 'HW2': 'HW', 'HH12': 'HH1', 'HH11': 'HH1',
+          'HH21': 'HH2', 'HH22': 'HH2', 'HB3': 'HB', 'HB2': 'HB', 'HD1': 'HD', 'HD2': 'HD', 'HD3': 'HD', 'HG2': 'HG',
+          'HG1': 'HG', 'HA1': 'HA', 'HA2': 'HA', 'H5\'\'': 'H5\'', 'H2\'\'': 'H2\'', 'H22': 'H21', 'H62': 'H61',
+          'H42': 'H41', 'O1P': 'OP', 'O2P': 'OP'}
+
 surf_type_dict = {}
 for i, surf in my_logs_info['surfs'].iterrows():
     atom_indices = [int(_) for _ in list(surf['atoms'])]
     atom0 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[0]].iloc[0]
     atom1 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[1]].iloc[0]
     atom_names = [atom0['name'].strip(), atom1['name'].strip()]
-    atom_names.sort()
-    combined_names = '_'.join(atom_names)
+    # for i in range(len(atom_names)):
+    #     try:
+    #         poopy = int(atom_names[i][-2])
+    #         atom_names[i] = atom_names[i][:-1]
+    #     except ValueError:
+    #         pass
+    #     except IndexError:
+    #         pass
+    for i, atom in enumerate(atom_names):
+        if atom in combos:
+            atom_names[i] = combos[atom]
+    if 'H' in atom_names[0]:
+        atom_names = [atom_names[1], atom_names[0]]
+    elif 'H' not in atom_names[1]:
+        atom_names.sort()
+    combined_names = ' - '.join(atom_names)
     if combined_names in surf_type_dict:
         surf_type_dict[combined_names].append(surf['curvature'])
     else:
         surf_type_dict[combined_names] = [surf['curvature']]
 
 new_surf_dict = {}
+new_surf_dict1 = {}
 for _ in surf_type_dict:
-    if sum(surf_type_dict[_]) / len(surf_type_dict[_]) >= 0.01 and len(surf_type_dict[_]) > 10:
-        new_surf_dict[_] = surf_type_dict[_]
+    curv_avg = sum(surf_type_dict[_]) / len(surf_type_dict[_])
+    if len(surf_type_dict[_]) > 10:
+        # Sort the outliars: Get the mean and standard deviation
+        my_mean, my_std = np.mean(surf_type_dict[_]), np.std(surf_type_dict[_])
+        # Filter out the outliars (2 stds)
+
+        new_surf_dict[_] = [__ for __ in surf_type_dict[_] if __ > my_mean]
+        # print(_, my_mean, my_std, new_surf_dict[_])
+        if len(new_surf_dict[_]) > 0:
+            new_surf_dict1[_] = new_surf_dict[_]
+
+
+surf_dict = dict(sorted(new_surf_dict1.items(), key=lambda item: np.mean(item[1]), reverse=True))
+
 
 # Prepare data for plotting
-labels, values = zip(*new_surf_dict.items())
+labels, values = zip(*surf_dict.items())
 
 # Create the boxplot
 fig, ax = plt.subplots(figsize=(12, 8))
-ax.boxplot(values, labels=labels, patch_artist=True)
+ax.boxplot(values[:min(15, len(values))], labels=labels[:min(15, len(values))], patch_artist=True)
 
 # Set plot title and labels
-ax.set_title('Distribution of Curvature Types')
-ax.set_xlabel('Curvature Type')
-ax.set_ylabel('Curvature Value')
+ax.set_title('Distribution of Curvatures ({})'.format(my_logs_info['data']['name'].capitalize()), fontdict=dict(size=30))
+ax.set_xlabel('Surface Type', fontdict=dict(size=30))
+ax.set_ylabel('Curvature', fontdict=dict(size=30))
 
 # Rotate x-axis labels for better readability
-plt.xticks(rotation=45)
+plt.xticks(rotation=45, size=20)
+plt.yticks(size=20)
 plt.tight_layout()
+plt.tick_params(axis='both', labelsize=20, width=2, length=15)
 
 # Display the plot
 plt.show()
