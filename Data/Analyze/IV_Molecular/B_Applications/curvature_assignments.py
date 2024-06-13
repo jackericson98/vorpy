7428b69f87f57e20e1d0730eb89d4c505ff36f10
jackeWin <jackericson98@gmail.com>
2024-06-13 16:39:21 -0400

    Added more labels and a try/except for surfaces not part of the atoms we are interested. Set the y lim

--- Data/Analyze/IV_Molecular/B_Applications/curvature_assignments.py
+++ Data/Analyze/IV_Molecular/B_Applications/curvature_assignments.py
@@ -12,7 +12,7 @@ import numpy as np
 root = tk.Tk()
 root.withdraw()
 root.wm_attributes('-topmost', 1)
-my_logs = filedialog.askopenfilename()
+my_logs = filedialog.askopenfilename(title='Choose Logs')
 
 my_logs_info = read_logs(my_logs)
 
@@ -20,22 +20,19 @@ combos = {'HG22': 'HG', 'HG21': 'HG', 'HG23': 'HG', 'HD1': 'HD', 'HD2': 'HD', 'H
           'CG2': 'CG', 'HG12': 'HG', 'HG13': 'HG', 'HW1': 'HW', 'HW2': 'HW', 'HH12': 'HH1', 'HH11': 'HH1',
           'HH21': 'HH2', 'HH22': 'HH2', 'HB3': 'HB', 'HB2': 'HB', 'HD1': 'HD', 'HD2': 'HD', 'HD3': 'HD', 'HG2': 'HG',
           'HG1': 'HG', 'HA1': 'HA', 'HA2': 'HA', 'H5\'\'': 'H5\'', 'H2\'\'': 'H2\'', 'H22': 'H21', 'H62': 'H61',
-          'H42': 'H41', 'O1P': 'OP', 'O2P': 'OP'}
+          'H42': 'H41', 'O1P': 'OP', 'O2P': 'OP', 'HD12': 'HD11', 'HD13': 'HD11', 'HD22': 'HD21', 'HD23': 'HD21',
+          'H5\'2': 'H5\'1', 'H2\'2': 'H2\'1', 'H72': 'H71', 'H73': 'H71'}
 
 surf_type_dict = {}
 for i, surf in my_logs_info['surfs'].iterrows():
     atom_indices = [int(_) for _ in list(surf['atoms'])]
-    atom0 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[0]].iloc[0]
-    atom1 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[1]].iloc[0]
+    try:
+        atom0 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[0]].iloc[0]
+        atom1 = my_logs_info['atoms'].loc[my_logs_info['atoms']['num'] == atom_indices[1]].iloc[0]
+    except IndexError:
+        continue
     atom_names = [atom0['name'].strip(), atom1['name'].strip()]
-    # for i in range(len(atom_names)):
-    #     try:
-    #         poopy = int(atom_names[i][-2])
-    #         atom_names[i] = atom_names[i][:-1]
-    #     except ValueError:
-    #         pass
-    #     except IndexError:
-    #         pass
+
     for i, atom in enumerate(atom_names):
         if atom in combos:
             atom_names[i] = combos[atom]
@@ -60,7 +57,7 @@ for _ in surf_type_dict:
 
         new_surf_dict[_] = [__ for __ in surf_type_dict[_] if __ > my_mean]
         # print(_, my_mean, my_std, new_surf_dict[_])
-        if len(new_surf_dict[_]) > 0:
+        if len(new_surf_dict[_]) > 5:
             new_surf_dict1[_] = new_surf_dict[_]
 
 
@@ -82,6 +79,7 @@ ax.set_ylabel('Curvature', fontdict=dict(size=30))
 # Rotate x-axis labels for better readability
 plt.xticks(rotation=45, size=20)
 plt.yticks(size=20)
+plt.ylim(bottom=0)
 plt.tight_layout()
 plt.tick_params(axis='both', labelsize=20, width=2, length=15)
 
