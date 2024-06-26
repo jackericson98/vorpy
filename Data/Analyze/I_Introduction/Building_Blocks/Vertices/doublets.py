30afa3eb3658ddfece25e3acde4e8f2d0c53b07c
jackeWin <jackericson98@gmail.com>
2024-06-26 02:22:15 -0400

    PLots edges for the two doublet types

--- Data/Analyze/I_Introduction/Building_Blocks/Vertices/doublets.py
+++ Data/Analyze/I_Introduction/Building_Blocks/Vertices/doublets.py
@@ -1,8 +1,9 @@
 import numpy as np
 from numpy import array as ar
 import matplotlib.pyplot as plt
-from Visualize.mpl_visualize import plot_atoms, plot_verts
+from Visualize.mpl_visualize import plot_atoms, plot_verts, plot_edges
 from System.Network.verts.calc_vert import calc_vert
+from System.Network.edges.build_edge import build_edge
 
 
 """
@@ -16,7 +17,7 @@ Doublet plotting code. Choose doublet type from below
 """
 
 # Choose Doublet type here V
-doublet_type = 4
+doublet_type = 1
 
 
 # Choose other settings here
@@ -30,9 +31,17 @@ my_vert = None
 
 # Type 1 Doublet - Perfect
 if doublet_type == 1:
-    rads = [1.1, 1.1, 1.0, 1.0]
-    locs = [2.1, 0.0, 0.0], [-2.1, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, -2.0, 0.0]
-    my_vert = ([0.0, 0.0, 2.5], 2.2, [0.0, 0.0, -2.5], 2.2)
+    rads = [2.5, 2.5, 1.0, 1.0]
+    locs = [3.6, 0.0, 0.0], [-3.6, 0.0, 0.0], [0.0, 1.5, 0.0], [0.0, -1.5, 0.0]
+    my_vert1 = calc_vert(locs, rads)
+    print(my_vert1)
+    my_vert = ([0.0, 0.0, 2.37], 1.822, [0.0, 0.0, -2.37], 1.822)
+    my_edges = [build_edge([locs[0]] + [_ for _ in locs[2:]], [rads[0]] + [_ for _ in rads[2:]], [my_vert[0], my_vert[2]], res=0.02),
+                build_edge(locs[1:], rads[1:], [my_vert[0], my_vert[2]], res=0.02)]
+    extendo_edges = [build_edge([locs[0], locs[1], locs[2]], [rads[0], rads[1], rads[2]], [my_vert[0], [0, 3.1, 5.7]], res=0.2),
+                     build_edge([locs[0], locs[1], locs[2]], [rads[0], rads[1], rads[2]], [my_vert[2], [0, 3.1, -5.7]], res=0.2),
+                     build_edge([locs[0], locs[1], locs[3]], [rads[0], rads[1], rads[3]], [my_vert[0], [0, -3.1, 5.7]], res=0.2),
+                     build_edge([locs[0], locs[1], locs[3]], [rads[0], rads[1], rads[3]], [my_vert[2], [0, -3.1, -5.7]], res=0.2)]
     title = 'Doublet Type 1'
 
 # No Overlap Vertex 1
@@ -40,18 +49,26 @@ elif doublet_type == 2:
     rads = [1.6, 1.5, 1.5, 0.72]
     locs = [1.31, -0.02, -0.9], [-1.03, 1.74, 0.58], [2.17, 0.21, 1.72], [0.35, 0.95, 1.63]
     title = 'Doublet Type 1, Verts on Either Side'
+    my_edges = []
+    extendo_edges = []
 
 # Type 1 Doublet - Both on the same size
 elif doublet_type == 3:
     rads = [1.8, 1.8, 1.3, 1.3]
     locs = [0.23, 0.39, 0.5], [-1.1, -0.37, 0.77], [0.3, 0.67, -0.56], [-1.82, 0.41, -1.48]
     title = 'Doublet Type 1, Verts on Same side'
+    my_edges = []
+    extendo_edges = []
 
 # Type 2 Doublet -
 elif doublet_type == 4:
     rads = [1.0, 1.0, 1.0, 0.5]
     locs = [1.5, np.sqrt(3)/2, 0.0], [-1.5, np.sqrt(3)/2, 0.0], [0.0, -np.sqrt(3), 0.0], [0.0, 0.0, 0.0]
     my_vert = ([0.0, 0.0, 2.75], 2.25, [0.0, 0.0, -2.75], 2.25)
+    my_edges = [build_edge([locs[i], locs[(i + 1) % 3], locs[3]], [rads[i], rads[(i + 1) % 3], rads[3]],
+                           [my_vert[0], my_vert[2]], res=0.2) for i in range(3)]
+    extendo_edges = [build_edge(locs[:3], rads[:3], [my_vert[0], [0, 0, 8]], res=0.2),
+                     build_edge(locs[:3], rads[:3], [my_vert[2], [0, 0, -8]], res=0.2)]
     title = 'Doublet Type 2'
 
 
@@ -66,11 +83,12 @@ ax = fig.add_subplot(projection='3d')
 
 
 # Plot the atoms
-plot_atoms(locs, rads, fig=fig, ax=ax, res=10, alpha=atom_alpha)
+plot_atoms(locs, rads, fig=fig, ax=ax, res=10, colors=['k' for _ in range(len(locs))], alpha=0.3)
 # Plot the vertices
-plot_verts([my_vert[0]], [abs(my_vert[1])], fig=fig, ax=ax, spheres=True, res=10)
-plot_verts([my_vert[2]], [abs(my_vert[3])], fig=fig, ax=ax, spheres=True, res=10)
+plot_verts([my_vert[0]], [abs(my_vert[1])], fig=fig, ax=ax, spheres=True, res=10, colors=['r'], alpha=0.3)
+plot_verts([my_vert[2]], [abs(my_vert[3])], fig=fig, ax=ax, spheres=True, res=10, colors=['r'], alpha=0.3)
 # Plot the edges
+plot_edges([_[0] for _ in my_edges + extendo_edges], fig=fig, ax=ax, colors=['b' for _ in range(len(my_edges + extendo_edges))], thickness=3)
 
 
 # Set the axes lines
