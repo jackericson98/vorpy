98d29a722c94203100df0f6cb0ca181dffcb1381
jackeWin <jackericson98@gmail.com>
2024-07-09 13:12:56 -0400

    Added legend orientation

--- Data/Analyze/tools/plot_templates/line.py
+++ Data/Analyze/tools/plot_templates/line.py
@@ -4,14 +4,17 @@ import numpy as np
 
 def line_plot(xs, ys, errors=None, labels=None, error_alpha=0.2, title=None, x_label=None, y_label=None,
               legend_title=None, Show=True, title_size=25, x_label_size=20, y_label_size=20, legend_title_size=20,
-              legend_label_size=20, tick_width=2, tick_length=12, legend_orientation='vertical'):
+              legend_label_size=20, tick_width=2, tick_length=12, legend_orientation='vertical', colors=None,
+              tick_val_size=15, linewidth=1):
+
+
     # Create a single plot
-    fig, ax = plt.subplots(figsize=(8, 6))
+    fig, ax = plt.subplots(figsize=(9, 6))
     for i in range(len(xs)):
         if labels is not None:
-            ax.plot(xs[i], ys[i], label=labels[i])
+            ax.plot(xs[i], ys[i], label=labels[i], linewidth=linewidth)
         else:
-            ax.plot(xs[i], ys[i])
+            ax.plot(xs[i], ys[i], linewidth=linewidth)
         if errors is not None:
             ax.fill_between(xs[i], [ys[i][j] - errors[i][j] for j in range(len(ys[i]))],
                             [ys[i][j] + errors[i][j] for j in range(len(ys[i]))], alpha=error_alpha)
@@ -24,12 +27,12 @@ def line_plot(xs, ys, errors=None, labels=None, error_alpha=0.2, title=None, x_l
         ax.set_xlabel(x_label, fontsize=x_label_size)
     if y_label is not None:
         ax.set_ylabel(y_label, fontsize=y_label_size)
-    ax.tick_params(axis='both', which='major', labelsize=legend_label_size, length=tick_length, width=tick_width)
+    ax.tick_params(axis='both', which='major', labelsize=tick_val_size, length=tick_length, width=tick_width)
     if labels is not None:
         ncol = 1
-        if legend_orientation == 'horizontal':
+        if legend_orientation.lower() == 'horizontal':
             ncol = len(labels)
-        legend = ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1), prop={'size': legend_label_size}, ncol=ncol,
+        legend = ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1), prop={'size': legend_label_size}, ncol=ncol,
                            shadow=True)
         if legend_title is not None:
             legend.set_title(legend_title)
