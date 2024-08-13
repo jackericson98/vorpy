84dfd00c95b39f271c21812e3d496cba97c39587
Optiplex_7060 <jackericson98@gmail.com>
2024-08-13 13:31:55 -0400

    Consolidated

---
+++ Visualize/cmnd/argv.py
@@ -0,0 +1,172 @@
+import sys
+import os
+from Visualize.cmnd.load import load
+from Visualize.cmnd.set import sett
+from Visualize.cmnd.group import group
+from Visualize.cmnd.commands import my_settings
+from System.system import System
+from System.Group.group import Group
+from System.sys_funcs.output.output import export_min1, export_min2, export_med, export_large, export_all, other_exports, set_sys_dir
+
+
+"""
+Argv rules: 
+1. Space delimited
+2. flags (-l: load, -s: set, -g: group, -e: export)
+3. For multiple inputs use &&
+4. Defaults to no sol, default settings, export all
+"""
+
+
+def argv_export(my_sys, usr_npt, interfaces=False, add_on=None):
+    """
+    Exports the specified elements from the system.
+
+    Specifications:
+
+        'default' - (optional) exports network, information, no Sol shell, pdb, set atoms from the system and atoms,
+                    surrounding atoms, shell, info from each group
+        'all' - exports everything from the full system and each group, needs a warning first
+        'info' - exports only the information files for the system and each group
+        'surfs' - exports the built surfaces individually from the system and all group surfaces + verts and edges
+
+
+    :param interfaces:
+    :param my_sys:
+    :param usr_npt:
+    :return:
+    """
+    # Check for interfaces
+    if interfaces:
+        for my_group in my_sys.groups:
+            my_group.exports(iface=True)
+        if len(usr_npt) == 0:
+            return
+    # Go through each of the inputs in the exports
+    if len(usr_npt) == 0:
+        usr_npt.append(['default'])
+    # Export the specified exports
+    for npt in usr_npt:
+        if npt[0].lower() in {'dir', 'directory'} and len(npt) == 2 and (os.path.isdir(npt[1]) or npt[1] == 'gsu_logs'):
+            if npt[1] == 'gsu_logs':
+                set_sys_dir(my_sys, "C:/Users/jacke/OneDrive - Georgia State University/GSU NSC/Jack/Vorpy/test_data/{}/logs".format(my_sys.name))
+            else:
+                if add_on is None:
+                    my_sys.dir = npt[1]
+                else:
+                    my_sys.dir = npt[1] + add_on
+    for npt in usr_npt:
+        if npt[0].lower() in {'dir', 'directory'}:
+            continue
+        export_npt(my_sys, npt[0])
+
+
+def export_npt(my_sys, usr_npt=None):
+    """
+    Exports the selection from the usr_npts
+    :param my_sys:
+    :param usr_npt:
+    :return:
+    """
+    # If nothing is specified export the defaults
+    if usr_npt is None or usr_npt.lower() in {'default', '2', 'medium', '', 'med'}:
+        export_large(sys=my_sys)
+
+    # Small export
+    elif usr_npt.lower() in {"tiny", "i", "info", "0", "smallest"}:
+        export_min1(my_sys)
+
+    # Medium small export
+    elif usr_npt.lower() in {"small", "s", "1"}:
+        export_min2(my_sys)
+
+    # Large Export
+    elif usr_npt.lower() in {"large", "l", "3"}:
+        export_large(my_sys)
+
+    # Export all
+    elif usr_npt.lower() in {'all', 'a', 'everything'}:
+        export_all(my_sys)
+
+    else:
+        other_exports(my_sys, usr_npt)
+
+
+def interpret_argvs():
+    # Separate the rest of the argv args
+    my_args = sys.argv[2:]
+    # Set up the commands dictionary
+    cmnds = {'npt': [], 'set': [], 'grp': [], 'bld': [], 'xpt': [], 'ifc': []}
+    # Go through the arguments
+    while my_args:
+        # Remove the first argument flag
+        arg = my_args.pop(0)
+        # Gather the cmnd and the flag
+        arg_cmnds = []
+        while True:
+            if len(my_args) == 0 or my_args[0][0] == '-':
+                break
+            else:
+                # Keep gathering the commands for the flag
+                arg_cmnds.append(my_args.pop(0))
+        # Add the command to the 181L list
+        if arg.lower() == '-l':
+            cmnds['npt'].append(arg_cmnds)
+        elif arg.lower() == '-s':
+            cmnds['set'].append(arg_cmnds)
+        elif arg.lower() == '-g':
+            cmnds['grp'].append(arg_cmnds)
+        elif arg.lower() == '-b':
+            cmnds['bld'].append(arg_cmnds)
+        elif arg.lower() == '-e':
+            cmnds['xpt'].append(arg_cmnds)
+        elif arg.lower() == '-i':
+            cmnds['ifc'].append(arg_cmnds)
+    # Return the lists
+    return cmnds
+
+
+def argv(my_sys):
+    # Load the atom file
+    load(my_sys, ["", sys.argv[1]])
+    # Interpret the commands
+    cmnds = interpret_argvs()
+    my_sys.cmnds = cmnds
+    # Go through each of the ls
+    print(cmnds)
+    load(my_sys, cmnds['npt'])
+    # Go through the user inputs loading files
+    while cmnds['set'] and len(cmnds['set']) > 0:
+        my_npt = cmnds['set'].pop(0)
+        # Pop the file descriptor
+        descriptor = my_npt[0]
+        # Check to see that it is a descriptor
+        if descriptor.lower() not in my_settings or len(my_npt) == 0:
+            return
+        # Load the file
+        sett(my_sys, my_npt, vorpy2_set=False)
+
+    # Make sure that the the group list is not None
+    if my_sys.groups is None:
+        my_sys.groups = []
+    # Check if a bff was specified
+    if cmnds['ifc'] is not None and len(cmnds['ifc']) >= 1:
+        bff = group(sys=my_sys, usr_npt=cmnds['ifc'][0])
+    # If the group is simple and we are just looking at no sol
+    if len(cmnds['grp']) == 0 or cmnds['grp'][0][0] == 'ns':
+        my_sys.groups.append(Group(sys=my_sys, chains=my_sys.chains, name=my_sys.name))
+    # if there is an input group
+    else:
+        for grouping in cmnds['grp']:
+            my_sys.groups.append(group(sys=my_sys, usr_npt=grouping, bff=bff))
+    # Check for a group network
+    for my_group in my_sys.groups:
+        if len(my_group.ball_ndxs) > 0:
+            my_group.build_network()
+
+    argv_export(my_sys, cmnds['xpt'])
+
+
+if __name__ == '__main__':
+    mySys = System()
+    argv(mySys)
