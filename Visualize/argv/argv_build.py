2c09f5e899957943e5c054d6448b961b61442aec
Optiplex_7060 <jackericson98@gmail.com>
2024-08-13 13:31:39 -0400

    Deleted and information moved to another place

--- Visualize/argv/argv_build.py
+++
@@ -1,7 +0,0 @@
-
-
-def argv_build(my_sys, usr_npt):
-    # Check for a group network
-    for my_group in my_sys.groups:
-        if len(my_group.ball_ndxs) > 0:
-            my_group.build_network()
