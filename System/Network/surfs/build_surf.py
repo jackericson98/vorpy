8617f4b8926b7861d930a6d9f4d8f0ae2abcb819
Optiplex_7060 <jackericson98@gmail.com>
2024-08-21 19:09:04 -0400

    Working toward a pure solve to the build surfaces process

--- System/Network/surfs/build_surf.py
+++ System/Network/surfs/build_surf.py
@@ -1,9 +1,9 @@
 from System.sys_funcs.calcs.surf import calc_surf_func, calc_surf_tri_curvs, calc_surf_point_curv
 from System.Network.surfs.perimeter import build_perimeter
 from System.Network.surfs.fill import fill_mesh, calc_surf_point
-from System.Network.surfs.triangulate import find_simps, tri_within, filter_tris
-from System.sys_funcs.calcs.calcs import calc_com, rotate_points
-from Visualize.mpl_visualize import plot_balls, plot_surfs
+from System.Network.surfs.triangulate import find_simps, tri_within, filter_tris, tri_within2
+from System.sys_funcs.calcs.calcs import calc_com, rotate_points, project_to_plane, map_to_plane
+from Visualize.mpl_visualize import plot_balls, plot_surfs, plot_edges
 import numpy as np
 import triangle as tri
 import matplotlib.pyplot as plt
@@ -100,34 +100,103 @@ def build_surf1(locs, rads, epnts, res, net_type, sfunc=None):
     # Get the center of mass for the surface
     surf_com, filter_hard = get_com(locs, rads, perimeter=perimeter, surf_loc=surf_loc, surf_norm=surf_norm, flat=flat,
                                     func=sfunc)
+    # Fill the mesh
+    # spoints = fill_mesh(locs, rads, func=sfunc, surf_loc=surf_loc, surf_norm=surf_norm, perimeter=perimeter,
+    #                     com=surf_com, flat=flat, res=res)
+
+    flat_perims = project_to_plane(np.array(perimeter), np.array(surf_loc), np.array(surf_norm))
+    # Triangulate the flat points
+    perim_dict = dict(vertices=np.array([[round(_[0], 3), round(_[1], 3)] for _ in flat_perims]),
+                      segments=np.array([[i, (i + 1) % len(flat_perims)] for i in range(len(flat_perims))]))
+
+    triangulation = tri.triangulate(perim_dict, 'a' + str(round(np.sqrt(3) / 4 * res ** 2, 3)))
+    spoints = triangulation['vertices']
+    # Check if we need to filter the points
+    new_spoints = spoints
+    if filter_hard:
+        flat_perim = project_to_plane(np.array(perimeter), np.array(surf_loc), np.array(surf_norm))
+        flat_points = project_to_plane(np.array(spoints), np.array(surf_loc), np.array(surf_norm))
+        new_spoints = []
+        for i, point in enumerate(flat_points):
+            if tri_within2(np.array(flat_perim), np.array(point)):
+                new_spoints.append(spoints[i])
+
+    # Find the simplices of the surface
+    tris, flat_points = find_simps(points=new_spoints, loc=surf_loc, norm=surf_norm)
+    # Set the surface curvature to 0
+    surf_curv = 0
+    # If the network type is voronoi the edges could be curved allowing for triangulations outside the edges
+    if net_type == 'aw':
+        # Add the normal curvature if possible
+        if tri_within(perimeter=perimeter,  loc=surf_loc, norm=surf_norm, flat_points=flat_points, point=surf_loc):
+            surf_curv = calc_surf_point_curv(sfunc, surf_loc)
+        # Filter out the bad triangles
+        surf_tris = filter_tris(tris=tris, flat_points=flat_points, res=res, perimeter=perimeter, loc=surf_loc,
+                                norm=surf_norm, filter_hard=filter_hard)
+        # Calculate the curvature of the triangles and the surface
+        if not flat:
+            tri_curvs, surf_curv = calc_surf_tri_curvs(sfunc, new_spoints, surf_tris, max_curv=surf_curv)
+        else:
+            tri_curvs, surf_curv = [0 for _ in range(len(list(surf_tris)))], 0
+    else:
+        surf_tris = tris
+        tri_curvs, surf_curv = [0 for _ in range(len(surf_tris))], 0
+
+    # Return the surface points, triangles, triangle curvatures, total curvature, surface function, com, and flatness
+    return new_spoints, tris, tri_curvs, surf_curv, sfunc, flat
+
+
+def build_surf1(locs, rads, epnts, res, net_type, sfunc=None):
+    """
+    Main build method for constructing surfaces
+    :param surf:
+    :param res: Specifies the resolution the surface is to be constructed with
+    :return: The surfaces points and triangles are filled
+    """
+
+    # Get the surface function if not already calculated
+    if sfunc is None:
+        sfunc = calc_surf_func(np.array(locs[0]), rads[0], np.array(locs[1]), rads[1])
+    # Check if the surface is flat
+    flat = False
+    if net_type in {'del', 'pow'} or rads[0] == rads[1]:
+        flat = True
+    # Build the perimeter of the surface
+    perimeter, surf_loc, surf_norm = build_perimeter(locs, rads, epnts=epnts, net_type=net_type)
     # Get the 2D Perimeter
     # Copy the perimeter points
     points = perimeter.copy()
     # Move all surf points toward the origin via center point
-    for i in range(len(points)):
-        points[i] = np.array(points[i]) - np.array(surf_loc)
+    # for i in range(len(points)):
+    #     points[i] = np.array(points[i]) - np.array(surf_loc)
     # Calculate the angles to rotate the center point around
-    nps = rotate_points(np.array(surf_norm), np.array(points))
+    flat_points = project_to_plane(np.array(points), np.array(surf_loc), np.array(surf_norm))
     # Get the 2d version of the points and their Delaunay tesselation
-    flat_points = [_[:2] for _ in nps]
-    # plt.scatter([round(_[0], 3) for _ in flat_points], [round(_[1], 3) for _ in flat_points])
-    # plt.show()
+    # flat_points = [_[:2] for _ in nps]
+    plt.scatter([round(_[0], 3) for _ in flat_points], [round(_[1], 3) for _ in flat_points])
+    plt.xlim([-5, 5])
+    plt.ylim([-5, 5])
+    plt.show()
     # Triangulate the flat points
     perim_dict = dict(vertices=np.array([[round(_[0], 3), round(_[1], 3)] for _ in flat_points]),
                       segments=np.array([[i, (i + 1) % len(flat_points)] for i in range(len(flat_points))]))
-    print(perimeter)
+
     try:
         triangulation = tri.triangulate(perim_dict, 'a' + str(round(np.sqrt(3)/4 * res ** 2, 3)))
     except RuntimeError:
-        print(rads)
+        print("here")
         plt.scatter([round(_[0], 3) for _ in flat_points], [round(_[1], 3) for _ in flat_points])
         plt.show()
+
     # Rotate the flat points back
-    triangulated_points = rotate_points(-np.array(surf_norm), np.array([[*_, 0] for _ in triangulation['vertices']]),
-                                        reverse=True)
-    triangulated_points = [_ + calc_surf_point(locs, surf_loc, sfunc) for _ in triangulated_points]
+    triangulated_points = map_to_plane(triangulation['vertices'], surf_loc, plane_normal=surf_norm)
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
+    ax.scatter([_[0] for _ in triangulated_points], [_[1] for _ in triangulated_points], [_[2] for _ in triangulated_points])
+    plt.show()
+
+    triangulated_points = [_ + calc_surf_point(locs, surf_loc, sfunc) for _ in triangulated_points]
+
     ax.scatter([_[0] for _ in triangulated_points], [_[1] for _ in triangulated_points], [_[2] for _ in triangulated_points])
     ax.scatter([_[0] for _ in perimeter], [_[1] for _ in perimeter], [_[2] for _ in perimeter])
     plot_balls(locs, rads, ['k', 'k'], fig, ax, alpha=0.1)
@@ -138,4 +207,4 @@ def build_surf1(locs, rads, epnts, res, net_type, sfunc=None):
     plot_surfs([spoints], [triangulation['triangles']], fig=fig, ax=ax, Show=True)
     tri_curvs, surf_curv = calc_surf_tri_curvs(sfunc, spoints, triangulation['triangles'])
     # Return the surface points, triangles, triangle curvatures, total curvature, surface function, com, and flatness
-    return np.array(spoints), np.array(triangulation['triangles']), tri_curvs, surf_curv, sfunc, surf_com, flat
+    return np.array(spoints), np.array(triangulation['triangles']), tri_curvs, surf_curv, sfunc, flat
