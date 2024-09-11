32ec7ae55d4ef814a771ea4c82667d404da5f36b
jack <jackericson98@gmail.com>
2024-09-10 21:28:26 -0400

    Added

---
+++ Data/Analyze/II_Macroscopic/wipe_user_data.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+import shutil
+
+root = tk.Tk()
+root.withdraw()
+root.wm_attributes('-topmost', 1)
+user_data = filedialog.askdirectory(title="Choose a user directory")
+
+removal_directories = []
+for roott, folders, files in os.walk(user_data):
+    for folder in folders:
+        for new_roott, sub_folders, filess in os.walk(roott + '/' + folder):
+            if new_roott[-3:] == 'vor' or new_roott[-3:] == 'pow':
+                print(new_roott)
+                removal_directories.append(new_roott)
+
+for dinkleschmitt in removal_directories:
+    try:
+        shutil.rmtree(dinkleschmitt + '/')
+    except Exception:
+        pass
