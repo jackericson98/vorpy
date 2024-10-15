cf636a1363ca891082d9e7ca4c0028d2eb204357
Optiplex_7060 <jackericson98@gmail.com>
2024-10-15 14:19:12 -0400

    Added edges to the exports

--- Visualize/cmnd/export.py
+++ Visualize/cmnd/export.py
@@ -179,8 +179,6 @@ def export(sys, usr_npt, my_group=None):
         my_group = group(sys, usr_npt)
     if sys.dir is None:
         sys.set_output_directory()
-    if my_group.surf_res is None:
-        my_group.surf_res = my_group.sys.net.settings['surf_res']
 
     while True:
         # Main prompt
