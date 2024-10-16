9f82e2723eef51e60c2d45165d93811398e71339
jackeWin <jackericson98@gmail.com>
2024-10-16 15:41:33 -0400

    Minor changes

--- Data/Analyze/II_Macroscopic/wipe_user_data.py
+++ Data/Analyze/II_Macroscopic/wipe_user_data.py
@@ -19,6 +19,9 @@ for roott, folders, files in os.walk(user_data):
                 if file == 'verts.txt':
                     print(file)
                     removal_directories.append(roott + '/' + folder + '/' + file)
+                elif 'info.txt' in file:
+                    print(file)
+                    removal_directories.append(roott + '/' + folder + '/' + file)
 
 for dinkleschmitt in removal_directories:
     try:
