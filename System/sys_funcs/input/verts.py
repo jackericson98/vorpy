c752d21f0956b8eb733f53f9c5eb667e64435594
jackeWin <jackericson98@gmail.com>
2024-10-18 12:18:17 -0400

    Fixed verts loading

--- System/sys_funcs/input/verts.py
+++ System/sys_funcs/input/verts.py
@@ -1,24 +1,30 @@
 import pandas as pd
 
 
-def read_verts(net, file):
+def read_verts(group, file):
 
     verts = []
     with open(file, 'r') as my_file:
         for i, line in enumerate(my_file.readlines()):
             line = line.split(' ')
             if i == 0:
-                if len(line) == 15 and net.type.lower() != line[-1].lower()[:-1]:
+                if len(line) == 15 and group.settings['net_type'].lower() != line[-1].lower()[:-1]:
                     print("\nWarning - Loaded Vertices do not match the set network type\n\n")
                 try:
-                    net.max_vert_rad = float(line[10][:-1])
+                    group.settings['max_vert'] = float(line[9][:-1])
                 except ValueError:
                     pass
+                continue
+            if line[0] == 'END':
+                continue
+            if int(line[8]) == 1 and [int(_) for _ in line[:4]] == verts[-1]['balls']:
+                verts[-1]['loc2'] = [float(_) for _ in line[4:7]]
+                verts[-1]['rad2'] = float(line[7])
+            else:
+                verts.append(
+                    {'balls': [int(_) for _ in line[:4]], 'loc': [float(_) for _ in line[4:7]], 'rad': float(line[7]),
+                     'loc2': None, 'rad2': None})
 
-            if len(line) == 8:
-                verts.append({'vatoms': [int(_) for _ in line[:4]], 'vloc': [float(_) for _ in line[4:7]], 'vrad': float(line[7])})
-            elif len(line) == 9:
-                verts.append({'vatoms': [int(_) for _ in line[:4]], 'vloc': [float(_) for _ in line[4:7]], 'vrad': float(line[7]), 'vdub': int(line[8])})
     verts = pd.DataFrame(verts)
     return verts
 
