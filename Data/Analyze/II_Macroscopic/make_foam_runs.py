28efd8671b2a837619e888891e7254e2637ee444
Optiplex_7060 <jackericson98@gmail.com>
2024-10-18 14:15:22 -0400

    Works for verts made before

--- Data/Analyze/II_Macroscopic/make_foam_runs.py
+++ Data/Analyze/II_Macroscopic/make_foam_runs.py
@@ -22,30 +22,35 @@ thine_dir = os.getcwd()
 
 OS = 'windows'
 
+# We want to create a script to run all of these
+num_done = 0
+tot = 0
+for my_dir in my_dirs_unfiltered:
+    # Get the settings to find the pdb within the directory
+    settings = my_dir.split('_')
+    try:
+        dinky_winky = int(settings[-1])
+        if dinky_winky > 19:
+            continue
+        new_file = '_'.join(settings[:-1])
+
+    except ValueError:
+        new_file = '_'.join(settings)
+    tot += 1
+    run_dir = file_directory + '/' + my_dir + '/' + new_file + '.pdb'
+    export_dir = file_directory + '/' + my_dir
+    # Check if the folder for AW exists, aka the network is Done
+    if os.path.exists(export_dir + '/' + new_file + '_Network_aw'):
+        # Check if the vertices have been solved
+        if os.path.exists(export_dir + '/verts.txt'):
+            strings.append('\npython3 vorpy.py {} -s mv -s nt compare -e dir {} -e logs -g chain a, -l verts {}'
+                           .format(run_dir, export_dir, export_dir + '/verts.txt'))
+        strings.append(
+            '\npython3 vorpy.py {} -s mv -s nt compare -e dir {} -e logs -g chain a'.format(run_dir, export_dir))
+    else:
+        num_done += 1
+
 if OS == 'linux':
-    # We want to create a script to run all of these
-    num_done = 0
-    tot = 0
-    for my_dir in my_dirs_unfiltered:
-        # Get the settings to find the pdb within the directory
-        settings = my_dir.split('_')
-        try:
-            dinky_winky = int(settings[-1])
-            if dinky_winky > 19:
-                continue
-            new_file = '_'.join(settings[:-1])
-
-        except ValueError:
-            new_file = '_'.join(settings)
-        tot += 1
-        run_dir = file_directory + '/' + my_dir +'/' + new_file + '.pdb'
-        export_dir = file_directory + '/' + my_dir
-        if os.path.exists(export_dir) and not os.path.exists(export_dir + '/' + new_file + '_Network_aw'):
-
-            strings.append('\npython3 vorpy.py {} -s mv {}} -s nt compare -e dir {} -e logs -g chain a'.format(run_dir, mv, export_dir))
-        else:
-            num_done += 1
-
     j = 0
     for i in range(len(strings)):
         pass
@@ -59,28 +64,6 @@ if OS == 'linux':
                 foam_write.write(strings[i])
 
 elif OS == 'windows':
-    # We want to create a script to run all of these
-    num_done = 0
-    tot = 0
-    strings = []
-    for my_dir in my_dirs_unfiltered:
-        # Get the settings to find the pdb within the directory
-        settings = my_dir.split('_')
-        try:
-            dinky_winky = int(settings[-1])
-            if dinky_winky > 19:
-                continue
-            new_file = '_'.join(settings[:-1])
-
-        except ValueError:
-            new_file = '_'.join(settings)
-        tot += 1
-        run_dir = file_directory + '/' + my_dir + '/' + new_file + '.pdb'
-        export_dir = file_directory + '/' + my_dir
-        if os.path.exists(export_dir) and not os.path.exists(export_dir + '/' + new_file + '_Network_aw'):
-            strings.append('\npy vorpy.py {} -s nt compare -e dir {} -s mv 20 -g chain a'.format(run_dir, export_dir))
-        else:
-            num_done += 1
 
     j = 0
     for i in range(len(strings)):
