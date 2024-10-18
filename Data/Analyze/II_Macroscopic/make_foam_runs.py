dda7fbf1473d7f548bd4c1c943c6d292d42a9d1e
jackeWin <jackericson98@gmail.com>
2024-10-18 14:41:45 -0400

    Fixed verts loading

--- Data/Analyze/II_Macroscopic/make_foam_runs.py
+++ Data/Analyze/II_Macroscopic/make_foam_runs.py
@@ -33,20 +33,22 @@ for my_dir in my_dirs_unfiltered:
         if dinky_winky > 19:
             continue
         new_file = '_'.join(settings[:-1])
-
+        export_type = 'logs'
     except ValueError:
+        export_type = 'large'
         new_file = '_'.join(settings)
     tot += 1
     run_dir = file_directory + '/' + my_dir + '/' + new_file + '.pdb'
     export_dir = file_directory + '/' + my_dir
+
     # Check if the folder for AW exists, aka the network is Done
-    if os.path.exists(export_dir + '/' + new_file + '_Network_aw'):
+    if not os.path.exists(export_dir + '/chain_a_aw') and '.csv' not in export_dir:
         # Check if the vertices have been solved
         if os.path.exists(export_dir + '/verts.txt'):
-            strings.append('\npython3 vorpy.py {} -s mv -s nt compare -e dir {} -e logs -g chain a, -l verts {}'
-                           .format(run_dir, export_dir, export_dir + '/verts.txt'))
-        strings.append(
-            '\npython3 vorpy.py {} -s mv -s nt compare -e dir {} -e logs -g chain a'.format(run_dir, export_dir))
+            strings.append('\npy vorpy.py {} -s mv 30 -s nt compare -e dir {} -e {} -g chain a, -l verts {}'
+                           .format(run_dir, export_dir, export_type, export_dir + '/verts.txt'))
+        else:
+            strings.append(  '\npy vorpy.py {} -s mv 30 -s nt compare -e dir {} -e {} -g chain a'.format(run_dir, export_dir, export_type))
     else:
         num_done += 1
 
@@ -67,8 +69,8 @@ elif OS == 'windows':
 
     j = 0
     for i in range(len(strings)):
-        if j < i // 220 or i == 0:
-            j = i // 220
+        if j < i // 200 or i == 0:
+            j = i // 200
             with open(thine_dir + '/foam_runs_{}.bat'.format(j), 'w') as foam_write:
                 foam_write.write(strings[i])
         else:
