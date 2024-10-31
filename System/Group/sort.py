c02d283a6df68418d1db468d9254d4d3eb54d9e8
Optiplex_7060 <jackericson98@gmail.com>
2024-10-30 20:11:15 -0400

    Fixed network silly boys

--- System/Group/sort.py
+++ System/Group/sort.py
@@ -8,10 +8,6 @@ def get_info(group):
     """
         Gathers information about the group and stores it in a dictionary
     """
-    # Get the group objects
-    group.get_surfs()
-    group.get_edges()
-    group.verts = [i for i, vert in group.net.verts.iterrows()]
     # Reset the group's data attributes
     group.sa, group.vol, group.vdw_vol, group.density, group.mass = 0, 0, 0, 0, 0
     com, vdw_com = [0, 0, 0], [0, 0, 0]
@@ -71,71 +67,3 @@ def add_balls(grp, ball_list):
         if sphere_ndx >= len(grp.ball_ndxs) or grp.ball_ndxs[sphere_ndx] != sphere:
             grp.ball_ndxs.insert(sphere_ndx, sphere)
             grp.atms.insert(sphere_ndx, sphere)
-
-
-def get_surfs(grp):
-    """
-    Finds and sorts all surfaces in the group without calculating them
-    :return: The group will have its surfaces sorted and non-redundant
-    """
-    net = grp.net
-    # Reset the surfaces lists
-    grp.surfs, grp.surf_ndxs = [], []
-    # Go through the atoms in the group
-    for i in grp.ball_ndxs:
-        sphere = net.balls.iloc[i]
-        # Go through the surfaces in the atoms list of surfaces
-        for j in sphere['surfs']:
-            surf = net.surfs.iloc[j]
-            # Get the index of the surface
-            surf_ndx = ndx_search(grp.surf_ndxs, surf['balls'])
-            # Check if the surface has been added yet or not
-            if surf_ndx >= len(grp.surf_ndxs) or grp.surf_ndxs[surf_ndx] == surf['balls']:
-                # Insert the index and the surfaces in their 181L place
-                grp.surfs.insert(surf_ndx, j)
-                grp.surf_ndxs.insert(surf_ndx, surf['balls'])
-
-
-def get_edges(grp):
-    """
-    Finds and sorts the edges in group
-    :return: The group will have all edge objects associated with it sorted and non-redundant
-    """
-    # Reset the surfaces lists
-    grp.edges, grp.edge_ndxs = [], []
-    # Go through the surfaces in the atoms list of surfaces
-    for i, edge in grp.net.edges.iterrows():
-        # Check that the edge shares an atom with the group
-        if len([0 for _ in edge['balls'] if _ in grp.ball_ndxs]) == 0:
-            continue
-        # Get the index of the edge
-        edge_ndx = ndx_search(grp.edge_ndxs, edge['balls'])
-        # Check if the edge has been added yet or not
-        if edge_ndx >= len(grp.edge_ndxs) or grp.edge_ndxs[edge_ndx] == edge['balls']:
-            # Insert the index and the surfaces in their 181L place
-            grp.edges.insert(edge_ndx, i)
-            grp.edge_ndxs.insert(edge_ndx, edge['balls'])
-
-
-def get_verts(grp):
-    """
-    Finds and sorts all the vertices in the group
-    :return: The groups vertices are sorted and non-redundant
-    """
-    # Create the group set
-    group_set = set(grp.ball_ndxs)
-    # Reset the surfaces lists
-    grp.verts, grp.vert_ndxs = [], []
-    # Go through the surfaces in the atoms list of surfaces
-    for i, vert in grp.net.verts.iterrows():
-        # Check that the edge shares an atom with the group
-        if not all([ball in group_set for ball in vert['balls']]):
-            continue
-        # Get the index of the vertex
-        vert_ndx = ndx_search(grp.vert_ndxs, vert['balls'])
-        # Check if the vertex has been added yet or not
-        if vert_ndx >= len(grp.vert_ndxs) or grp.vert_ndxs[vert_ndx] == vert['balls']:
-            # Insert the index
-            grp.verts.insert(vert_ndx, i)
-            grp.vert_ndxs.insert(vert_ndx, vert['balls'])
-    print('Group verts complete = ', len(grp.net.verts), grp.verts)
