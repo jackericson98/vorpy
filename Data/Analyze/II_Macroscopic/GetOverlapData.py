0ab96fe5e3d9e12acf29873b9ee75fe290c8a02e
Optiplex_7060 <jackericson98@gmail.com>
2024-11-19 16:41:38 -0500

    Starting the working process of this

---
+++ Data/Analyze/II_Macroscopic/GetOverlapData.py
@@ -0,0 +1,8 @@
+from Data.Analyze.tools.batch.compile_logs import get_logs_and_pdbs
+from Data.Analyze.tools.compare.read_logs2 import read_logs2
+
+
+my_logs = get_logs_and_pdbs(False)
+
+for loggy in my_logs:
+
