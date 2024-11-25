5b68b36869bbf6e3b8522c5777489fdd0b94ed11
Optiplex_7060 <jackericson98@gmail.com>
2024-11-25 13:15:01 -0500

    Has a flag for the simple case not matching the confirmed working case

--- System/Network/edges/edge_project.py
+++ System/Network/edges/edge_project.py
@@ -24,8 +24,8 @@ def edge_project(rn, pa, func, elocs, erads, direction=None):
         if len(roots) == 1:
             return pa + roots[0] * rn
         else:
-            p1 = pa + min(roots) * rn
-            p2 = pa + max(roots) * rn
+            p1 = pa + roots[0] * rn
+            p2 = pa + roots[1] * rn
         # Check the distance from the edge balls
         d11 = round(calc_dist(p1, elocs[0]) - erads[0], 5)
         d12 = round(calc_dist(p1, elocs[1]) - erads[1], 5)
@@ -33,12 +33,29 @@ def edge_project(rn, pa, func, elocs, erads, direction=None):
         d21 = round(calc_dist(p2, elocs[0]) - erads[0], 5)
         d22 = round(calc_dist(p2, elocs[1]) - erads[1], 5)
         d23 = round(calc_dist(p2, elocs[2]) - erads[2], 5)
+
+        # First find out if either of the roots is negative for later flagging
+        if roots[0] < 0:
+            point = p2
+        elif roots[1] < 0:
+            point = p1
+        elif roots[0] < roots[1]:
+            point = p1
+        else:
+            point = p2
+
         # First test for the distance between the points and the surfaces of the edge balls
         if d11 == d12 == d13 and d21 == d22 == d23:
             pass
         elif d11 == d12 == d13:
+            if all([point[i] == p1[i] for i in range(3)]):
+                print("1 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+                print(rn, pa, func, elocs, erads, direction)
             return p1
         elif d21 == d22 == d23:
+            if not all([point[i] == p2[i] for i in range(3)]):
+                print("2 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+                print(rn, pa, func, elocs, erads, direction)
             return p2
         # Next test to see if either of the points is heading the wrong direction
         if direction is not None:
@@ -46,10 +63,22 @@ def edge_project(rn, pa, func, elocs, erads, direction=None):
             p1_dot = np.dot(p1 - pa, direction)
             p2_dot = np.dot(p2 - pa, direction)
             if p1_dot > 0 > p2_dot:
+                if not all([point[i] == p1[i] for i in range(3)]):
+                    print("3 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+                    print(rn, pa, func, elocs, erads, direction)
                 return p1
             elif p2_dot > 0 > p1_dot:
+                if not all([point[i] == p2[i] for i in range(3)]):
+                    print("4 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+                    print(rn, pa, func, elocs, erads, direction)
                 return p2
         # Last go with the smaller of the two distances to the balls
         if d11 < d22:
+            if not all([point[i] == p1[i] for i in range(3)]):
+                print("5 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+                print(rn, pa, func, elocs, erads, direction)
             return p1
+        if not all([point[i] == p2[i] for i in range(3)]):
+            print("6 INCOMPATIBLE SOLUTION:   ", p1, p2, point)
+            print(rn, pa, func, elocs, erads, direction)
         return p2
