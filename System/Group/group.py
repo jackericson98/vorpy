a7e11606e5c85b586d6444c2aa47db5186310774
jack <jackericson98@gmail.com>
2024-12-02 08:28:08 -0500

    Fixed some discrepancies

--- System/Group/group.py
+++ System/Group/group.py
@@ -8,7 +8,7 @@ class Group:
     """Group class. Used to hold selections of atoms and do analysis on it"""
     def __init__(self, sys, name=None, atoms=None, molecules=None, chains=None, residues=None,
                  settings=None, build_net=False, surf_res=0.2, box_size=1.5, max_vert=40, build_type='all', net=None,
-                 net_type='aw', surf_col='plasma', surf_scheme='curv', num_splits=None, print_metrics=True,
+                 net_type='aw', surf_col='plasma', surf_scheme='mean', num_splits=None, print_metrics=True,
                  scheme_factor='log', make_net=False, verts=None):
         # System attributes
         self.sys = sys                  # Network            :    Network of the System
