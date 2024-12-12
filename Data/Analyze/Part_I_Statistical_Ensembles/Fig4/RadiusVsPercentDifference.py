3ebbc4c06b71c62a0d35b353d8f5e0c53e8ef095
Optiplex_7060 <jackericson98@gmail.com>
2024-12-11 20:10:02 -0500

    Made a separate plot

---
+++ Data/Analyze/Part_I_Statistical_Ensembles/Fig4/RadiusVsPercentDifference.py
@@ -0,0 +1,125 @@
+import csv
+import os
+from os import path
+import tkinter as tk
+from tkinter import filedialog
+from scipy import stats
+from matplotlib import pyplot as plt
+from System.system import System
+from Data.Analyze.tools.compare.read_logs2 import read_logs2
+from System.sys_funcs.input.pdb import read_pdb_line
+from System.sys_funcs.output.atoms import make_pdb_line
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+root = tk.Tk()
+root.withdraw()
+root.wm_attributes('-topmost', 1)
+sets = {}
+fig = plt.figure(figsize=(7, 5))
+
+colors = ['purple', 'orange', 'green']
+counter = 0
+folders = []
+
+
+while True:
+    folders.append(filedialog.askdirectory(title='Choose the folder mf'))
+    plot_another = input('Plot Another?    ')
+    if plot_another == 'n':
+        break
+
+for folder in folders:
+    for rroot, directys, files in os.walk(folder):
+        for file in files:
+            if file[-3:] == 'pdb' and 'atoms' not in file and 'diff' not in file:
+                pdb = rroot + '/' + file
+            if file[-3:] == 'csv' and 'aw' in file:
+                aw_logs = rroot + '/' + file
+            if file[-3:] == 'csv' and 'pow' in file:
+                pow_logs = rroot + '/' + file
+
+
+    # Take in the PDB
+    # pdb = filedialog.askopenfilename(title='Get the pdb file mf')
+    my_split_pdb = pdb.split('/')[-1]
+    density = my_split_pdb.split('_')[3]
+    cv = my_split_pdb.split('_')[1]
+    # Get the power logs
+    # pow_logs = filedialog.askopenfilename(title='Get the pow logs mf')
+    # Get the Voronoit Logs
+    # vor_logs = filedialog.askopenfilename(title='Get the vor logs mf')
+    sets[density] = {'pdb': pdb, 'pow_logs': pow_logs, 'vor_logs': aw_logs}
+
+    pow = read_logs2(pow_logs)
+    aw = read_logs2(aw_logs)
+    my_sys = System(pdb, simple=True)
+    rads, diffs, names = [], [], []
+
+    # Loop through the pdb file
+    with open(pdb, 'r') as pdb_reader, open(pdb[:-4] + '_vor_diff_colored.pdb', 'w') as pdb_writer:
+        mini, maxi = np.inf, -np.inf
+        # Loop through the lines
+        for i, line in enumerate(pdb_reader.readlines()):
+
+            if i == 0:
+                pdb_writer.write(line)
+                continue
+            npl = read_pdb_line(line)
+            # Check that the number is actually in the dataframe
+
+            # Get the pow atom and the vor atom
+            try:
+                pow_atom = pow['atoms'].loc[pow['atoms']['Index'] == i - 1].to_dict('records')[0]
+                vor_atom = aw['atoms'].loc[aw['atoms']['Index'] == i - 1].to_dict('records')[0]
+            except IndexError:
+                continue
+            # Calculate the difference in volume
+            vol_diff = (pow_atom['Volume'] - vor_atom['Volume']) / vor_atom['Volume']
+
+            # Check for crazy volume difference and trigger a volume difference
+            if vol_diff >= 10:
+                continue
+            diffs.append(vol_diff)
+            rads.append(npl['temperature_factor'])
+            names.append(npl['atom_name'])
+            if vol_diff < mini:
+                mini = vol_diff
+            if vol_diff > maxi:
+                maxi = vol_diff
+
+    def func(x, a, b, c):
+        return a * np.exp(-b * x) + c
+    try:
+        color = colors[counter]
+    except IndexError:
+        color = None
+    # Plot the radius to difference values
+    if counter == 0:
+        plt.plot([min(rads), max(rads)], [0, 0], c='k')
+    mean = np.mean([100 * _ for _ in diffs])
+    abs_mean = np.mean([100 * abs(_) for _ in diffs])
+    print(f"Density: {density} --- Mean = {mean}, Absolute Mean = {abs_mean}")
+    plt.plot([min(rads), max(rads)], [mean, mean], linestyle=':', c=color, linewidth=3)
+    plt.plot([min(rads), max(rads)], [abs_mean, abs_mean], linestyle='--', c=color, linewidth=3)
+    popt, pcov = curve_fit(func, np.array(rads), np.array(diffs))
+    # plt.text(s='y = {:.2f} * exp(-{:.2f} * x) + {:.2f}'.format(*popt), x=1.5, y=1.5, font=dict(size=10))
+    plt.plot(rads, [100 * _ for _ in func(np.array(rads), *popt)], c=color, linewidth=3, label=density)
+
+    plt.scatter(rads, [100 * _ for _ in diffs], s=2, alpha=0.3, c=color)
+
+    counter += 1
+
+plt.xlabel('Radius', fontdict=dict(size=30))
+plt.ylabel('% Difference', fontdict=dict(size=30))
+# plt.ti
+plt.xticks(font=dict(size=30))
+plt.yticks(font=dict(size=30))
+plt.ylim([-17, 27])
+plt.title('AW vs Power Volume', font=dict(size=30))
+plt.tick_params(axis='both', width=2, length=12)
+plt.legend(fontsize=25)
+
+plt.tight_layout()
+plt.show()
