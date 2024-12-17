842560e836314862d5109b844e29c1d24f6618f4
jackeWin <jackericson98@gmail.com>
2024-12-17 08:01:55 -0500

    Improved greatly

--- Data/Analyze/tools/batch/make_foam_runs.py
+++ Data/Analyze/tools/batch/make_foam_runs.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+import platform
 
 
 root = tk.Tk()
 root.withdraw()
 root.wm_attributes('-topmost', 1)
-file_directory = filedialog.askdirectory()
+
+
+# Try to open up the foam_gen user_data file
+try:
+    file_directory = filedialog.askdirectory(initialdir='../foam_gen/Data/user_data')
+except:
+    file_directory = filedialog.askdirectory()
 
 
 my_dirs_unfiltered = []
@@ -20,8 +27,13 @@ strings = []
 
 thine_dir = os.getcwd()
 
-OS = 'windows'
+# Detect OS
+if platform.system() == "Windows":
+    OS = "windows"
+else:
+    OS = "linux"
 
+run_dirs, numbers = [], []
 # We want to create a script to run all of these
 num_done = 0
 tot = 0
@@ -30,6 +42,7 @@ for my_dir in my_dirs_unfiltered:
     settings = my_dir.split('_')
     try:
         dinky_winky = int(settings[-1])
+        number = dinky_winky
         if dinky_winky > 19:
             continue
         new_file = '_'.join(settings[:-1])
@@ -37,21 +50,24 @@ for my_dir in my_dirs_unfiltered:
     except ValueError:
         export_type = 'large'
         new_file = '_'.join(settings)
+        number = 0
     tot += 1
     run_dir = file_directory + '/' + my_dir + '/' + new_file + '.pdb'
     export_dir = file_directory + '/' + my_dir
+
     if len(settings) < 4:
+        print(settings)
         continue
     if float(settings[3]) == 0.05:
-        max_vert = 35
+        max_vert = 150
     elif float(settings[3]) <= 0.25:
-        max_vert = 25
+        max_vert = 100
     elif float(settings[3]) <= 0.35:
-        max_vert = 20
+        max_vert = 60
     elif float(settings[3]) <= 0.45:
-        max_vert = 15
+        max_vert = 30
     else:
-        max_vert = 12
+        max_vert = 25
     # Check if the folder for AW exists, aka the network is Done
     if (not os.path.exists(export_dir + '/chain_a_aw') and '.csv' not in export_dir) and not os.path.exists(export_dir + '/' + new_file + '_Network_aw'):
         # Check if the vertices have been solved
@@ -60,33 +76,40 @@ for my_dir in my_dirs_unfiltered:
                            .format(run_dir, max_vert, export_dir, export_type, export_dir + '/verts.txt'))
         else:
             strings.append('\npy vorpy.py {} -s mv {} -s nt compare -e dir {} -e {} -g chain a'.format(run_dir, max_vert, export_dir, export_type))
+        numbers.append(number)
     else:
         num_done += 1
 
-# if OS == 'linux':
-#     j = 0
-#     for i in range(len(strings)):
-#         pass
-#         if j < i // 400 or i == 0:
-#             j = i // 400
-#             with open(thine_dir + '/foam_runs_{}.sh'.format(j), 'w') as foam_write:
-#                 foam_write.write('#!/bin/sh\n')
-#                 foam_write.write(strings[i])
-#         else:
-#             with open(thine_dir + '/foam_runs_{}.sh'.format(j), 'a') as foam_write:
-#                 foam_write.write(strings[i])
-#
-# elif OS == 'windows':
-#
-#     j = 0
-#     for i in range(len(strings)):
-#         if j < i // 220 or i == 0:
-#             j = i // 220
-#             with open(thine_dir + '/foam_runs_{}.bat'.format(j), 'w') as foam_write:
-#                 foam_write.write(strings[i])
-#         else:
-#             with open(thine_dir + '/foam_runs_{}.bat'.format(j), 'a') as foam_write:
-#                 foam_write.write(strings[i])
+# Sort the strings by the las number on their
+strings = [x for _, x in sorted(zip(numbers, strings), key=lambda _: _)]
+
+# Define chunk size for how many strings per file
+chunk_size = 400
+num_files = (len(strings) + chunk_size - 1) // chunk_size  # Calculate number of files
+
+# Initialize file writers and create the files
+file_handles = []
+for j in range(num_files):
+    file_name = f"{thine_dir}/foam_runs_{j}.{'sh' if OS == 'linux' else 'bat'}"
+    mode = 'w'  # Write mode for initial creation
+    file_handles.append(open(file_name, mode))
+
+# Write the strings evenly into the files
+for i, string in enumerate(strings):
+    # Distribute first 'important' strings sequentially across all files
+    file_index = i % num_files if i < num_files else i // chunk_size
+    foam_write = file_handles[file_index]
+
+    # For Linux files, add a header only once per file
+    if foam_write.tell() == 0 and OS == 'linux':
+        foam_write.write('#!/bin/sh\n')
+
+    # Write the current string to the appropriate file
+    foam_write.write(string)
+
+# Close all file handles
+for foam_write in file_handles:
+    foam_write.close()
 
 
 print('{}/{} finished'.format(num_done, tot))
