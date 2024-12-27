4577b9ef6a051d3db6a0fa2c2a3b4e7fda708d2f
jackeWin <jackericson98@gmail.com>
2024-12-27 09:46:12 -0500

    Better plots

--- Data/Analyze/Part_I_Statistical_Ensembles/Fig3/PlotXYZDists.py
+++ Data/Analyze/Part_I_Statistical_Ensembles/Fig3/PlotXYZDists.py
@@ -1,16 +1,13 @@
 import csv
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 
 def convert_point(string_point):
     vals = string_point.split(',')
     return float(vals[0][1:]), float(vals[1]), float(vals[2][:-1])
 
-
 tracker = {}
-with open('location_data.csv', 'r') as my_data:
+with open('location_data1.0.csv', 'r') as my_data:
     reader = csv.reader(my_data)
     for line in reader:
         if len(line) == 0:
@@ -25,17 +22,24 @@ with open('location_data.csv', 'r') as my_data:
 for cv, density in tracker:
     if cv != '0.05' or density != '0.05':
         continue
-    fig, ax = plt.figure(figsize=(8, 6)), plt.gca()
+    fig, ax = plt.figure(figsize=(5, 4)), plt.gca()
     points = np.array(tracker[(cv, density)])
     scatter = ax.scatter(points[:, 0], points[:, 1], c=points[:, 2], cmap='gray', s=0.8, marker='x')
     ax.set_xlabel('X value', fontsize=25)
     ax.set_ylabel('Y value', fontsize=25)
-    ax.set_title('Distribution of Coordinates\n(cv={}, d={})'.format(cv, density), fontsize=30)
+    ax.set_title('Coordinates', fontsize=30)
+
+    # Set x and y axis limits and ticks to 0 and 1 only
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_xticks([0, 1])
+    ax.set_yticks([0, 1])
 
     # Adjusting color bar
     cbar = fig.colorbar(scatter, ax=ax)
     cbar.set_label('Z value', fontsize=25)
     cbar.ax.tick_params(labelsize=20)  # Adjust color bar tick font size
+    cbar.set_ticks([0, 1])
 
     # Adjusting x and y tick font sizes
     ax.tick_params(axis='x', labelsize=20)
