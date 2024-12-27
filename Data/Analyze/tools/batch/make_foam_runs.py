13ae5cdff95529df0e572e8b340ef8cc23244357
jackeWin <jackericson98@gmail.com>
2024-12-27 09:45:45 -0500

    Added a stronger user interface

--- Data/Analyze/tools/batch/make_foam_runs.py
+++ Data/Analyze/tools/batch/make_foam_runs.py
@@ -81,6 +81,11 @@ for my_dir in my_dirs_unfiltered:
     else:
         num_done += 1
 
+
+# Create the yesses and nos dictionary
+yeses = {_: True for _ in {'y', 'ys', 'yes', 'ya', 'yas', 'yess', 'yaur', 't', 'true', 'tru', 'affirmative'}}
+nos = {_: False for _ in {'n', 'no', 'false', 'f', ''}}
+
 # Sort the strings by the las number on their
 strings = [x for _, x in sorted(zip(numbers, strings), key=lambda _: _)]
 
@@ -88,9 +93,49 @@ strings = [x for _, x in sorted(zip(numbers, strings), key=lambda _: _)]
 chunk_size = 400
 num_files = (len(strings) + chunk_size - 1) // chunk_size  # Calculate number of files
 
+# Print the data for the making of foam file runs
+print(f"{num_done}/{tot} finished at {datetime.now().strftime('%Y-%m-%d %I:%M %p')}")
+
 # As if the user wants to make the files
 make_files = input('Make run files? (y/n)  >>>   ')
-if make_files.strip().lower() in {'y', 'yes'}:
+
+
+if yeses[make_files.strip().lower()]:
+
+    # Set the default directory
+    dft_dir = os.getcwd()
+    # Change the destination
+    change_destination = yeses[input(f'Change the output directory from \"...{dft_dir[-10:]}\"?  (y/n)  >>>  ').lower()]
+    # If the change destination thing has been triggered start that process
+    while change_destination:
+        # Get the new directory
+        dft_dir = filedialog.askdirectory()
+        # Print that the directory has been changed to the new directory
+        change_destination = yeses[input(f'\nDirectory changed to:\n')]
+
+    # Ask the initial question
+    num_files_npt = input(f'Change the number of output files (cores): {num_files} files?  (y/n)  >>>  ').lower()
+    # Change the destination
+    while True:
+
+        # First see if the number is something to get out
+        if num_files_npt in nos:
+            break
+        # Next check if the input is a number
+        try:
+            num_files = int(num_files_npt)
+        except ValueError:
+            pass
+        # Next see if the user wants to change it
+        if num_files_npt in yeses:
+            # Change the number of files
+            try:
+                num_files = int(input("How many files?  >>>  ").lower())
+            except ValueError:
+                pass
+        # Confirm this is the correct number of files
+        num_files_npt = input(f"Number of output file set to {num_files}. Change?  (y/n)  >>>>    ")
+
     # Initialize file writers and create the files
     file_handles = []
     for j in range(num_files):
@@ -114,6 +159,3 @@ if make_files.strip().lower() in {'y', 'yes'}:
     # Close all file handles
     for foam_write in file_handles:
         foam_write.close()
-
-
-print(f"{num_done}/{tot} finished at {datetime.now().strftime('%Y-%m-%d %I:%M %p')}")
