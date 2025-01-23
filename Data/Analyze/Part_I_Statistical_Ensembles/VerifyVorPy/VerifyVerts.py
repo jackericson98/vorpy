45dd33f7faa19f4b29328d024fdfaa056eaa3103
Optiplex_7060 <jackericson98@gmail.com>
2025-01-23 14:26:14 -0500

    Allowed for the user to browse and find a file

--- Data/Analyze/Part_I_Statistical_Ensembles/VerifyVorPy/VerifyVerts.py
+++ Data/Analyze/Part_I_Statistical_Ensembles/VerifyVorPy/VerifyVerts.py
@@ -17,13 +17,16 @@ for roott, dirs, files in os.walk(folder):
         for dire in dirs:
             results[dire] = {}
 
+
     # Check for 'Voronota' and 'Vorpy' directories and specific files
     if 'Voronota' in dirs:
         key = None
         for dire in results:
-            if dire in roott:
+            if dire in roott and dire != 'V':
                 key = dire
+        print(roott, key)
         voronota_path = os.path.join(roott, 'Voronota')
+        # print
         if os.path.exists(voronota_path) and 'vertices.txt' in os.listdir(voronota_path):
             results[key]['Voronota'] = {'file': os.path.join(voronota_path, 'vertices.txt')}
         vorpy_path = os.path.join(roott, 'Vorpy')
@@ -37,8 +40,8 @@ for key, value in results.items():
 
     # Load the data from both files
     try:
-        file1 = pd.read_csv(value['Voronota']['file'], delim_whitespace=True, header=None)
-        file2 = pd.read_csv(value['Vorpy']['file'], delim_whitespace=True, header=None)
+        file1 = pd.read_csv(value['Voronota']['file'], delimiter=' ', header=None, dtype=str)
+        file2 = pd.read_csv(value['Vorpy']['file'], delimiter=' ', header=None, dtype=str)
     except KeyError:
         continue
     # Assume the first four columns are vertex identifiers for both files
@@ -47,15 +50,20 @@ for key, value in results.items():
     # Create sets of tuples from both files for comparison
     set1 = {tuple(row) for row in file1[columns_of_interest].values}
     set2 = {tuple(row) for row in file2[columns_of_interest].values}
-
+    # print(set1, set2)
     # Find intersections (common elements)
     intersection = set1.intersection(set2)
+    vpy_miss_verts = len(set2) - len(intersection)
+    vta_miss_verts = len(set1) - len(intersection)
 
     # Calculate similarity metrics
     similarity_percentage = len(intersection) / min(len(set1), len(set2)) * 100
 
     print(f"Number of overlapping vertex configurations: {len(intersection)}")
     print(f"Percentage of smaller dataset represented in the overlap: {similarity_percentage:.2f}%")
+    print(f"Number of VorPy Vertices = {set2} Number of Voronota Vertices = {set1} \n"
+          f"Number of Voronota Vertices Missing from VorPy = {vta_miss_verts}, "
+          f"Number of VorPy Vertices Missing from Voronota = {vpy_miss_verts}")
 
 # Next get the voronota vertices
 
