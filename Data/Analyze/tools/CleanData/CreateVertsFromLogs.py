9c92aacbe06b68bbbcfb6526e3680c87f557ea74
jackeWin <jackericson98@gmail.com>
2025-02-24 14:08:20 -0500

    More cleaning

---
+++ Data/Analyze/tools/CleanData/CreateVertsFromLogs.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+from System.sys_funcs.output.net import write_verts
+from Data.Analyze.tools.compare.read_logs2 import read_logs2
+
+
+def create_verts_from_logs(folder=None):
+
+    if folder is None:
+        root = tk.Tk()
+        root.withdraw()
+        root.wm_attributes('-topmost', 1)
+        folder = filedialog.askdirectory()
+
+    for subfolder in os.listdir(folder):
+        # Create the aw folder
+        if not os.path.exists(folder + '/' + subfolder + '/aw'):
+            os.mkdir(folder + '/' + subfolder + '/aw')
+        # Create the pow folder
+        if not os.path.exists(folder + '/' + subfolder + '/pow'):
+            os.mkdir(folder + '/' + subfolder + '/pow')
+
+        # For each subfolder we need to first get the logs and then read them
+        # aw_path1 = folder + '/' + subfolder + '/aw/aw_logs.csv'
+        # aw_path2 = folder + '/' + subfolder + '/aw_logs.csv'
+        # aw_path1 = folder + '/' + subfolder + '/aw/aw_logs.csv'
+        # aw_path2 = folder + '/' + subfolder + '/aw_logs.csv'
+        # if os.path.exists(aw_path1):
+        try:
+            shutil.move(folder + '/' + subfolder + '/aw_logs.csv', folder + '/' + subfolder + '/aw/aw_logs.csv')
+        except FileNotFoundError:
+            pass
+        try:
+            shutil.move(folder + '/' + subfolder + '/pow_logs.csv', folder + '/' + subfolder + '/pow/pow_logs.csv')
+        except FileNotFoundError:
+            pass
+        aw_logs = read_logs2(folder + '/' + subfolder + '/aw/aw_logs.csv', vert_list=True)
+        pow_logs = read_logs2(folder + '/' + subfolder + '/pow/pow_logs.csv', vert_list=True)
+        # Write the aw_verts
+        with open(folder + '/' + subfolder + '/aw/aw_verts.txt', 'w') as file:
+
+            # Create a header for the vertices file
+            file.write("Vertices - {} vertices, {} atoms, max vert = {}, Net type = {}\n"
+                       .format(len(aw_logs['verts']['Index']), 1000, max(aw_logs['verts']['rad']),
+                               'aw'))
+            # Write the vertices
+            for i, vert in aw_logs['verts'].iterrows():
+                # Write the vertex
+                file.write(" ".join([str(_) for _ in vert['Balls']]) + " " + " ".join([str(_) for _ in vert['loc']]) +
+                           " " + str(vert['rad']) + " \n")
+            # Write the end line for the file
+            file.write("END")
+
+        # Write the aw_verts
+        with open(folder + '/' + subfolder + '/pow/pow_verts.txt', 'w') as file:
+
+            # Create a header for the vertices file
+            file.write("Vertices - {} vertices, {} atoms, max vert = {}, Net type = {}\n"
+                       .format(len(pow_logs['verts']['Index']), 1000, max(pow_logs['verts']['rad']),
+                               'pow'))
+            # Write the vertices
+            for i, vert in pow_logs['verts'].iterrows():
+                # Write the vertex
+                file.write(" ".join([str(_) for _ in vert['Balls']]) + " " + " ".join([str(_) for _ in vert['loc']]) +
+                           " " + str(vert['rad']) + " \n")
+            # Write the end line for the file
+            file.write("END")
+
+
+if __name__ == '__main__':
+    create_verts_from_logs()
