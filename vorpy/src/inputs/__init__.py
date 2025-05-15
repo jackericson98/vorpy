5e6947caaed5a5a0ecc132a53e28fded9c83acc8
Optiplex_7060 <jackericson98@gmail.com>
2025-05-15 15:16:41 -0400

    Added two more functions

--- vorpy/src/inputs/__init__.py
+++ vorpy/src/inputs/__init__.py
@@ -5,6 +5,8 @@ from vorpy.src.inputs.integrate_net import integrate_net
 from vorpy.src.inputs.mol import read_mol
 from vorpy.src.inputs.net import read_net
 from vorpy.src.inputs.pdb import read_pdb
+from vorpy.src.inputs.pdb import read_pdb_simple
+from vorpy.src.inputs.pdb import read_pdb_line
 from vorpy.src.inputs.txt import read_txt
 from vorpy.src.inputs.surfs import read_surf
 from vorpy.src.inputs.verts import read_verts
