b95d3f3684ccb8b2c4aae83a4c7ca6a50b9c055a
Optiplex_7060 <jackericson98@gmail.com>
2025-07-28 13:15:04 -0400

    Added init file

--- vorpy/src/output/color_tris.py
+++ vorpy/src/output/color_tris.py
@@ -229,13 +229,7 @@ def color_tris(surf, color_scheme, color_map, color_factor, max_val=None, min_va
     except AttributeError:
         my_cmap = mpl.cm.get_cmap(color_map)
     except ValueError:
-        try:
-            my_cmap = mpl.cm.get_cmap(color_map.capitalize())
-        except ValueError:
-            try:
-                my_cmap = mpl.cm.get_cmap(color_map.lower())
-            except ValueError:
-                my_cmap = mpl.cm.get_cmap('viridis')
+        my_cmap = mpl.cm.get_cmap(color_map.capitalize())
     except Exception as e:
         print(f"Error: {e}")
         my_cmap = mpl.cm.get_cmap('viridis')
