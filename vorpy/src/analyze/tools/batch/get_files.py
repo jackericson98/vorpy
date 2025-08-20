54ea54699d7e8e5e7d3daaa4af53ae23cd371933
Optiplex_7060 <jackericson98@gmail.com>
2025-08-20 12:30:22 -0400

    Added a new get_files function

--- vorpy/src/analyze/tools/batch/get_files.py
+++ vorpy/src/analyze/tools/batch/get_files.py
@@ -1,5 +1,7 @@
 import os
 from os import path
+import tkinter as tk
+from tkinter import filedialog
 
 
 def get_files(folder):
@@ -24,3 +26,30 @@ def get_files(folder):
                     if file.endswith('.csv'):
                         pow_logs = path.join(root_dir, sub_dir, file)
     return pdb_file, aw_logs, pow_logs
+
+
+def get_all_files(folder=None):
+    """Goes through the folder finding all the systems and gethers their logs, but only if they are full"""
+    # Get the folder from the user if not provided
+    if folder is None:
+        root = tk.Tk()
+        root.withdraw()
+        folder = filedialog.askdirectory()
+    # Create a dictionary to store the files
+    files = {}
+    # Loop through the folder finding the systems
+    for subfolder in os.listdir(folder):
+        # Make the new dictionary entry
+        sys_letter = subfolder[0]
+        sys_name = subfolder.split('_')[1]
+        
+        if all(os.path.exists(os.path.join(folder, subfolder, f"{log}_logs.csv")) for log in ['aw', 'pow', 'prm']):
+            files[sys_letter] = {'name': sys_name, 'aw': os.path.join(folder, subfolder, 'aw_logs.csv'), 'pow': os.path.join(folder, subfolder, 'pow_logs.csv'), 'prm': os.path.join(folder, subfolder, 'prm_logs.csv')}
+        
+    return files
+
+
+if __name__ == "__main__":
+    files = get_all_files()
+    for key, value in files.items():
+        print(key, value)
