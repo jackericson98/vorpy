2109cd8fe0d676ae8d579e887eb5117b7b3c50a2
Optiplex_7060 <jackericson98@gmail.com>
2025-08-26 16:59:23 -0400

    Removed integrate net from the zeitguiest

--- vorpy/src/inputs/__init__.py
+++ vorpy/src/inputs/__init__.py
@@ -1,7 +1,7 @@
 from vorpy.src.inputs.balls import read_balls
 from vorpy.src.inputs.cif import read_cif
 from vorpy.src.inputs.gro import read_gro
-from vorpy.src.inputs.integrate_net import integrate_net
+# from vorpy.src.inputs.integrate_net import integrate_net
 from vorpy.src.inputs.mol import read_mol
 from vorpy.src.inputs.net import read_net
 from vorpy.src.inputs.pdb import read_pdb
