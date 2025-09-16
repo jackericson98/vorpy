e5a5095ff09b0d9a734a268ffbafc55ee4ab98ff
Optiplex_7060 <jackericson98@gmail.com>
2025-09-16 12:55:38 -0400

    You can get data by the chain instead of all together

--- vorpy/src/analyze/Part_II_Molecular_Analysis/SuplamentalMaterial/FrameAnalysis.py
+++ vorpy/src/analyze/Part_II_Molecular_Analysis/SuplamentalMaterial/FrameAnalysis.py
@@ -18,7 +18,7 @@ from vorpy.src.analyze.tools.compare.read_logs2 import read_logs2
 from vorpy.src.analyze.tools.plot_templates.line import line_plot
 
 
-def get_frames(folder=None):
+def get_frames_by_group(folder=None, chains=None):
     # Get the folder if it is not provided
     if folder is None:
         root = tk.Tk()
@@ -26,6 +26,7 @@ def get_frames(folder=None):
         folder = filedialog.askdirectory()
     # Create the frame dictionary
     frame_dict = {}
+    max_diffs = {}
     # Loop through the folders in the folder
     for subfolder in os.listdir(folder):
         if subfolder[0] != 'f':
@@ -37,15 +38,32 @@ def get_frames(folder=None):
         pw = os.path.join(folder, subfolder, 'pow/pow_logs.csv')
         pm = os.path.join(folder, subfolder, 'prm/prm_logs.csv')
         # read the logs
-        aw_logs = read_logs2(aw, all_=False)
-        pw_logs = read_logs2(pw, all_=False)
-        pm_logs = read_logs2(pm, all_=False)
-        # Get the volumes
-        aw_vol = aw_logs['group data']['Volume']
-        pw_vol = pw_logs['group data']['Volume']
-        pm_vol = pm_logs['group data']['Volume']
-        # Add the frames to the frame dictionary
-        frame_dict[frame] = {'aw': aw_vol, 'pow': pw_vol, 'prm': pm_vol}
+        aw_logs = read_logs2(aw, all_=False, balls=True)
+        pw_logs = read_logs2(pw, all_=False, balls=True)
+        pm_logs = read_logs2(pm, all_=False, balls=True)
+        # Add the volumes to the frame dictionary
+        frame_dict[frame] = {'aw': 0, 'pow': 0, 'prm': 0}
+        max_diffs[frame] = {'pow': (-1, 0), 'prm': (-1, 0)}
+        # Get the atoms
+        for i, aw_atom in aw_logs['atoms'].iterrows():
+            if chains is not None and aw_atom['Chain'] not in chains:
+                continue
+            # Get the pow information
+            pw_atom = pw_logs['atoms'].loc[pw_logs['atoms']['Index'] == aw_atom['Index']].to_dict()
+            pm_atom = pm_logs['atoms'].loc[pm_logs['atoms']['Index'] == aw_atom['Index']].to_dict()
+            # Get the volume
+            aw_vol = aw_atom['Volume']
+            pw_vol = pw_atom['Volume'][aw_atom['Index']]
+            pm_vol = pm_atom['Volume'][aw_atom['Index']]
+
+            max_diffs[frame]['pow'] = (aw_atom['Index'], max(max_diffs[frame]['pow'][1], abs(aw_vol - pw_vol) / aw_vol))
+            max_diffs[frame]['prm'] = (aw_atom['Index'], max(max_diffs[frame]['prm'][1], abs(aw_vol - pm_vol) / aw_vol))
+
+            # Add the frames to the frame dictionary
+            frame_dict[frame]['aw'] += aw_vol
+            frame_dict[frame]['pow'] += pw_vol
+            frame_dict[frame]['prm'] += pm_vol
+    print(max_diffs)
     return frame_dict
 
 
@@ -65,12 +83,21 @@ def plot_frame_diffs(frame_dict):
         pow_diffs.append(pow_diff)
         prm_diffs.append(prm_diff)
         frame_names.append(frame)
+
+    # INSERT_YOUR_CODE
+    # Sort the diffs and frame_names by frame number (ascending)
+    sorted_data = sorted(zip(frame_names, pow_diffs, prm_diffs))
+    frame_names, pow_diffs, prm_diffs = zip(*sorted_data)
+    frame_names = list(frame_names)
+    pow_diffs = list(pow_diffs)
+    prm_diffs = list(prm_diffs)
     # Plot the differences
     line_plot([frame_names, frame_names], [pow_diffs, prm_diffs], y_label='% Difference', 
               title='% Difference from AW', Show=True, x_label="Frame", labels=["Pow", "Prm"], 
               colors=["red", "purple"], y_ticks=[-2, -1, 0, 1, 2, 3], y_label_size=20, x_label_size=20, 
               title_size=25, xlim=[0.5, 11.5], x_ticks=[2, 4, 6, 8, 10], axis_line_thickness=2,
               linewidth=3, tick_val_size=20)
+
 
 def plot_frame_deviations(frame_dict):
     # Create the lists
@@ -94,6 +121,12 @@ def plot_frame_deviations(frame_dict):
         pow_devs.append(pow_dev)
         prm_devs.append(prm_dev)
         frame_names.append(frame)
+    # Sort the diffs and frame_names by frame number (ascending)
+    sorted_data = sorted(zip(frame_names, pow_devs, prm_devs))
+    frame_names, pow_devs, prm_devs = zip(*sorted_data)
+    frame_names = list(frame_names)
+    pow_devs = list(pow_devs)
+    prm_devs = list(prm_devs)
     # Plot the deviations
     line_plot([frame_names, frame_names, frame_names], [aw_devs, pow_devs, prm_devs], y_label='% Deviation', 
               title='Frame Volume Deviation', Show=True, x_label="Frame", labels=["AW", "Pow", "Prm"], 
@@ -101,8 +134,8 @@ def plot_frame_deviations(frame_dict):
               title_size=25, xlim=[0.5, 11.5], x_ticks=[2, 4, 6, 8, 10], axis_line_thickness=2, 
               linewidth=3, tick_val_size=20, alpha=0.8)
 
+
 if __name__ == "__main__":
-    frame_dict = get_frames()
+    frame_dict = get_frames_by_group()
     plot_frame_diffs(frame_dict)
     plot_frame_deviations(frame_dict)
-
